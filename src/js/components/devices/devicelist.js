cd3e67d2fb92cc9bd520c203799c2e2871e72809
Michael Clelland <michael.clelland@northern.tech>
2018-01-05 11:57:22 +0100

    Revert "Small fixes"

--- src/js/components/devices/devicelist.js
+++ src/js/components/devices/devicelist.js
@@ -480,13 +480,13 @@ var DeviceList = createReactClass({
                   data-tip
                   data-for='expand-device-tip'
                   data-event='click focus'
-                  style={{left: "inherit", right:"45px", bottom: "132px"}}>
+                  style={{left: "inherit", right:"45px", bottom: "85px"}}>
                   <FontIcon className="material-icons">help</FontIcon>
                 </div>
                 <ReactTooltip
                   id="expand-device-tip"
                   globalEventOff='click'
-                  place="left"
+                  place="bottom"
                   type="light"
                   effect="solid"
                   className="react-tooltip">
