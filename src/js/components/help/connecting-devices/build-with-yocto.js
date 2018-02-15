3b64fed34088cfa202fabeffff279aee57d1ebd9
Eystein Måløy Stenberg <eystein.maloy.stenberg@cfengine.com>
2018-02-15 15:01:55 -0800

    adjust help text for easier RPi3, BBB and Yocto use with Hosted Mender
    Signed-off-by: Eystein Måløy Stenberg <eystein.maloy.stenberg@cfengine.com>

--- src/js/components/help/connecting-devices/build-with-yocto.js
+++ src/js/components/help/connecting-devices/build-with-yocto.js
@@ -13,9 +13,9 @@ var BuildYocto =  createReactClass({
       <div>
 
         <h2>Build with Yocto</h2>
-        <p>You can build disk images and Artifacts with any software of your choosing that support Mender for any device with the Mender Yocto Project integration.</p>
+        <p>You can build your own Yocto Project images for use with Mender. By updating a small part of your build configuration, your newly provisioned devices will securely connect to the Mender server the first time they boot.</p>
 
-        <p>Follow the docs at <a href="https://docs.mender.io/development/artifacts/building-mender-yocto-image">https://docs.mender.io/development/artifacts/building-mender-yocto-image</a> to get your .sdimg and .mender files.</p>
+        <p>Follow the docs at <a href="https://docs.mender.io/development/artifacts/building-mender-yocto-image">https://docs.mender.io/development/artifacts/building-mender-yocto-image</a> to build your .sdimg and .mender files.</p>
 
         
         {this.props.isHosted ? 
@@ -23,12 +23,16 @@ var BuildYocto =  createReactClass({
         <div>
           <p>Make sure to update the local.conf build configuration:</p>
 
+          <h3>Remove demo layer if used</h3>
+
           <p>You should not be using the <span className="code">meta-mender-demo layer</span>. If you have it in your build environment, you need to remove it.</p>
-          <p>In order to remove the meta-mender-demo layer, go to your <span className="code">build</span> directory and type run the following command:</p>
+          <p>Go to your <span className="code">build</span> directory and run the following command:</p>
 
           <div className="code">
           bitbake-layers remove-layer ../meta-mender/meta-mender-demo
           </div>
+
+          <h3>Update local.conf for Hosted Mender</h3>
 
           <p>Add or replace the following two lines in your <span className="code">local.conf</span>:</p>
 
--- src/js/components/help/connecting-devices/raspberry-pi-3.js
+++ src/js/components/help/connecting-devices/raspberry-pi-3.js
@@ -34,25 +34,27 @@ var RaspberryPi =  createReactClass({
 
         <p>To make testing easy, we provide demo images you can test Mender with for the Raspberry Pi 3.</p>
 
-        <p>The purpose of this is to make it easier for you to test Mender with a physical device. {this.props.isHosted ? <span>The demo images contain configuration specific for your Mender setup, so they will not work on other setups out of the box.</span> : null}</p>
+        <p>The purpose of this is to make it easier for you to test Mender with a physical device.</p>
 
-        <p>If you would like to use a different device type, please see the <a onClick={this.props.changePage.bind(null, "help/connecting-devices/demo-artifacts")}>other demo images</a> or <a onClick={this.props.changePage.bind(null, "help/connecting-devices/build-with-yocto")}>Build with Yocto Project</a>.</p>
+        <p>If you would like to use a different device type, please see the page on <a onClick={this.props.changePage.bind(null, "help/connecting-devices/provision-a-demo/beagleboneblack")}>BeagleBone Black</a> or <a onClick={this.props.changePage.bind(null, "help/connecting-devices/build-with-yocto")}>Build with Yocto Project</a>.</p>
 
         <h3>Prerequisites</h3>
 
         <h4>A device to test with</h4>
         <p>One or more Raspberry Pi 3 devices.</p>
 
-        <h4>Internet connectivity</h4>
-        <p>An Ethernet cable connection for your device(s) that provides DHCP and allows internet access over TLS/443.</p>
-
+        <h4>Ability to connect to Hosted Mender over the Internet</h4>
+        <p>An Ethernet cable connection for your device(s) that provides DHCP and allows Internet access over TLS/443.</p>
+        <p>Specifically, your device needs to be able to create outgoing TCP connections to hosted.mender.io and s3.amazonaws.com, both on port 443 (TLS).</p>
+        <p>As the Mender client does not listen to any ports there are no incoming connections.</p>
 
         <h3>Download the disk image</h3>
 
         {!this.props.isEmpty(link) ? 
-          <p><a href={link.href}>Download the correct disk image for Raspberry Pi 3 here</a>.</p>
+          <p><a href={link.href}>Download the disk image for Raspberry Pi 3 here</a>.</p>
+          <p>This demo disk image already contains configuration specific to Hosted Mender and your account (i.e. your tenant token), so it does not need any further configuration. Devices flashed with this disk image will connect to your Hosted Mender account when the devices boot.</p>
           :
-          <p>Download the correct disk image for Raspberry Pi 3 from <a href="https://docs.mender.io/development/getting-started/download-test-images" target="_blank">the downloads page</a>.</p>
+          <p>Download the disk image for Raspberry Pi 3 from <a href="https://docs.mender.io/development/getting-started/download-test-images" target="_blank">the downloads page</a>.</p>
         }
 
         <p>After the image has been downloaded, unpack it:</p>
@@ -64,17 +66,17 @@ var RaspberryPi =  createReactClass({
         <p>Insert the SD card of your device into your workstation.</p>
 
         <p>Find the path to the device where your SD card is placed on your workstation.</p>
-        <p>Normally this will be <span className="code">/dev/mmcblk0</span> or <span className="code">/dev/sdb</span>. If you are unsure about its location, refer to these tutorials for <a href="https://www.raspberrypi.org/documentation/installation/installing-images/linux.md" target="_blank">Linux</a>, <a href="https://www.raspberrypi.org/documentation/installation/installing-images/mac.md" target="_blank">Mac OS</a> or <a href="https://www.raspberrypi.org/documentation/installation/installing-images/windows.md" target="_blank">Windows</a>.</p>
+        <p> Normally this will be <span className="code">/dev/mmcblk0</span> or <span className="code">/dev/sdb</span>. If you are unsure about its location, refer to these tutorials for <a href="https://www.raspberrypi.org/documentation/installation/installing-images/linux.md" target="_blank">Linux</a>, <a href="https://www.raspberrypi.org/documentation/installation/installing-images/mac.md" target="_blank">Mac OS</a> or <a href="https://www.raspberrypi.org/documentation/installation/installing-images/windows.md" target="_blank">Windows</a>.</p>
 
-        <p>When you know the correct device for the SD card, set it and your disk image in shell variables and write it with the following commands:</p>
+        <p> When you know the correct device for the SD card, set it and your disk image in shell variables and write it with the following commands:</p>
 
         <div className="code">
           <p>{'SD_CARD_DEVICE=<YOUR-SD-CARD-DEVICE>'}</p>
           <p>{'PATH_TO_DISK_IMAGE=<PATH-TO-YOUR-DISK-IMAGE>.sdimg'}</p>
           <p>sudo dd if=$PATH_TO_DISK_IMAGE of=$SD_CARD_DEVICE bs=1M && sudo sync</p>
         </div>
-        <p>This may take a few minutes complete; wait until the command returns.</p>
 
+        <p>This may take a few minutes complete; wait until the command returns.</p>
 
         <h3>Boot the device</h3>
 
@@ -85,7 +87,23 @@ var RaspberryPi =  createReactClass({
         <p>Wait for 3-4 minutes until the device has fully booted and the Mender client has been able to connect to the Mender server.</p>
         <p>You should see your device pending in the <a onClick={this.props.changePage.bind(null, "devices")}>Devices tab</a>. Simply Authorize the pending device.</p>
 
-        <p>If you cannot see any new device after 10 minutes, please verify that the network connection is working, and feel free to reach out for help with diagnostics via <a onClick={this.props.changePage.bind(null, "help/more-help-resources")}>our further Help resources</a>.</p>
+        <p> If you cannot see any new device after 10 minutes, please verify that the network connection is working, and feel free to reach out for help with diagnostics via <a onClick={this.props.changePage.bind(null, "help/more-help-resources")}>our further Help resources</a>.</p>
+
+        <p> NOTE: If you reimage the device with the disk image or switch storage (e.g. SD cards) between the devices after they have booted once, <b>authenticating the device to the Mender server will fail</b>. This is because the Mender server tracks the identity (MAC address by default) <-> device public key (randomly generated upon first run of the Mender client) binding. If this happens, you need to *decommission* the device from the Mender server and try again. <p>
+
+
+        <h3>Deploy updates</h3>
+
+        {this.props.isHosted ? 
+          <p>Download these two Raspberry Pi 3 Artifacts customized for your Hosted Mender account: TODO LINKS: Artifact1, [Artifact2].</p>
+          :
+          <p>Download Artifact 1 and Artifact 2 for Raspberry Pi 3 from <a href="https://docs.mender.io/development/getting-started/download-test-images" target="_blank">the downloads page</a>.</p>
+        }
+
+        After you have downloaded the two Artifacts, upload them to the Mender server in the <a onClick={this.props.changePage.bind(null, "artifacts")}>Artifacts tab</a>.
+
+        Then head over to the <a onClick={this.props.changePage.bind(null, "deployments")}>Deployments tab</a> and do some remote deployments to your device!
+
       </div>
     )
   }
