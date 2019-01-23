2fe418927c135dac23fe151f907e99d919e2dde6
Manuel Zedel <manuel.zedel@northern.tech>
2019-01-23 13:50:52 +0100

    implemented device acceptance history for the dashboard
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/dashboard/devices.js
+++ src/js/components/dashboard/devices.js
@@ -23,17 +23,48 @@ export class Devices extends React.Component {
     self.state = { devices: [], inactiveDevices: [], pendingDevices: [], deltaActivity: null };
     AppActions.getAllDevicesByStatus('pending').then(pendingDevices => self.setState({ pendingDevices }));
     AppActions.getAllDevicesByStatus('accepted').then(devices => {
-      const inactiveDevices = devices.reduce((accu, device) => {
-        console.log(device.updated_ts);
-        return accu;
-      }, []);
-      const yesterdaysDelta = 0;
-      self.setState({ devices, inactiveDevices, yesterdaysDelta });
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      const yesterdaysIsoString = yesterday.toISOString();
+      const inactiveDevices = devices
+        // now boil the list down to the ones that were not updated since yesterday
+        .reduce((accu, item) => {
+          if (item.updated_ts < yesterdaysIsoString) {
+            accu.push(item);
+          }
+          return accu;
+        }, []);
+      const deltaActivity = this._updateDeviceActivityHistory(new Date(), yesterday, devices.length);
+      self.setState({ devices, inactiveDevices, deltaActivity });
     });
   }
 
-  _clickHandle(params) {
-    this.props.clickHandle(params);
+  _updateDeviceActivityHistory(today, yesterday, deviceCount) {
+    const jsonContent = window.localStorage.getItem('dailyDeviceActivityCount')
+    let history = [];
+    try {
+      history = jsonContent ? JSON.parse(jsonContent) : [];
+    } catch (error) {
+      console.warn(error);
+      window.localStorage.setItem('dailyDeviceActivityCount', JSON.stringify(history));
+    }
+    const yesterdaysDate = yesterday.toISOString().split('T')[0];
+    const todaysDate = today.toISOString().split('T')[0];
+    const result = history.reduce((accu, item) => {
+      if (item.date < yesterdaysDate) {
+        accu.previousCount = item.count;
+      }
+      if (item.date === todaysDate) {
+        accu.newDay = false;
+      }
+      return accu;
+    }, { previousCount: 0, newDay: true });
+    const previousCount = result.previousCount;
+    if (result.newDay) {
+      history.unshift({ count: deviceCount, date: todaysDate });
+    }
+    window.localStorage.setItem('dailyDeviceActivityCount', JSON.stringify(history.slice(0, 7)));
+    return deviceCount - previousCount;
   }
 
   render() {
