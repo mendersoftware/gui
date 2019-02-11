3752b5515bf6389fe1374f02fc5640ebdb81dfab
Manuel Zedel <manuel.zedel@northern.tech>
2019-02-11 18:05:57 +0100

    device widgets now polling devicecount changes
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/dashboard/devices.js
+++ src/js/components/dashboard/devices.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BoardIcon from 'react-material-icons/icons/hardware/developer-board';
 
 import AppActions from '../../actions/app-actions';
+import AppStore from '../../stores/app-store';
 import { AcceptedDevices } from './widgets/accepteddevices';
 import { RedirectionWidget } from './widgets/redirectionwidget';
 import { PendingDevices } from './widgets/pendingdevices';
@@ -20,27 +21,41 @@ export class Devices extends React.Component {
   constructor(props, state) {
     super(props, state);
     const self = this;
-    self.state = { devices: [], inactiveDevices: [], pendingDevices: [], deltaActivity: null };
-    AppActions.getAllDevicesByStatus('pending').then(pendingDevices => self.setState({ pendingDevices }));
-    AppActions.getAllDevicesByStatus('accepted').then(devices => {
-      const yesterday = new Date();
-      yesterday.setDate(yesterday.getDate() - 1);
-      const yesterdaysIsoString = yesterday.toISOString();
-      const inactiveDevices = devices
-        // now boil the list down to the ones that were not updated since yesterday
-        .reduce((accu, item) => {
-          if (item.updated_ts < yesterdaysIsoString) {
-            accu.push(item);
-          }
-          return accu;
-        }, []);
-      const deltaActivity = this._updateDeviceActivityHistory(new Date(), yesterday, devices.length);
-      self.setState({ devices, inactiveDevices, deltaActivity });
-    });
+    self.timer = null;
+    self.state = { devices: [], inactiveDevices: [], pendingDevices: [], deltaActivity: null, refreshDevicesLength: 30000 };
+    // on render the store might not be updated so we resort to the API and let all later request go through the store
+    // to be in sync with the rest of the UI
+    AppActions.getAllDevicesByStatus('pending').then(devices => self.setState({ pendingDevices: devices.length }));
+    self._refreshDevices().then(result => self.setState(result));
   }
-
+  componentDidMount() {
+    var self = this;
+    self.timer = setInterval(() => self._refreshDevices().then(result => self.setState(result)), self.state.refreshDevicesLength);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  _refreshDevices() {
+    return AppActions.getAllDevicesByStatus('accepted')
+      .then(devices => {
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        const yesterdaysIsoString = yesterday.toISOString();
+        const inactiveDevices = devices
+          // now boil the list down to the ones that were not updated since yesterday
+          .reduce((accu, item) => {
+            if (item.updated_ts < yesterdaysIsoString) {
+              accu.push(item);
+            }
+            return accu;
+          }, []).length;
+        const deltaActivity = this._updateDeviceActivityHistory(new Date(), yesterday, devices.length);
+        return Promise.resolve({ devices: devices.length, inactiveDevices, deltaActivity });
+      })
+      .then(result => Object.assign(result, { pendingDevices: AppStore.getTotalPendingDevices() }));
+  }
   _updateDeviceActivityHistory(today, yesterday, deviceCount) {
-    const jsonContent = window.localStorage.getItem('dailyDeviceActivityCount')
+    const jsonContent = window.localStorage.getItem('dailyDeviceActivityCount');
     let history = [];
     try {
       history = jsonContent ? JSON.parse(jsonContent) : [];
@@ -50,15 +65,18 @@ export class Devices extends React.Component {
     }
     const yesterdaysDate = yesterday.toISOString().split('T')[0];
     const todaysDate = today.toISOString().split('T')[0];
-    const result = history.reduce((accu, item) => {
-      if (item.date < yesterdaysDate) {
-        accu.previousCount = item.count;
-      }
-      if (item.date === todaysDate) {
-        accu.newDay = false;
-      }
-      return accu;
-    }, { previousCount: 0, newDay: true });
+    const result = history.reduce(
+      (accu, item) => {
+        if (item.date < yesterdaysDate) {
+          accu.previousCount = item.count;
+        }
+        if (item.date === todaysDate) {
+          accu.newDay = false;
+        }
+        return accu;
+      },
+      { previousCount: 0, newDay: true }
+    );
     const previousCount = result.previousCount;
     if (result.newDay) {
       history.unshift({ count: deviceCount, date: todaysDate });
@@ -69,7 +87,7 @@ export class Devices extends React.Component {
 
   render() {
     const { devices, inactiveDevices, pendingDevices, deltaActivity } = this.state;
-    const hasPending = pendingDevices.length > 0;
+    const hasPending = pendingDevices > 0;
     const noDevicesAvailable = !(devices.length || pendingDevices.length);
     return (
       <div>
@@ -79,8 +97,20 @@ export class Devices extends React.Component {
           </h3>
         </div>
         <div style={styles.container}>
-          <PendingDevices itemStyle={styles.containedItems} pendingDevices={pendingDevices} isActive={hasPending} showHelptips={this.props.showHelptips} onClick={this.props.clickHandle} />
-          <AcceptedDevices itemStyle={styles.containedItems} devices={devices} inactiveDevices={inactiveDevices} delta={deltaActivity} onClick={this.props.clickHandle} />
+          <PendingDevices
+            itemStyle={styles.containedItems}
+            pendingDevicesCount={pendingDevices}
+            isActive={hasPending}
+            showHelptips={this.props.showHelptips}
+            onClick={this.props.clickHandle}
+          />
+          <AcceptedDevices
+            itemStyle={styles.containedItems}
+            devicesCount={devices}
+            inactiveCount={inactiveDevices}
+            delta={deltaActivity}
+            onClick={this.props.clickHandle}
+          />
           <RedirectionWidget
             itemStyle={styles.containedItems}
             target={'/help/connecting-devices'}
