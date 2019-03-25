1a6eea87b5ad81723f7a1ba52d0b9ed543b891d7
Manuel Zedel <manuel.zedel@northern.tech>
2019-03-25 23:30:30 +0100

    detect long attribute lines and only make those expandable
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>
    (cherry picked from commit 72d6e01650945b8b981df51c322324098b721388)

--- src/js/components/devices/expandable-device-attribute.js
+++ src/js/components/devices/expandable-device-attribute.js
@@ -9,19 +9,41 @@ export default class ExpandableDeviceAttribute extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      expanded: false
+      expanded: false,
+      overflowActive: false
     };
+  }
+
+  componentDidMount() {
+    const self = this;
+    if (self.textContent && self.textContainer) {
+      const overflowActive = self.textContent.offsetWidth > self.textContainer.clientWidth;
+      self.setState({ overflowActive });
+    }
   }
 
   render() {
     const self = this;
     const { primary, secondary, classes, textClasses } = self.props;
     const defaultClasses = { root: 'attributes', disabled: 'opaque' };
-    const currentTextClasses = { secondary: `${textClasses ? textClasses.secondary : ''} ${self.state.expanded ? 'expanded' : ''}` };
+    const currentTextClasses = `${textClasses ? textClasses.secondary : ''} ${self.state.expanded ? 'expanded-attribute' : ''}`;
+    let secondaryText = (
+      <div className={currentTextClasses} ref={r => (self.textContainer = r)}>
+        <span className={currentTextClasses} ref={r => (self.textContent = r)}>
+          {secondary}
+        </span>{' '}
+        {self.state.overflowActive ? <a>show {self.state.expanded ? 'less' : 'more'}</a> : null}
+      </div>
+    );
     return (
       <div onClick={() => self.setState({ expanded: !self.state.expanded })}>
         <ListItem classes={classes || defaultClasses} disabled={true}>
-          <ListItemText classes={currentTextClasses} primary={primary} secondary={secondary} secondaryTypographyProps={{ title: secondary }} />
+          <ListItemText
+            classes={{ secondary: currentTextClasses }}
+            primary={primary}
+            secondary={secondaryText}
+            secondaryTypographyProps={{ title: secondary }}
+          />
         </ListItem>
         <Divider />
       </div>
