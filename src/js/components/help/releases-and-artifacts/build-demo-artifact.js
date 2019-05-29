78f71bdcff88aff9b3774ca23daf6bfc5c5853b7
Michael Clelland <michael.clelland@mender.io>
2019-05-29 14:37:32 +0200

    Update demo build Artifact instructions Changelog: None
    Signed-off-by: Michael Clelland <michael.clelland@mender.io>

--- src/js/components/help/releases-and-artifacts/build-demo-artifact.js
+++ src/js/components/help/releases-and-artifacts/build-demo-artifact.js
@@ -25,8 +25,8 @@ export default class BuildDemoArtifact extends React.Component {
   render() {
 
     var executable = `chmod +x mender-artifact`;
-    var directory_install = `wget https://raw.githubusercontent.com/mendersoftware/mender/master/support/modules-artifact-gen/single-file-artifact-gen \nchmod +x file-install-artifact-gen`;
-    var generate = `ARTIFACT_NAME="demo-webserver-2.0" \nDEVICE_TYPE="*" \nOUTPUT_PATH="demo_webserver-2.0.mender" \nDEST_DIR="/opt/installed-by-file-installer/" \nFILE_TREE="dir-to-deploy" \n/file-install-artifact-gen -n` + '${ARTIFACT_NAME} -t ${DEVICE_TYPE} -d ${DEST_DIR} -o ${OUTPUT_PATH} ${FILE_TREE}';
+    var file_install = `wget https://raw.githubusercontent.com/mendersoftware/mender/master/support/modules-artifact-gen/single-file-artifact-gen \nchmod +x single-file-artifact-gen`;
+    var generate = `ARTIFACT_NAME="demo-webserver-updated" \nDEVICE_TYPE="generic_x86" \nOUTPUT_PATH="demo-webserver-updated.mender" \nDEST_DIR="/var/www/localhost/htdocs/" \nFILE_NAME="index.html" \n./single-file-artifact-gen -n` + ' ${ARTIFACT_NAME} -t ${DEVICE_TYPE} -d ${DEST_DIR} -o ${OUTPUT_PATH} ${FILE_NAME}';
   
     return (
       <div>
@@ -54,29 +54,30 @@ export default class BuildDemoArtifact extends React.Component {
           <p>{this.state.executable ? <span className="green fadeIn">Copied to clipboard.</span> : null}</p>
         </div>
 
-        <p>2. Copy and paste the following to download <span className="code">directory-artifact-gen</span> and make it executable:</p>
+        <p>2. Copy and paste the following to download <span className="code">single-file-artifact-gen</span> and make it executable:</p>
 
         <div>
           <div className="code">
-            <CopyToClipboard text={directory_install} onCopy={() => this._copied('directory_install')}>
+            <CopyToClipboard text={file_install} onCopy={() => this._copied('file_install')}>
               <IconButton style={{ float: 'right', margin: '-20px 0 0 10px' }}>
                 <CopyPasteIcon/>
               </IconButton>
             </CopyToClipboard>
-            <span style={{ wordBreak: 'break-word' }}>{directory_install}</span>
+            <span style={{ wordBreak: 'break-word' }}>{file_install}</span>
           </div>
 
-          <p>{this.state.directory_install ? <span className="green fadeIn">Copied to clipboard.</span> : null}</p>
+          <p>{this.state.file_install ? <span className="green fadeIn">Copied to clipboard.</span> : null}</p>
         </div>
 
-        <p>3. And <a href="https://github.com/mendersoftware/mender-demo-artifact/archive/master.zip">download the demo application</a>.</p>
+        <p>3. And <a href="https://dgsbl4vditpls.cloudfront.net/mender-demo-artifact.mender">download the demo application</a>.</p>
 
 
         <h3>Edit the contents</h3>
-        <p>4. Extract the <span className="code">mender-demo-artifact-master</span> file you just downloaded, and cd to the extracted folder so you can see the <i>index.html</i> file within.</p>
+        <p>4. Take the <span className="code">mender-demo-artifact-master</span> Artifact you just downloaded, and extract its contents and enclosed <span className="code">.tar</span> files until you can see the <i>index.html</i> file within.</p>
         <p>5. Replace the contents of <i>index.html</i> with a simple string (e.g. &quot;Hello world&quot;), so you will be able to easily see the change when the webpage content is updated.</p>
 
-        <p>6. Now, you can create a new version of the demo webserver application with this modified <i>index.html</i> file. Generate a new Artifact by copying & pasting:</p>
+        <p>6. Now, you can create a new version of the demo webserver application with this modified <i>index.html</i> file. Generate a new Artifact by copying & pasting the following snippet.</p>
+        <p>NOTE: You should replace the <span className="code">generic_x86</span> device type with the type you require.</p>
         <div>
           <div className="code">
             <CopyToClipboard text={generate} onCopy={() => this._copied('generate')}>
@@ -90,8 +91,8 @@ export default class BuildDemoArtifact extends React.Component {
           <p>{this.state.generate ? <span className="green fadeIn">Copied to clipboard.</span> : null}</p>
         </div>
 
-        <p>You should now have a new Artifact file called <span className="code">demo-webserver-2.0.mender</span>!</p>
-        <p>If you upload this Artifact to the Mender server, it will create a new Release. You can then deploy this &quot;2.0&quot; Release of the webserver demo to your device, and when it has updated successfully you should see the page content will have been replaced with the &quot;Hello world&quot; string you modified.
+        <p>You should now have a new Artifact file called <span className="code">demo-webserver-updated.mender</span>!</p>
+        <p>If you upload this Artifact to the Mender server, it will create a new Release. You can then deploy this new Release of the webserver demo to your device, and when it has updated successfully you should see the page content will have been replaced with the &quot;Hello world&quot; string you modified.
         </p>
        
       </div>
