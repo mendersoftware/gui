9261cd2c686ed3c4410249a86227364fc06c9245
Manuel Zedel <manuel.zedel@northern.tech>
2019-06-08 22:52:52 +0200

    ensured docs & hub links open in new tabs
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/helptips/onboardingcompletetip.js
+++ src/js/components/helptips/onboardingcompletetip.js
@@ -33,6 +33,7 @@ export default class OnboardingCompleteTip extends React.Component {
   render() {
     const { loading, targetUrl } = this.state;
     const url = targetUrl ? targetUrl : this.props.targetUrl;
+    const currentLocation = encodeURIComponent(window.location);
 
     return (
       <div className="onboard-tip" style={{ left: '50%', top: '50%' }}>
@@ -62,8 +63,13 @@ export default class OnboardingCompleteTip extends React.Component {
           <p>You&apos;ve now got a good foundation in how to use Mender. Look for more help hints in the UI as you go along.</p>
           What next?
           <div>
-            <a href="https://docs.mender.io/2.0/getting-started/deploy-to-physical-devices#prepare-the-disk-image">Learn about full-image updates</a> or{' '}
-            <a href="https://hub.mender.io/c/update-modules">how to create other kinds of application updates.</a>
+            <a href="https://docs.mender.io/2.0/getting-started/deploy-to-physical-devices#prepare-the-disk-image" target="_blank">
+              Learn about full-image updates
+            </a>{' '}
+            or{' '}
+            <a href="https://hub.mender.io/c/update-modules" target="_blank">
+              how to create other kinds of application updates.
+            </a>
           </div>
           <div className="flexbox">
             <div style={{ flexGrow: 1 }} />
