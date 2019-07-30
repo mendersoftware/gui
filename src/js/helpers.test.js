6e9b718b0211430e5c535cb2d11176d56f04acf2
Manuel Zedel <manuel.zedel@northern.tech>
2019-07-30 15:48:58 +0200

    lint fixes
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/helpers.test.js
+++ src/js/helpers.test.js
@@ -2,29 +2,29 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 // TODO: test ALL of the following!
-import {
-  fullyDecodeURI,
-  isEncoded,
-  statusToPercentage,
-  decodeSessionToken,
-  isEmpty,
-  preformatWithRequestID,
-  versionCompare,
-  deepCompare,
-  hashString,
-  formatTime,
-  formatPublicKey,
-  intersection,
-  customSort,
-  mapDeviceAttributes,
-  getFormattedSize,
-  timeoutPromise,
-  FileSize,
-  collectAddressesFrom,
-  getDemoDeviceAddress,
-  detectOsIdentifier,
-  findLocalIpAddress
-} from './helpers';
+// [
+//   fullyDecodeURI,
+//   isEncoded,
+//   statusToPercentage,
+//   decodeSessionToken,
+//   isEmpty,
+//   preformatWithRequestID,
+//   versionCompare,
+//   deepCompare,
+//   hashString,
+//   formatTime,
+//   formatPublicKey,
+//   intersection,
+//   customSort,
+//   mapDeviceAttributes,
+//   getFormattedSize,
+//   timeoutPromise,
+//   collectAddressesFrom,
+//   getDemoDeviceAddress,
+//   detectOsIdentifier,
+//   findLocalIpAddress
+// ];
+import { FileSize } from './helpers';
 
 describe('ExpandArtifact', () => {
   it('renders correctly', () => {
