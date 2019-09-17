dd9a5a708d3d14da1a2b4d2df7986f6d41c9e7e2
Manuel Zedel <manuel.zedel@northern.tech>
2019-09-17 13:22:44 +0200

    fixed failing tests to work with updated mui version
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/dashboard/widgets/redirectionwidget.test.js
+++ src/js/components/dashboard/widgets/redirectionwidget.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { createMount } from '@material-ui/core/test-utils';
 import RedirectionWidget from './redirectionwidget';
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <MemoryRouter>
-        <RedirectionWidget target="testlocation" buttonContent={<div />} />
-      </MemoryRouter>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  const tree = createMount()(
+    <MemoryRouter>
+      <RedirectionWidget target="testlocation" buttonContent={<div />} />
+    </MemoryRouter>
+  );
+  expect(tree.html()).toMatchSnapshot();
 });
--- src/js/components/devices/devices.test.js
+++ src/js/components/devices/devices.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { createMount } from '@material-ui/core/test-utils';
 import Devices from './devices';
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <MemoryRouter>
-        <Devices />
-      </MemoryRouter>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  const context = {
+    childContextTypes: {
+      router: () => {}
+    },
+    context: {
+      router: {
+        route: {
+          location: {
+            hash: '',
+            pathname: '',
+            search: '',
+            state: ''
+          },
+          match: { params: {}, isExact: false, path: '', url: '' }
+        }
+      }
+    }
+  };
+  const tree = createMount()(
+    <MemoryRouter>
+      <Devices />
+    </MemoryRouter>,
+    context
+  );
+  expect(tree.html()).toMatchSnapshot();
 });
