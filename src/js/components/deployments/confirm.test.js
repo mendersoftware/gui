a0bf9f68b125f7b39c1ca4eee84f65017ca77130
Manuel Zedel <manuel.zedel@northern.tech>
2019-09-24 16:41:07 +0200

    preliminarily fixed existing tests
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/confirm.test.js
+++ src/js/components/deployments/confirm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import ConfirmAbort from './confirmabort';
+import Confirm from './confirm';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Confirm type="abort" />).toJSON();
