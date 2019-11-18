ad097cfa07db83206cd58ff755e533637eea5853
Manuel Zedel <manuel.zedel@northern.tech>
2019-11-18 14:12:47 +0100

    migrated release actions & storage to redux
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/actions/releaseActions.js
+++ src/js/actions/releaseActions.js
@@ -1,118 +1,120 @@
 import ArtifactsApi from '../api/artifacts-api';
+import AppActions from '../actions/app-actions';
 import * as ReleaseConstants from '../constants/releaseConstants';
+
+import { customSort, preformatWithRequestID } from '../helpers';
 
 const apiUrl = '/api/management/v1';
 const deploymentsApiUrl = `${apiUrl}/deployments`;
 
-// function _uploadArtifact(artifact) {
-//   if (artifact.id) {
-//     _artifactsRepo[_artifactsRepo.findIndex(item => item.id === artifact.id)] = artifact;
-//   } else {
-//     artifact.id = _artifactsRepo.length + 1;
-//     _artifactsRepo.push(artifact);
-//   }
-// }
+const flattenRelease = release => {
+  release.device_types_compatible = release.Artifacts.reduce((accu, item) => [...accu, ...item.device_types_compatible], []);
+  release.Artifacts.sort(customSort(1, 'modified'));
+  return release;
+};
 
-// function _uploadProgress(bool) {
-//   _uploadInProgress = bool;
-// }
-
-// const removeArtifact = id => {
-//   const index = _artifactsRepo.findIndex(item => item.id === id);
-//   const name = _artifactsRepo[index].name;
-//   _artifactsRepo.splice(index, 1);
-//   const releaseIndex = _releasesRepo.findIndex(item => item.Name === name);
-//   const releaseArtifactIndex = _releasesRepo[releaseIndex].Artifacts.findIndex(item => item.id === id);
-//   _releasesRepo[releaseIndex].Artifacts.splice(releaseArtifactIndex, 1);
-//   if (!_releasesRepo[releaseIndex].Artifacts.length) {
-//     _releasesRepo.splice(releaseIndex, 1);
-//   }
-// };
-
-// function setArtifactUrl(id, url) {
-//   const artifact = AppStore.getSoftwareArtifact('id', id);
-//   artifact.url = url;
-// }
-
-// function setReleases(releases) {
-//   if (releases) {
-//     _releasesRepo = releases;
-//   }
-//   _releasesRepo.sort(customSort(1, 'name'));
-// }
-// function setArtifacts(artifacts) {
-//   if (artifacts) {
-//     _artifactsRepo = artifacts;
-//   }
-//   _artifactsRepo.sort(customSort(1, 'modified'));
-// }
-
-// function _collateArtifacts() {
-//   var newArray = [];
-//   _artifactsRepo.forEach(repo => {
-//     var x = newArray.findIndex(item => item.name === repo.name);
-//     if (x > -1) {
-//       newArray[x].device_types_compatible = newArray[x].device_types_compatible.concat(
-//         repo.device_types_compatible.filter(item => {
-//           return newArray[x].device_types_compatible.indexOf(item) < 0;
-//         })
-//       );
-//     } else {
-//       newArray.push(repo);
-//     }
-//   });
-//   return newArray;
-// }
-
-// // discover devices with artifacts (in 'array' parameter) installed
-// function discoverDevices(array) {
-//   var unique = {};
-//   _alldevices.forEach(device => {
-//     if (typeof unique[device.artifact_name] == 'undefined') {
-//       unique[device.artifact_name] = 0;
-//     }
-//     unique[device.artifact_name]++;
-//   });
-
-//   if (array.length) {
-//     for (var val in unique) {
-//       const idx = array.findIndex(item => item.name === val);
-//       if (idx > -1) {
-//         array[idx]['devices'] = unique[val];
-//       }
-//     }
-//   }
-//   return array;
-// }
-
-/* Artifacts */
-export const getArtifacts = () => dispatch =>
-  ArtifactsApi.get(`${deploymentsApiUrl}/artifacts`).then(artifacts => dispatch({ type: ReleaseConstants.RECEIVE_ARTIFACTS, artifacts }));
-
-export const getArtifactUrl = id => dispatch =>
-  ArtifactsApi.get(`${deploymentsApiUrl}/artifacts/${id}/download`).then(response =>
-    dispatch({ type: ReleaseConstants.ARTIFACTS_SET_ARTIFACT_URL, id, url: response.uri })
+const findArtifactIndexInRelease = (releases, id) =>
+  Object.values(releases).reduce(
+    (accu, item) => {
+      let index = item.Artifacts.findIndex(releaseArtifact => releaseArtifact.id === id);
+      if (index > -1) {
+        accu = { release: item, index };
+      }
+      return accu;
+    },
+    { release: null, index: -1 }
   );
 
-export const uploadArtifact = (meta, file, progress) => dispatch => {
+/* Artifacts */
+export const getArtifactUrl = id => (dispatch, getState) =>
+  ArtifactsApi.get(`${deploymentsApiUrl}/artifacts/${id}/download`).then(response => {
+    const state = getState();
+    let { release, index } = findArtifactIndexInRelease(state.releases.byId, id);
+    if (!release || index === -1) {
+      return getReleases()(dispatch);
+    }
+    release.Artifacts[index].url = response.uri;
+    return dispatch({ type: ReleaseConstants.ARTIFACTS_SET_ARTIFACT_URL, release });
+  });
+
+export const uploadArtifact = (meta, file) => (dispatch, getState) => {
   var formData = new FormData();
   formData.append('size', file.size);
   formData.append('description', meta.description);
   formData.append('artifact', file);
+  var progress = percent => dispatch({ type: ReleaseConstants.UPLOAD_PROGRESS, uploadProgress: percent });
   return Promise.all([
-    dispatch({ type: ReleaseConstants.UPLOAD_PROGRESS, inprogress: true }),
+    AppActions.setSnackbar('Uploading artifact'),
+    dispatch({ type: ReleaseConstants.UPLOAD_PROGRESS, inprogress: true, uploadProgress: 0 }),
     ArtifactsApi.postFormData(`${deploymentsApiUrl}/artifacts`, formData, e => progress(e.percent))
   ])
-    .then(() => dispatch({ type: ReleaseConstants.UPLOAD_ARTIFACT, artifact: file }))
-    .finally(() => dispatch({ type: ReleaseConstants.UPLOAD_PROGRESS, inprogress: false }));
+    .then(() => Promise.all([selectArtifact(file)(dispatch, getState), AppActions.setSnackbar('Upload successful', 5000)]))
+    .catch(err => {
+      try {
+        var errMsg = err.res.body.error || '';
+        AppActions.setSnackbar(preformatWithRequestID(err.res, `Artifact couldn't be uploaded. ${errMsg}`), null, 'Copy to clipboard');
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .finally(() => dispatch({ type: ReleaseConstants.UPLOAD_PROGRESS, inprogress: false, uploadProgress: 0 }));
 };
 
-export const editArtifact = (id, body) => dispatch =>
-  ArtifactsApi.putJSON(`${deploymentsApiUrl}/artifacts/${id}`, body).then(() => dispatch({ type: ReleaseConstants.UPDATED_ARTIFACT, id, update: body }));
+export const editArtifact = (id, body) => (dispatch, getState) =>
+  ArtifactsApi.putJSON(`${deploymentsApiUrl}/artifacts/${id}`, body).then(() => {
+    const state = getState();
+    let { release, index } = findArtifactIndexInRelease(state.releases.byId, id);
+    if (!release || index === -1) {
+      return getReleases()(dispatch);
+    }
+    release.Artifacts[index].description = body.description;
+    return dispatch({ type: ReleaseConstants.UPDATED_ARTIFACT, release });
+  });
 
-export const removeArtifact = id => dispatch =>
-  ArtifactsApi.delete(`${deploymentsApiUrl}/artifacts/${id}`).then(() => dispatch({ type: ReleaseConstants.ARTIFACTS_REMOVED_ARTIFACT, id }));
+export const removeArtifact = id => (dispatch, getState) =>
+  ArtifactsApi.delete(`${deploymentsApiUrl}/artifacts/${id}`).then(() => {
+    const state = getState();
+    let { release, index } = findArtifactIndexInRelease(state.releases.byId, id);
+    release.Artifacts.splice(index, 1);
+    if (!release.Artifacts.length) {
+      return dispatch({ type: ReleaseConstants.RELEASE_REMOVED, release: release.Name });
+    }
+    return dispatch({ type: ReleaseConstants.ARTIFACTS_REMOVED_ARTIFACT, release });
+  });
+
+export const selectArtifact = artifact => (dispatch, getState) => {
+  if (!artifact) {
+    return dispatch({ type: ReleaseConstants.SELECTED_ARTIFACT, artifact });
+  }
+  let artifactName = artifact;
+  if (artifact && artifact.hasOwnProperty('id')) {
+    artifactName = artifact.id;
+  }
+  const state = getState();
+  const release = Object.values(state.releases.byId).find(item => item.Artifacts.find(releaseArtifact => releaseArtifact.id === artifactName));
+  if (release) {
+    const selectedArtifact = release.Artifacts.find(releaseArtifact => releaseArtifact.id === artifactName);
+    let tasks = [dispatch({ type: ReleaseConstants.SELECTED_ARTIFACT, artifact: selectedArtifact })];
+    if (release.Name !== state.releases.selectedRelease) {
+      tasks.push(dispatch({ type: ReleaseConstants.SELECTED_RELEASE, release: release.Name }));
+    }
+    return Promise.all(tasks);
+  }
+};
+
+export const selectRelease = release => dispatch => dispatch({ type: ReleaseConstants.SELECTED_RELEASE, release: release ? release.Name || release : null });
+
+export const showRemoveArtifactDialog = showRemoveDialog => dispatch => dispatch({ type: ReleaseConstants.SHOW_REMOVE_DIALOG, showRemoveDialog });
 
 /* Releases */
 export const getReleases = () => dispatch =>
-  ArtifactsApi.get(`${deploymentsApiUrl}/deployments/releases`).then(releases => dispatch({ type: ReleaseConstants.RECEIVE_RELEASES, releases }));
+  ArtifactsApi.get(`${deploymentsApiUrl}/deployments/releases`).then(releases => {
+    const flatReleases = releases.sort(customSort(1, 'Name')).reduce((accu, release) => {
+      accu[release.Name] = flattenRelease(release);
+      return accu;
+    }, {});
+    return Promise.all([
+      dispatch({ type: ReleaseConstants.RECEIVE_RELEASES, releases: flatReleases }),
+      AppActions.setOnboardingArtifactIncluded(!!releases.length)
+    ]);
+  });
