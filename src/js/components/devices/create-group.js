0f6c5ff2b2a1d774cba50d3a0b5cdf7f48106a56
Manuel Zedel <manuel.zedel@northern.tech>
2020-02-04 10:46:36 +0100

    lint fix
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/create-group.js
+++ src/js/components/devices/create-group.js
@@ -253,8 +253,7 @@ export default class CreateGroup extends React.Component {
           {self.state.showWarning ? (
             <div className="help-message">
               <h2>
-                <ErrorOutlineIcon style={{ marginRight: '4px', verticalAlign: 'sub' }} />
-                {" You're creating a new group"}
+                <ErrorOutlineIcon style={{ marginRight: '4px', verticalAlign: 'sub' }} /> You&apos;re creating a new group
               </h2>
               <p>
                 Just a heads-up: if a device is already in another group, it will be removed from that group and moved to the new one. A device can only belong
