9e4775a07caa2f434ed0d687e3411add7d5168ec
Manuel Zedel <manuel.zedel@northern.tech>
2020-02-06 13:52:32 +0100

    slightly cleaned up state reflecting static nature of artifact refresh time
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/artifacts/artifacts.js
+++ src/js/components/artifacts/artifacts.js
@@ -25,11 +25,12 @@ import ReleasesList from './releaseslist';
 import RemoveArtifactDialog from './dialogs/removeartifact';
 import AddArtifactDialog from './dialogs/addartifact';
 
+const refreshArtifactsLength = 30000; //60000
+
 export class Artifacts extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      refreshArtifactsLength: 30000, //60000,
       doneLoading: false,
       remove: false,
       showCreateArtifactDialog: false
@@ -62,7 +63,7 @@ export class Artifacts extends React.Component {
         }
       });
     }
-    self.artifactTimer = setInterval(() => self._getReleases(), self.state.refreshArtifactsLength);
+    self.artifactTimer = setInterval(() => self._getReleases(), refreshArtifactsLength);
   }
   componentWillUnmount() {
     clearInterval(this.artifactTimer);
