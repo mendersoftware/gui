1bf477c5cca665496e506c017fe1b6c8fe938874
Manuel Zedel <manuel.zedel@northern.tech>
2020-02-07 10:35:29 +0100

    prevented test runs from collecting coverage on regular file watch
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ let argv = process.argv.slice(2);
 
 // Watch unless on CI or explicitly running all tests
 if (!process.env.CI && argv.indexOf('--watchAll') === -1) {
-  argv.push('--watchAll');
+  argv.push('--watch');
 }
 
 const jest = require('jest');
