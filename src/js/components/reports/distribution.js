0abfea84cb6b5a21b50d1e8b98b8e68dd66d7566
Manuel Zedel <manuel.zedel@northern.tech>
2020-03-09 11:21:07 +0100

    restructured report graphs location
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ src/js/components/reports/distribution.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { IconButton, Paper } from '@material-ui/core';
+import { Clear as ClearIcon } from '@material-ui/icons';
+import { VictoryPie, VictoryTooltip } from 'victory';
+
+import Loader from '../common/loader';
+
+export default class DistributionReport extends React.Component {
+  constructor(props, state) {
+    super(props, state);
+    this.state = {
+      distribution: {}
+    };
+  }
+
+  componentDidMount() {
+    const { attribute, devices, group, groups } = this.props;
+    const relevantDevices = group && groups[group] ? groups[group].deviceIds.map(id => devices[id]) : Object.values(devices);
+    const distribution = relevantDevices.reduce((accu, item) => {
+      if (!item.attributes) return accu;
+      if (!accu[item.attributes[attribute]]) {
+        accu[item.attributes[attribute]] = 0;
+      }
+      accu[item.attributes[attribute]] = accu[item.attributes[attribute]] + 1;
+      return accu;
+    }, {});
+    this.setState({ distribution });
+  }
+
+  render() {
+    const { attribute, group, onClick } = this.props;
+    const data = Object.entries(this.state.distribution).reduce((accu, [key, value]) => [...accu, { x: key, y: value }], []);
+    return (
+      <Paper className="flexbox column centered space-between margin-right margin-bottom" elevation={2} style={{ minWidth: 380, width: 380 }}>
+        {data.length ? (
+          <>
+            <IconButton onClick={onClick} style={{ alignSelf: 'flex-end' }}>
+              <ClearIcon fontSize="small" />
+            </IconButton>
+            <VictoryPie
+              colorScale="qualitative"
+              data={data}
+              height={220}
+              innerRadius={30}
+              labelComponent={<VictoryTooltip flyoutWidth={150} constrainToVisibleArea />}
+              padding={0}
+              // radius={20}
+              style={{
+                parent: { width: 220, height: 220, display: 'flex', alignSelf: 'center' }
+              }}
+              width={220}
+              events={[
+                {
+                  target: 'data',
+                  eventHandlers: {
+                    onClick: (evt, clickedProps) => {
+                      console.log(clickedProps);
+                    }
+                  }
+                }
+              ]}
+            />
+            <h4 className="flexbox centered">{`${attribute} in ${group || 'All devices'}`}</h4>
+          </>
+        ) : (
+          <Loader show={true} />
+        )}
+      </Paper>
+    );
+  }
+}
