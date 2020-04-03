ee4e272ff2c587a8e2337e193d9f9f1afe81610b
Manuel Zedel <manuel.zedel@northern.tech>
2020-04-03 11:08:22 +0200

    limited filtering to inventory attributes + device id
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/filters.js
+++ src/js/components/devices/filters.js
@@ -134,7 +134,7 @@ const actionCreators = {
 
 const mapStateToProps = (state, ownProps) => {
   const plan = state.users.organization ? state.users.organization.plan : 'os';
-  let attributes = state.devices.filteringAttributes.identityAttributes.map(item => ({ key: item, value: item, scope: 'identity' }));
+  let attributes = []; // state.devices.filteringAttributes.identityAttributes.map(item => ({ key: item, value: item, scope: 'identity' }));
   if (!ownProps.identityOnly) {
     attributes = [...attributes, ...state.devices.filteringAttributes.inventoryAttributes.map(item => ({ key: item, value: item, scope: 'inventory' }))];
   }
