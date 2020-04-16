bc990c9a04d1e72356b219fc5e132abb32327e4b
Manuel Zedel <manuel.zedel@northern.tech>
2020-04-16 17:31:09 +0200

    removed now unused hint dependency aligning on mui tooltips
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import './../hint.css';
 import './../less/main.less';
 import theme from './themes/mender-theme';
 import routes from './config/routes';
