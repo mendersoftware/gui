59747635494a436335ae86b7091908d5e8c1b164
Manuel Zedel <manuel.zedel@northern.tech>
2020-04-17 16:07:02 +0200

    switched scheduled deployments to new deployment list implementation
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/scheduleddeployments.js
+++ src/js/components/deployments/scheduleddeployments.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-
-import { Tab, Tabs } from '@material-ui/core';
-
-import DeploymentsList, { defaultHeaders } from './deploymentslist';
-
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { Button } from '@material-ui/core';
+import { CalendarToday as CalendarTodayIcon, List as ListIcon } from '@material-ui/icons';
+
+import DeploymentsList, { defaultHeaders } from './deploymentslist';
+import { DeploymentDeviceCount, DeploymentEndTime, DeploymentPhases, DeploymentStartTime, DeploymentWindows } from './deploymentitem';
+
 const localizer = momentLocalizer(moment);
+
+const headers = [
+  ...defaultHeaders.slice(0, 2),
+  { title: 'Start time', renderer: DeploymentStartTime, props: { direction: 'up' } },
+  { title: `End time`, renderer: DeploymentEndTime },
+  { title: '# devices', class: 'align-right', renderer: DeploymentDeviceCount },
+  { title: 'Scheduled windows', renderer: DeploymentWindows },
+  { title: 'Phases', renderer: DeploymentPhases }
+];
 
 export class Scheduled extends React.Component {
   constructor(props, context) {
@@ -22,13 +32,16 @@ export class Scheduled extends React.Component {
     const self = this;
     const { tabIndex } = self.state;
     return (
-      <div>
-        <Tabs className="margin-bottom-large" value={tabIndex} onChange={(e, tabIndex) => self.setState({ tabIndex })}>
-          <Tab label="List view" value="list" />
-          <Tab label="Calendar" value="calendar" />
-        </Tabs>
-
-        {tabIndex === 'list' && <DeploymentsList headers={defaultHeaders} items={[]} {...self.props} />}
+      <div className="fadeIn margin-left">
+        <div className="margin-large margin-left-small">
+          <Button color="secondary" startIcon={<ListIcon />} style={{ textTransform: 'none' }} onClick={() => self.setState({ tabIndex: 'list' })}>
+            List view
+          </Button>
+          <Button color="primary" startIcon={<CalendarTodayIcon />} style={{ textTransform: 'none' }} onClick={() => self.setState({ tabIndex: 'calendar' })}>
+            Calendar
+          </Button>
+        </div>
+        {tabIndex === 'list' && <DeploymentsList headers={headers} {...self.props} type="scheduled" />}
         {tabIndex === 'calendar' && <Calendar localizer={localizer} events={[]} startAccessor="start" endAccessor="end" style={{ height: 500 }} />}
       </div>
     );
