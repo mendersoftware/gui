6f52db19ec010550ab148b7eae487565aee1f31e
Manuel Zedel <manuel.zedel@northern.tech>
2020-05-06 23:05:32 +0200

    further aligned roles page with design
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/settings/roles.js
+++ src/js/components/settings/roles.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 // material ui
-import {
-  Button,
-  Checkbox,
-  Chip,
-  FormControl,
-  FormControlLabel,
-  Input,
-  InputLabel,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField
-} from '@material-ui/core';
+import { Button, Checkbox, Chip, Collapse, FormControlLabel, Table, TableBody, TableCell, TableHead, TableRow, TextField } from '@material-ui/core';
 import { Add as AddIcon } from '@material-ui/icons';
 
 import { setSnackbar } from '../../actions/appActions';
@@ -25,12 +11,24 @@ export class RoleManagement extends React.Component {
     super(props, context);
     this.state = {
       adding: false,
-      groups: props.groups.map(group => ({ name: group, selected: false }))
+      groups: props.groups.map(group => ({ name: group, selected: false })),
+      description: undefined,
+      name: undefined
     };
   }
 
   onSubmit() {
-    this.setState({ adding: false });
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.setState({
+      adding: false,
+      allowUserManagement: false,
+      description: undefined,
+      groups: this.props.groups.map(group => ({ name: group, selected: false })),
+      name: undefined
+    });
   }
 
   handleGroupSelection(selected, group) {
@@ -50,6 +48,7 @@ export class RoleManagement extends React.Component {
     const { roles } = self.props;
     return (
       <div>
+        <h2 style={{ marginLeft: 20 }}>Roles</h2>
         <Table className="margin-bottom">
           <TableHead>
             <TableRow>
@@ -72,55 +71,51 @@ export class RoleManagement extends React.Component {
             ))}
           </TableBody>
         </Table>
-        {adding ? (
+
+        {!adding && <Chip className="margin-top-small" color="primary" icon={<AddIcon />} label="Add a role" onClick={() => self.setState({ adding: true })} />}
+        <Collapse in={adding} className="margin-right-small filter-wrapper" classes={{ wrapperInner: 'margin-bottom-small margin-right' }}>
+          <h4 style={{ marginTop: 5 }}>Add a role</h4>
+          <TextField
+            label="Role name"
+            id="role-name"
+            value={name}
+            onChange={e => self.setState({ name: e.target.value })}
+            style={{ marginTop: 0, marginRight: 30 }}
+          />
+          <TextField
+            label="Description"
+            id="role-description"
+            value={description}
+            placeholder="-"
+            onChange={e => self.setState({ description: e.target.value })}
+            style={{ marginTop: 0, marginRight: 30 }}
+          />
           <div>
-            <h4>Add a role</h4>
-            <div>
-              <TextField className="margin-right-large" label="Role name" id="role-name" value={name} onChange={e => self.setState({ name: e.target.value })} />
-              <FormControl>
-                <InputLabel htmlFor="artifact-description">Description</InputLabel>
-                <Input id="role-description" type="text" value={description} onChange={e => self.setState({ description: e.target.value })} />
-              </FormControl>
-              <div>
-                <FormControlLabel
-                  control={<Checkbox color="secondary" onChange={(e, checked) => self.setState({ allowUserManagement: checked })} />}
-                  label="Allow to manage other users"
-                />
-              </div>
-              <div className="flexbox column">
-                <h4>Device group permission</h4>
-                {groups.map(group => (
-                  <FormControlLabel
-                    style={{ marginTop: 0, marginLeft: 0 }}
-                    key={group.name}
-                    control={<Checkbox color="secondary" checked={group.selected} onChange={(e, checked) => self.handleGroupSelection(checked, group)} />}
-                    label={group.name}
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="flexbox">
-              <Button
-                onClick={() =>
-                  self.setState({
-                    adding: false,
-                    allowUserManagement: false,
-                    description: '-',
-                    groups: self.props.groups.map(group => ({ name: group, selected: false })),
-                    name: ''
-                  })
-                }
-              >
-                Cancel
-              </Button>
-              <Button color="primary" variant="contained" onClick={() => self.onSubmit()}>
-                Submit
-              </Button>
-            </div>
+            <FormControlLabel
+              control={<Checkbox color="primary" onChange={(e, checked) => self.setState({ allowUserManagement: checked })} />}
+              label="Allow to manage other users"
+            />
           </div>
-        ) : (
-          <Chip className="margin-top-small" color="primary" icon={<AddIcon />} label="Add a role" onClick={() => self.setState({ adding: true })} />
-        )}
+          <div className="flexbox column margin-top-small">
+            <div>Device group permission</div>
+            {groups.map(group => (
+              <FormControlLabel
+                style={{ marginTop: 0, marginLeft: 0 }}
+                key={group.name}
+                control={<Checkbox color="primary" checked={group.selected} onChange={(e, checked) => self.handleGroupSelection(checked, group)} />}
+                label={group.name}
+              />
+            ))}
+          </div>
+          <div className="flexbox centered" style={{ justifyContent: 'flex-end' }}>
+            <Button onClick={() => self.onCancel()} style={{ marginRight: 15 }}>
+              Cancel
+            </Button>
+            <Button color="secondary" variant="contained" href={mailTrigger} target="_blank" onClick={() => self.onCancel()}>
+              Submit
+            </Button>
+          </div>
+        </Collapse>
       </div>
     );
   }
