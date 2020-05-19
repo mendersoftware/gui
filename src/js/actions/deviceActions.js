7283492630abc312ddb48df209a55ce966ccdf6a
Manuel Zedel <manuel.zedel@northern.tech>
2020-05-19 13:04:42 +0200

    aligned inventory retrieved timestamps with expected device object structure
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/actions/deviceActions.js
+++ src/js/actions/deviceActions.js
@@ -254,6 +254,9 @@ const reduceReceivedDevices = (devices, ids, state, status) =>
       } else {
         const attributes = mapDeviceAttributes(device.attributes);
         device.attributes = stateDevice ? { ...stateDevice.attributes, ...attributes } : attributes;
+        device.status = status ? status : device.status || device.attributes.status;
+        device.created_ts = device.attributes.created_ts ? device.attributes.created_ts : device.created_ts;
+        device.updated_ts = device.attributes.updated_ts ? device.attributes.updated_ts : device.updated_ts;
       }
       accu.devicesById[device.id] = { ...stateDevice, ...device };
       accu.ids.push(device.id);
@@ -341,6 +344,7 @@ export const getDeviceById = id => dispatch =>
   DevicesApi.get(`${inventoryApiUrl}/devices/${id}`)
     .then(res => {
       const device = { ...res.body, attributes: mapDeviceAttributes(res.body.attributes) };
+      delete device.updated_ts;
       dispatch({
         type: DeviceConstants.RECEIVE_DEVICE,
         device
