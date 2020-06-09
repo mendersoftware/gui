1e69bdf733b642840588573f62ebaf3823bd248e
Manuel Zedel <manuel.zedel@northern.tech>
2020-06-09 15:40:15 +0200

    added top level error tracking and basic handling instructions
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ src/js/errorboundary.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactGA from 'react-ga';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { error: null, info: null };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ error, info });
+    ReactGA.exception(error);
+  }
+
+  render() {
+    return this.state.error ? (
+      // this will get embedded in the main grid, so we have to simulate the three empty areas for logo, header and sidebar
+      <>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="flexbox centered">
+          <div>
+            <h2>An error occured.</h2>
+            <p>Try refreshing the page, or look below for more information on the error that occured.</p>
+            <details style={{ whiteSpace: 'pre-wrap' }}>
+              {this.state.error && this.state.error.toString()}
+              <br />
+              {this.state.info.componentStack}
+            </details>
+          </div>
+        </div>
+      </>
+    ) : (
+      this.props.children
+    );
+  }
+}
