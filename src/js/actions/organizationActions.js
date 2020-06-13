4382d20409fda7f85874cadb93d21b826eb3b0a6
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2020-06-13 11:56:22 +0200

    MEN-3616: sign-up form to create a trial account
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- src/js/actions/organizationActions.js
+++ src/js/actions/organizationActions.js
@@ -4,3 +4,4 @@ const apiUrl = '/api/management/v2';
 const tenantadmApiUrl = `${apiUrl}/tenantadm`;
 
 export const cancelRequest = (tenantId, reason) => () => Api.post(`${tenantadmApiUrl}/tenants/${tenantId}/cancel`, { reason: reason });
+export const createOrganizationTrial = data => () => Api.postForm(`${tenantadmApiUrl}/tenants/trial`, data);
