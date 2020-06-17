7697c6293373969a051a174f95821758aa74c9fb
Manuel Zedel <manuel.zedel@northern.tech>
2020-06-17 18:06:23 +0200

    added tests for separate signup sections
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ src/js/components/user-management/signup-steps/userdata-entry.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { undefineds } from '../../../../../tests/mockData';
+import UserDataEntry from './userdata-entry';
+
+describe('Login Component', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<UserDataEntry setSnackbar={jest.fn} onSubmit={jest.fn} />).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+  });
+});
