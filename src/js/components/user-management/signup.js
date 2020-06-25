2cecd7c20e25e6902e9b50c14f5c318c6fa51029
Manuel Zedel <manuel.zedel@northern.tech>
2020-06-25 14:58:31 +0200

    reintroduced extra log in step for oauth signups
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/user-management/signup.js
+++ src/js/components/user-management/signup.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-
 import Cookies from 'universal-cookie';
+
+import { Button } from '@material-ui/core';
 
 import { setSnackbar } from '../../actions/appActions';
 import { createOrganizationTrial } from '../../actions/organizationActions';
@@ -11,6 +12,7 @@ import { loginUser } from '../../actions/userActions';
 import Loader from '../common/loader';
 import UserDataEntry from './signup-steps/userdata-entry';
 import OrgDataEntry from './signup-steps/orgdata-entry';
+import { providers } from './login';
 
 export class Signup extends React.Component {
   constructor(props, context) {
@@ -58,8 +60,17 @@ export class Signup extends React.Component {
     };
     return self.props
       .createOrganizationTrial(signup)
-      .then(() => self.setState({ loading: false, redirectToReferrer: true }))
-      .finally(() => setTimeout(() => self.setState({ loading: false, step: 1 }), 3000));
+      .catch(() => self.setState({ step: 1 }))
+      .then(() => {
+        if (!oauthProvider) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => self.props.loginUser({ email, password }).catch(reject).then(resolve), 3000);
+          });
+        }
+        return Promise.resolve();
+      })
+      .then(() => self.setState({ step: 3, redirectToReferrer: !oauthProvider }))
+      .finally(() => self.setState({ loading: false }));
   }
 
   componentDidUpdate() {
@@ -71,7 +82,7 @@ export class Signup extends React.Component {
 
   render() {
     const self = this;
-    const { step, loading, redirectToReferrer } = this.state;
+    const { step, loading, oauthProvider, redirectToReferrer } = this.state;
     const { recaptchaSiteKey } = this.props;
     let from = { pathname: '/' };
     if (location && location.state && location.state.from.pathname !== '/ui/') {
@@ -80,6 +91,7 @@ export class Signup extends React.Component {
     if (redirectToReferrer) {
       return <Redirect to={from} />;
     }
+    const provider = providers.find(item => item.id.toLowerCase() === oauthProvider);
     return (
       <div className="full-screen">
         <div id="signup-box">
@@ -89,12 +101,32 @@ export class Signup extends React.Component {
             <>
               {step == 1 && <UserDataEntry setSnackbar={setSnackbar} onSubmit={formdata => self._handleStep1(formdata)} />}
               {step == 2 && <OrgDataEntry setSnackbar={setSnackbar} onSubmit={formdata => self._handleSignup(formdata)} recaptchaSiteKey={recaptchaSiteKey} />}
-              <div className="flexbox margin-top" style={{ color: 'rgba(0, 0, 0, 0.3)', justifyContent: 'center' }}>
-                Already have an account?{' '}
-                <Link style={{ marginLeft: '4px' }} to="/login">
-                  Log in
-                </Link>
-              </div>
+              {step == 3 && (
+                <div className="flexbox centered" style={{ minHeight: '50vh' }}>
+                  <h1>Sign up completed</h1>
+                  <h2 className="margin-bottom-large">
+                    Your account has been created,
+                    <br />
+                    you can now log in.
+                  </h2>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    href={`/api/management/v1/useradm/oauth2/${provider.id.toLowerCase()}`}
+                    startIcon={provider.icon}
+                  >
+                    {provider.id}
+                  </Button>
+                </div>
+              )}
+              {step !== 3 && (
+                <div className="flexbox margin-top" style={{ color: 'rgba(0, 0, 0, 0.3)', justifyContent: 'center' }}>
+                  Already have an account?{' '}
+                  <Link style={{ marginLeft: '4px' }} to="/login">
+                    Log in
+                  </Link>
+                </div>
+              )}
             </>
           )}
         </div>
