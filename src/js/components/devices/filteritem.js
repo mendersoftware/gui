1436dce4bb66a038c559188638ee6abb3ae70ac1
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2020-06-29 15:18:00 +0200

    Help message to explain the usage of filter operators
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- src/js/components/devices/filteritem.js
+++ src/js/components/devices/filteritem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // material ui
-import { IconButton, MenuItem, Select, TextField } from '@material-ui/core';
+import { IconButton, MenuItem, Select, TextField, FormHelperText } from '@material-ui/core';
 import { HighlightOff as HighlightOffIcon } from '@material-ui/icons';
 import { Autocomplete, createFilterOptions } from '@material-ui/lab';
 
@@ -19,6 +19,16 @@ const filterOptionsByPlan = {
   os: { $eq: { title: 'equals' } },
   professional: DEVICE_FILTERING_OPTIONS,
   enterprise: DEVICE_FILTERING_OPTIONS
+};
+
+const operatorHelpMessages = {
+  $gt: 'The "greater than" operator can work both on numbers and strings. In the latter case, the operator applies the lexicographical order to the value strings.',
+  $gte: 'The "greater than or equal" operator can work both on numbers and strings. In the latter case, the operator applies the lexicographical order to the value strings.',
+  $lt: 'The "lesser than" operator can work both on numbers and strings. In the latter case, the operator applies the lexicographical order to the value strings.',
+  $lte: 'The "lesser than or equal" operator can work both on numbers and strings. In the latter case, the operator applies the lexicographical order to the value strings.',
+  $in: 'The "in" operator accepts a list of comma-separated values. It matches if the selected field is equal to one of the specified values.',
+  $nin: 'The "not in" operator accepts a list of comma-separated values. It matches if the selected field\'s value is not equal to any of the specified options.',
+  $exists: 'The "exists" operator matches if the selected field\'s value has a value.'
 };
 
 const defaultScope = 'inventory';
@@ -108,64 +118,74 @@ export default class FilterItem extends React.Component {
     const { attributes, filters, loading, plan } = self.props;
     const { key, operator, reset, value } = self.state;
     const filterOptions = plan ? filterOptionsByPlan[plan] : DEVICE_FILTERING_OPTIONS;
+    const operatorHelpMessage = operatorHelpMessages[operator] || '';
     return (
-      <div className="flexbox" style={{ alignItems: 'center' }}>
-        <Autocomplete
-          autoComplete
-          autoHighlight
-          autoSelect
-          freeSolo
-          filterSelectedOptions
-          filterOptions={(options, params) => {
-            const filtered = filter(options, params);
-            if (filtered.length !== 1 && params.inputValue !== '') {
-              filtered.push({
-                inputValue: params.inputValue,
-                key: 'custom',
-                value: `Use "${params.inputValue}"`,
-                category: 'custom',
-                priority: 99
-              });
-            }
-            return filtered;
-          }}
-          groupBy={option => option.category}
-          getOptionLabel={option => option.value || option.key || option}
-          id="filter-selection"
-          includeInputInList={true}
-          onChange={(e, changedValue) => {
-            if (changedValue && changedValue.inputValue) {
-              // only circumvent updateFilterKey if we deal with a custom attribute - those will be treated as inventory attributes
-              return self.setState({ key: changedValue.inputValue, scope: defaultScope }, () => self.notifyFilterUpdate());
-            }
-            self.updateFilterKey(changedValue && changedValue.key ? changedValue.key : changedValue);
-          }}
-          options={filters.sort((a, b) => a.priority - b.priority)}
-          renderInput={params => <TextField {...params} label="Attribute" style={textFieldStyle} />}
-          key={reset}
-          value={getFilterLabelByKey(key, attributes)}
-        />
-        <Select className="margin-left-small margin-right-small" onChange={event => self.updateFilterOperator(event.target.value)} value={operator}>
-          {Object.entries(filterOptions).map(([optionKey, option]) => (
-            <MenuItem key={optionKey} value={optionKey}>
-              {option.title}
-            </MenuItem>
-          ))}
-        </Select>
-        <TextField
-          label="Value"
-          value={value}
-          onChange={e => self.updateFilterValue(e.target.value)}
-          InputLabelProps={{ shrink: !!value }}
-          style={textFieldStyle}
-        />
-        {!!key && (
-          <IconButton className="margin-left" onClick={() => self._removeFilter()} size="small">
-            <HighlightOffIcon />
-          </IconButton>
+      <>
+        <div className="flexbox" style={{ alignItems: 'center' }}>
+          <Autocomplete
+            autoComplete
+            autoHighlight
+            autoSelect
+            freeSolo
+            filterSelectedOptions
+            filterOptions={(options, params) => {
+              const filtered = filter(options, params);
+              if (filtered.length !== 1 && params.inputValue !== '') {
+                filtered.push({
+                  inputValue: params.inputValue,
+                  key: 'custom',
+                  value: `Use "${params.inputValue}"`,
+                  category: 'custom',
+                  priority: 99
+                });
+              }
+              return filtered;
+            }}
+            groupBy={option => option.category}
+            getOptionLabel={option => option.value || option.key || option}
+            id="filter-selection"
+            includeInputInList={true}
+            onChange={(e, changedValue) => {
+              if (changedValue && changedValue.inputValue) {
+                // only circumvent updateFilterKey if we deal with a custom attribute - those will be treated as inventory attributes
+                return self.setState({ key: changedValue.inputValue, scope: defaultScope }, () => self.notifyFilterUpdate());
+              }
+              self.updateFilterKey(changedValue && changedValue.key ? changedValue.key : changedValue);
+            }}
+            options={filters.sort((a, b) => a.priority - b.priority)}
+            renderInput={params => <TextField {...params} label="Attribute" style={textFieldStyle} />}
+            key={reset}
+            value={getFilterLabelByKey(key, attributes)}
+          />
+          <Select className="margin-left-small margin-right-small" onChange={event => self.updateFilterOperator(event.target.value)} value={operator}>
+            {Object.entries(filterOptions).map(([optionKey, option]) => (
+              <MenuItem key={optionKey} value={optionKey}>
+                {option.title}
+              </MenuItem>
+            ))}
+          </Select>
+          <TextField
+            label="Value"
+            value={value}
+            onChange={e => self.updateFilterValue(e.target.value)}
+            InputLabelProps={{ shrink: !!value }}
+            style={textFieldStyle}
+          />
+          {!!key && (
+            <IconButton className="margin-left" onClick={() => self._removeFilter()} size="small">
+              <HighlightOffIcon />
+            </IconButton>
+          )}
+          <Loader show={loading} />
+        </div>
+        {operatorHelpMessage && (
+          <div className="margin-bottom-small">
+            <FormHelperText>
+              {operatorHelpMessage}
+            </FormHelperText>
+          </div>
         )}
-        <Loader show={loading} />
-      </div>
+      </>
     );
   }
 }
