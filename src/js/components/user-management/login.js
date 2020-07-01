0ed224e606086ab54db17c042b050a8713843375
Manuel Zedel <manuel.zedel@northern.tech>
2020-07-01 23:31:42 +0200

    positioned public routes across the entire app viewport ensuring centering & scrollability
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/user-management/login.js
+++ src/js/components/user-management/login.js
@@ -94,73 +94,71 @@ export class Login extends React.Component {
       };
     }
     return (
-      <div className="full-screen">
-        <div id="login-box">
-          <h3>Log in</h3>
-          <img src="assets/img/loginlogo.png" alt="mender-logo" className="margin-bottom-small" />
+      <div className="flexbox column" id="login-box">
+        <h3>Log in</h3>
+        <img src="assets/img/loginlogo.png" alt="mender-logo" className="margin-bottom-small" />
 
-          {isHosted && (
-            <>
-              <div className="flexbox centered margin-bottom">Log in with:</div>
-              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                {OAuth2Providers.map(provider => (
-                  <Button
-                    className="oauth-provider"
-                    variant="contained"
-                    key={provider.id}
-                    href={`/api/management/v1/useradm/oauth2/${provider.id}`}
-                    startIcon={provider.icon}
-                  >
-                    {provider.name}
-                  </Button>
-                ))}
-              </div>
-              <h4 className="dashboard-header margin-top-large" style={{ display: 'flex', justifyContent: 'center' }}>
-                <span style={{ padding: 15, top: -24 }}>or your email address</span>
-              </h4>
-            </>
-          )}
-
-          <Form showButtons={true} buttonColor="primary" onSubmit={formdata => this._handleLogin(formdata)} submitLabel="Log in" submitButtonId="login_button">
-            <TextInput hint="Your email" label="Your email" id="email" required={true} validations="isLength:1,isEmail" />
-            <PasswordInput className="margin-bottom-small" id="password" label="Password" required={true} />
-            {has2FA ? (
-              <TextInput
-                hint="Two Factor Authentication Code"
-                label="Two Factor Authentication Code"
-                id="token2fa"
-                validations="isLength:6,isNumeric"
-                required={true}
-                setControlRef={re => (this.twoFARef = re)}
-              />
-            ) : (
-              <div />
-            )}
-            <FormCheckbox id="noExpiry" label="Stay logged in" checked={noExpiry === 'true'} />
-          </Form>
-
-          {isHosted ? (
-            <div className="margin-top text-muted">
-              <div className="flexbox centered">
-                Don&#39;t have an account?{' '}
-                <Link style={{ marginLeft: '4px' }} to="/signup">
-                  Sign up here
-                </Link>
-              </div>
-              {this.twoFARef && (
-                <div>
-                  <div id="onboard-6" className="tooltip info" data-tip data-for="2fa-tip" data-event="click focus" style={twoFAAnchor}>
-                    <HelpIcon />
-                  </div>
-                  <ReactTooltip id="2fa-tip" globalEventOff="click" place="top" effect="solid" className="react-tooltip info" style={{ maxWidth: 300 }}>
-                    Two Factor Authentication is enabled for your account. If you haven&apos;t set up a 3rd party authentication app with a verification code,
-                    please contact an administrator.
-                  </ReactTooltip>
-                </div>
-              )}
+        {isHosted && (
+          <>
+            <div className="flexbox centered margin-bottom">Log in with:</div>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+              {OAuth2Providers.map(provider => (
+                <Button
+                  className="oauth-provider"
+                  variant="contained"
+                  key={provider.id}
+                  href={`/api/management/v1/useradm/oauth2/${provider.id}`}
+                  startIcon={provider.icon}
+                >
+                  {provider.name}
+                </Button>
+              ))}
             </div>
-          ) : null}
-        </div>
+            <h4 className="dashboard-header margin-top-large" style={{ display: 'flex', justifyContent: 'center' }}>
+              <span style={{ padding: 15, top: -24 }}>or your email address</span>
+            </h4>
+          </>
+        )}
+
+        <Form showButtons={true} buttonColor="primary" onSubmit={formdata => this._handleLogin(formdata)} submitLabel="Log in" submitButtonId="login_button">
+          <TextInput hint="Your email" label="Your email" id="email" required={true} validations="isLength:1,isEmail" />
+          <PasswordInput className="margin-bottom-small" id="password" label="Password" required={true} />
+          {has2FA ? (
+            <TextInput
+              hint="Two Factor Authentication Code"
+              label="Two Factor Authentication Code"
+              id="token2fa"
+              validations="isLength:6,isNumeric"
+              required={true}
+              setControlRef={re => (this.twoFARef = re)}
+            />
+          ) : (
+            <div />
+          )}
+          <FormCheckbox id="noExpiry" label="Stay logged in" checked={noExpiry === 'true'} />
+        </Form>
+
+        {isHosted ? (
+          <div className="margin-top text-muted">
+            <div className="flexbox centered">
+              Don&#39;t have an account?{' '}
+              <Link style={{ marginLeft: '4px' }} to="/signup">
+                Sign up here
+              </Link>
+            </div>
+            {this.twoFARef && (
+              <div>
+                <div id="onboard-6" className="tooltip info" data-tip data-for="2fa-tip" data-event="click focus" style={twoFAAnchor}>
+                  <HelpIcon />
+                </div>
+                <ReactTooltip id="2fa-tip" globalEventOff="click" place="top" effect="solid" className="react-tooltip info" style={{ maxWidth: 300 }}>
+                  Two Factor Authentication is enabled for your account. If you haven&apos;t set up a 3rd party authentication app with a verification code,
+                  please contact an administrator.
+                </ReactTooltip>
+              </div>
+            )}
+          </div>
+        ) : null}
       </div>
     );
   }
