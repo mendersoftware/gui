9789ad6e0ab4863d0e310c9b6c0207ceb8cddc02
Manuel Zedel <manuel.zedel@northern.tech>
2020-07-03 11:18:11 +0200

    adjusted timing to reflect changed mender client in use
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/e2e_tests/cypress/integration/03-files.spec.js
+++ tests/e2e_tests/cypress/integration/03-files.spec.js
@@ -8,9 +8,7 @@ context('Files', () => {
     // enter valid username and password
     cy.get('[id=email]').type(Cypress.env('username'));
     cy.get('[name=password]').type(Cypress.env('password'));
-    cy.contains('button', 'Log in')
-      .click()
-      .wait(3000);
+    cy.contains('button', 'Log in').click().wait(2000);
     cy.visit('ui/#/releases');
   });
 
@@ -44,9 +42,7 @@ context('Files', () => {
 
   it('allows artifact downloads', () => {
     // TODO allow download in tests, for reference: https://github.com/cypress-io/cypress/issues/949
-    cy.get('.expandButton')
-      .click()
-      .end();
+    cy.get('.expandButton').click().end();
     cy.get('.release-repo-item a').contains('Download Artifact');
     // .click().then(anchor => {
     //   const url = anchor.attr('href');
@@ -63,44 +59,20 @@ context('Deployments', () => {
     // enter valid username and password
     cy.get('[id=email]').type(Cypress.env('username'));
     cy.get('[name=password]').type(Cypress.env('password'));
-    cy.contains('button', 'Log in')
-      .click()
-      .wait(2000);
+    cy.contains('button', 'Log in').click().wait(2000);
     cy.visit('ui/#/devices').wait(3000);
     cy.visit('ui/#/releases').wait(2000);
   });
 
   it('allows shortcut deployments', () => {
     // create an artifact to download first
-    cy.get('.repository-list-item')
-      .contains('mender-demo-artifact')
-      .click()
-      .end();
-    cy.get('a')
-      .contains('Create deployment')
-      .click({ force: true })
-      .wait(5000)
-      .end();
+    cy.get('.repository-list-item').contains('mender-demo-artifact').click().end();
+    cy.get('a').contains('Create deployment').click({ force: true }).wait(5000).end();
     cy.get('[placeholder="Select a device group to deploy to"]').click({ force: true });
-    cy.get('[role="tooltip"]')
-      .get('li')
-      .contains('All devices')
-      .click()
-      .wait(2000)
-      .end();
-    cy.get('button')
-      .contains('Next')
-      .click()
-      .end();
-    cy.get('.MuiDialog-container button')
-      .contains('Create')
-      .click()
-      .wait(30000)
-      .end();
-    cy.get('[role="tab"]')
-      .contains('Finished')
-      .click()
-      .end();
+    cy.get('[role="tooltip"]').get('li').contains('All devices').click().end();
+    cy.get('button').contains('Next').click().end();
+    cy.get('.MuiDialog-container button').contains('Create').click().wait(20000).end();
+    cy.get('[role="tab"]').contains('Finished').click().end();
     cy.get('.deployment-item:not(.deployment-header-item)')
       .get('time')
       .should($elems => {
