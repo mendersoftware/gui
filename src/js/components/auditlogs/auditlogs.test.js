39ba7a5c7c008cb2927dfac555bf3ae7e49ca6e1
Manuel Zedel <manuel.zedel@northern.tech>
2020-09-18 08:59:18 +0200

    mocked dates in auditlogs for timepicker to work
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/auditlogs/auditlogs.test.js
+++ src/js/components/auditlogs/auditlogs.test.js
@@ -7,12 +7,25 @@ import configureStore from 'redux-mock-store';
 import AuditLogs from './auditlogs';
 import { defaultState, undefineds } from '../../../../tests/mockData';
 
+let dateMock;
 const mockStore = configureStore([thunk]);
 
 describe('Auditlogs Component', () => {
   let store;
   beforeEach(() => {
     store = mockStore({ ...defaultState });
+    const mockDate = new Date('2019-01-01T13:00:00.000Z');
+    const _Date = Date;
+    global.Date = jest.fn(() => mockDate);
+    global.Date.parse = _Date.parse;
+    global.Date.now = _Date.now;
+    global.Date.toISOString = _Date.toISOString;
+    global.Date.UTC = _Date.UTC;
+    dateMock = jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
+  });
+
+  afterEach(() => {
+    dateMock.mockRestore();
   });
 
   it('renders correctly', () => {
