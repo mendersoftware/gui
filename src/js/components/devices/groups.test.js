f07cdc4c03abe3467c1acdc57d2dd168207e3759
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-03 14:29:21 +0100

    extracted group list sections to ease testability
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/groups.test.js
+++ src/js/components/devices/groups.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Groups from './groups';
-import { undefineds } from '../../../../tests/mockData';
+import Groups, { GroupItem, GroupsSubheader } from './groups';
+import { defaultState, undefineds } from '../../../../tests/mockData';
 
 describe('Groups Component', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Groups groups={[]} openGroupDialog={jest.fn} />).toJSON();
+    const tree = renderer.create(<Groups groups={defaultState.devices.groups.byId} openGroupDialog={jest.fn} />).toJSON();
     expect(tree).toMatchSnapshot();
     expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+  });
+});
+
+describe('Groups sub components', () => {
+  [GroupItem, GroupsSubheader].forEach(Component => {
+    it(`renders ${Component.displayName || Component.name} correctly`, () => {
+      const tree = renderer.create(<Component heading="test" name="test" />).toJSON();
+      expect(tree).toMatchSnapshot();
+      expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    });
   });
 });
