f3e614101ddd50bdb8f1b4a0d0f17aa9f6f192ed
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-11 22:44:12 +0100

    tests: added shallow tests for auditlogs functionality
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/auditlogs/auditlogs.test.js
+++ src/js/components/auditlogs/auditlogs.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { prettyDOM } from '@testing-library/dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
@@ -7,7 +9,6 @@ import configureStore from 'redux-mock-store';
 import AuditLogs from './auditlogs';
 import { defaultState, undefineds } from '../../../../tests/mockData';
 
-let dateMock;
 const mockStore = configureStore([thunk]);
 
 describe('Auditlogs Component', () => {
@@ -21,24 +22,40 @@ describe('Auditlogs Component', () => {
     global.Date.now = _Date.now;
     global.Date.toISOString = _Date.toISOString;
     global.Date.UTC = _Date.UTC;
-    dateMock = jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
-  });
-
-  afterEach(() => {
-    dateMock.mockRestore();
+    global.Date.getUTCFullYear = _Date.getUTCFullYear;
+    global.Date.getUTCMonth = _Date.getUTCMonth;
+    global.Date.getUTCDate = _Date.getUTCDate;
+    jest.mock('moment', () => {
+      return () => jest.requireActual('moment')('2019-01-01T13:00:00.000Z');
+    });
   });
 
   it('renders correctly', () => {
-    const tree = renderer
-      .create(
-        <MemoryRouter>
-          <Provider store={store}>
-            <AuditLogs />
-          </Provider>
-        </MemoryRouter>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    const { baseElement } = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <AuditLogs />
+        </Provider>
+      </MemoryRouter>
+    );
+    const view = prettyDOM(baseElement.firstChild, 100000, { highlight: false })
+      .replace(/id="mui-[0-9]*"/g, '')
+      .replace(/aria-labelledby="(mui-[0-9]* *)*"/g, '')
+      .replace(/\\/g, '');
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+
+  it('works as expected', async () => {
+    render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <AuditLogs />
+        </Provider>
+      </MemoryRouter>
+    );
+    userEvent.click(screen.getByText(/last 7 days/i));
+    userEvent.click(screen.getByText(/clear filter/i));
+    userEvent.click(screen.getByRole('button', { name: /Download results as csv/i }));
   });
 });
