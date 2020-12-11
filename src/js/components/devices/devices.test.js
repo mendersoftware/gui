798f0f050ea1210c3ccaeac98109819d4c2dc5ba
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-11 23:14:50 +0100

    tests: added shallow testing of overall device tab navigation & functionality
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/devices.test.js
+++ src/js/components/devices/devices.test.js
@@ -1,45 +1,30 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { render, screen, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import Devices, { convertQueryToFilterAndGroup } from './devices';
+import { selectMaterialUiSelectOption } from '../../../../tests/setupTests';
 import { defaultState, undefineds } from '../../../../tests/mockData';
 
 const mockStore = configureStore([thunk]);
 
 describe('Devices Component', () => {
   let store;
-  beforeEach(() => {
-    store = mockStore({
-      ...defaultState,
-      devices: {
-        ...defaultState.devices,
-        byStatus: {
-          ...defaultState.devices.byStatus,
-          accepted: { total: 0, deviceIds: [] }
-        },
-        groups: {
-          ...defaultState.devices.groups,
-          byId: {}
-        }
-      }
-    });
-  });
-
   it('renders correctly', () => {
-    const tree = renderer
-      .create(
-        <MemoryRouter>
-          <Provider store={store}>
-            <Devices />
-          </Provider>
-        </MemoryRouter>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    store = mockStore({ ...defaultState });
+    const { baseElement } = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Devices />
+        </Provider>
+      </MemoryRouter>
+    );
+    const view = baseElement.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
   });
 
   it('converts url queries properly to filters and groups', () => {
@@ -63,5 +48,52 @@ describe('Devices Component', () => {
     expect(identityFilters).toHaveLength(2);
     expect(inventoryFilters).toHaveLength(2);
     expect(groupName).toEqual('test');
+  });
+
+  it('works as expected', async () => {
+    store = mockStore({
+      ...defaultState,
+      devices: {
+        ...defaultState.devices,
+        selectedDeviceList: defaultState.devices.byStatus.accepted.deviceIds,
+        groups: {
+          ...defaultState.devices.groups,
+          selectedGroup: 'testGroup'
+        }
+      }
+    });
+    render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Devices />
+        </Provider>
+      </MemoryRouter>
+    );
+    await waitFor(() => expect(screen.getByText(defaultState.devices.byId.a1.id)).toBeInTheDocument());
+
+    userEvent.click(screen.getByRole('tab', { name: /Pending/i }));
+    userEvent.click(screen.getByRole('tab', { name: /Device groups/i }));
+
+    expect(screen.getByRole('heading', { level: 2, name: 'testGroup' })).toBeInTheDocument();
+    const deviceIdHeader = screen.getByText('Device ID');
+    userEvent.click(deviceIdHeader.lastChild);
+    const idDialog = screen.getByText('Default device identity attribute').parentElement.parentElement;
+    expect(idDialog).toBeInTheDocument();
+    await selectMaterialUiSelectOption(idDialog, 'mac');
+    userEvent.click(within(idDialog).getByText(/Save/i));
+    await waitForElementToBeRemoved(() => screen.getByText('Default device identity attribute'));
+    userEvent.click(screen.getByRole('button', { name: 'testGroupDynamic' }));
+    userEvent.click(screen.getByRole('button', { name: /Remove group/i }));
+
+    const removalDialog = screen.getByText('This will remove the group from the list. Are you sure you want to continue?').parentElement.parentElement;
+    expect(removalDialog).toBeInTheDocument();
+    userEvent.click(within(removalDialog).getByRole('button', { name: /Remove group/i }));
+    expect(removalDialog).toBeInTheDocument();
+
+    const deviceListItem = screen.getByText(defaultState.devices.byId.a1.id);
+    userEvent.click(deviceListItem);
+    expect(screen.getByText(defaultState.devices.byId.a1.attributes.ipv4_wlan0)).toBeInTheDocument();
+    userEvent.click(deviceListItem);
+    expect(screen.queryByText(defaultState.devices.byId.a1.attributes.ipv4_wlan0)).toBeFalsy();
   });
 });
