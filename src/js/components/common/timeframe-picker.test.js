f1e75f78342b150426fe5be3babb456a76552ba7
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-11 22:47:23 +0100

    tests: added tests to timerange pickers - for now limited for frame pickers, due to portal usage in datepickers Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/common/timeframe-picker.test.js
+++ src/js/components/common/timeframe-picker.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import TimeframePicker from './timeframe-picker';
 import { undefineds } from '../../../../tests/mockData';
 
-let dateMock;
+let dateMock = global.Date;
 
 describe('TimeframePicker Component', () => {
   beforeEach(() => {
@@ -12,16 +12,17 @@ describe('TimeframePicker Component', () => {
     global.Date = jest.fn(() => mockDate);
     global.Date.parse = _Date.parse;
     global.Date.now = _Date.now;
-    dateMock = jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
+    global.Date.getTime = _Date.getTime;
   });
 
   afterEach(() => {
-    dateMock.mockRestore();
+    global.Date = dateMock;
   });
 
   it('renders correctly', () => {
-    const tree = renderer.create(<TimeframePicker />).toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    const { baseElement } = render(<TimeframePicker />);
+    const view = baseElement.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
   });
 });
