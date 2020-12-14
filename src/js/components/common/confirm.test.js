b2030985bfcfe6a1ba16f3574c774bb2596b4068
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-14 12:01:40 +0100

    tests: added tests for confirmation & copy components
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/common/confirm.test.js
+++ src/js/components/common/confirm.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Confirm from './confirm';
 import { undefineds } from '../../../../tests/mockData';
 
 describe('Confirm Component', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Confirm type="abort" />).toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    const { baseElement } = render(<Confirm type="abort" />);
+    const view = baseElement.firstChild.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+
+  it('works as intended', async () => {
+    const actionCheck = jest.fn();
+    const cancelCheck = jest.fn();
+
+    const { container } = render(<Confirm type="chartRemoval" action={actionCheck} cancel={cancelCheck} />);
+
+    expect(screen.queryByText(/remove this chart\?/i)).toBeInTheDocument();
+    userEvent.click(container.querySelector('.green'));
+    expect(actionCheck).toHaveBeenCalledTimes(1);
+    expect(screen.queryByText(/removing/i)).toBeInTheDocument();
+    userEvent.click(container.querySelector('.red'));
+    expect(cancelCheck).toHaveBeenCalledTimes(1);
   });
 });
--- src/js/components/common/copy-code.test.js
+++ src/js/components/common/copy-code.test.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CopyCode from './copy-code';
 import { undefineds } from '../../../../tests/mockData';
 
 describe('CopyCode Component', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<CopyCode code="sudo it all!" />).toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    const { baseElement } = render(<CopyCode code="sudo it all!" />);
+    const view = baseElement;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+
+  it('works as intended', async () => {
+    const submitCheck = jest.fn();
+    document.execCommand = jest.fn(() => true);
+
+    render(<CopyCode code="sudo it all!" onCopy={submitCheck} withDescription={true} />);
+
+    jest.useFakeTimers('modern');
+    expect(screen.queryByText(/Copied to clipboard/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('button', { name: /Copy to clipboard/i }));
+    expect(submitCheck).toHaveBeenCalledTimes(1);
+    expect(document.execCommand).toHaveBeenCalledTimes(1);
+    expect(screen.queryByText(/Copied to clipboard/i)).toBeInTheDocument();
+    jest.advanceTimersByTime(6000);
+    expect(screen.queryByText(/Copied to clipboard/i)).not.toBeInTheDocument();
   });
 });
