07adbcd6293f93c97abcaf13ca02b5b718e26998
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-16 23:39:43 +0100

    tests: added tests for account cancellation
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/settings/dialogs/cancelrequest.test.js
+++ src/js/components/settings/dialogs/cancelrequest.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CancelRequestDialog from './cancelrequest';
 import { undefineds } from '../../../../../tests/mockData';
 
@@ -8,9 +9,28 @@ describe('CancelRequestDialog Component', () => {
     Math.random = jest.fn(() => 0);
   });
   it('renders correctly', () => {
-    const { baseElement } = render(<CancelRequestDialog open={true} />);
+    const { baseElement } = render(<CancelRequestDialog />);
     const view = baseElement.getElementsByClassName('MuiDialog-root')[0];
     expect(view).toMatchSnapshot();
     expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+
+  it('works as intended', async () => {
+    const submitMock = jest.fn();
+    render(<CancelRequestDialog onCancel={jest.fn} onSubmit={submitMock} />);
+    expect(screen.getByRole('button', { name: /Continue/i })).toBeDisabled();
+    userEvent.click(screen.getByRole('radio', { name: /My project is delayed/i }));
+    expect(screen.getByRole('button', { name: /Continue/i })).not.toBeDisabled();
+    userEvent.click(screen.getByRole('radio', { name: /other/i }));
+    expect(screen.getByRole('button', { name: /Continue/i })).toBeDisabled();
+    userEvent.type(screen.getByPlaceholderText(/reason/i), 'test reason');
+    expect(screen.getByRole('button', { name: /Continue/i })).not.toBeDisabled();
+    userEvent.type(screen.getByPlaceholderText(/suggestions/i), 'test suggestion');
+    userEvent.click(screen.getByRole('button', { name: /Continue/i }));
+
+    expect(screen.queryByText(/thank you/i)).toBeInTheDocument();
+    userEvent.click(screen.getByRole('button', { name: /Confirm/i }));
+
+    expect(submitMock).toHaveBeenCalledWith(`test reason\ntest suggestion`);
   });
 });
