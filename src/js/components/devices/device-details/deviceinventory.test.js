a36d1a65bd3276c4090577b8c9994fe68fd511fc
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-17 00:11:17 +0100

    tests: improved coverage of device details by including software information
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/device-details/deviceinventory.test.js
+++ src/js/components/devices/device-details/deviceinventory.test.js
@@ -6,10 +6,21 @@ import { defaultState, undefineds } from '../../../../../tests/mockData';
 
 describe('CreateGroup Component', () => {
   it('renders correctly', () => {
+    const attributes = {
+      ...defaultState.devices.byId.a1.attributes,
+      artifact_name: 'myapp',
+      'rootfs-image.version': 'stablev1-beta-final-v0',
+      'rootfs-image.checksum': '12341143',
+      'test.version': 'test-2',
+      'a.whole.lot.of.dots.version': 'test-3',
+      'a.whole.lot.of.dots.more': 'test-4',
+      'even.more.dots.than.before.version': 'test-5',
+      'even.more.dots.than.before.more': 'test-6'
+    };
     const tree = renderer
       .create(
         <MemoryRouter>
-          <DeviceInventory attributes={defaultState.devices.byId.a1.attributes} id="a1" setSnackbar={jest.fn} />
+          <DeviceInventory attributes={attributes} id="a1" setSnackbar={jest.fn} />
         </MemoryRouter>
       )
       .toJSON();
