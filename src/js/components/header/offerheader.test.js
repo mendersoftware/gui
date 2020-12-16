41658af62dd4d0e22a07052355944a880c5b4ba1
Manuel Zedel <manuel.zedel@northern.tech>
2020-12-16 17:04:19 +0100

    tests: added snapshot based test for offerheader
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ src/js/components/header/offerheader.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import OfferHeader from './offerheader';
+import { undefineds } from '../../../../tests/mockData';
+
+describe('DeviceNotifications Component', () => {
+  it('renders correctly', () => {
+    const { baseElement } = render(
+      <MemoryRouter>
+        <OfferHeader />
+      </MemoryRouter>
+    );
+    const view = baseElement.firstChild.firstChild;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
+  });
+});
