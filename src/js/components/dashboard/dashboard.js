308f8c412f2e77fc7758f8be0010ee4b217cfd3b
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-01-13 07:03:13 +0100

    Avoid multiple calls to getOnboardingComponentFor in dashboard at page open
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- src/js/components/dashboard/dashboard.js
+++ src/js/components/dashboard/dashboard.js
@@ -20,6 +20,8 @@ const rowBaseStyles = {
 };
 const rowStyles = { ...rowBaseStyles.container, ...styles.rowStyle };
 
+var timeoutID = null;
+
 export const Dashboard = ({ acceptedDevicesCount, currentUser, deploymentDeviceLimit, onboardingState, setSnackbar }) => {
   const [redirect, setRedirect] = useState(null);
 
@@ -27,7 +29,10 @@ export const Dashboard = ({ acceptedDevicesCount, currentUser, deploymentDeviceL
     if (!currentUser || !onboardingState.showTips) {
       return;
     }
-    setTimeout(() => {
+    if (timeoutID !== null) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
       const notification = getOnboardingComponentFor(onboardingSteps.ONBOARDING_START, onboardingState);
       !!notification && setSnackbar('open', 10000, '', notification, () => {}, true);
     }, 400);
