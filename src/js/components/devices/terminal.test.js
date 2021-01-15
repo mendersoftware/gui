dcdfca74cb3a60c34a61e7416767c723c60ea4e9
Manuel Zedel <manuel.zedel@northern.tech>
2021-01-15 20:54:38 +0100

    refactored terminal to allow socket closing confirmation after component unmount
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/terminal.test.js
+++ src/js/components/devices/terminal.test.js
@@ -10,14 +10,29 @@ const mockStore = configureStore([thunk]);
 
 describe('TerminalDialog Component', () => {
   let store;
+  let socketSpyFactory;
+  let socketSpy;
+
   beforeEach(() => {
     store = mockStore({ ...defaultState });
+    socketSpyFactory = jest.spyOn(window, 'WebSocket');
+    socketSpyFactory.mockImplementation(() => {
+      socketSpy = {
+        close: () => {},
+        send: () => {}
+      };
+      return socketSpy;
+    });
+  });
+
+  afterEach(() => {
+    socketSpyFactory.mockReset();
   });
 
   it('renders correctly', async () => {
     const { baseElement } = render(
       <Provider store={store}>
-        <TerminalDialog onCancel={jest.fn} open={true} />
+        <TerminalDialog onCancel={jest.fn} onSocketClose={jest.fn} open={true} />
       </Provider>
     );
     const view = baseElement.getElementsByClassName('MuiDialog-root')[0];
