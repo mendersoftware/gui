f2d41d847daaec0080e683476ab39d513af84ed4
Manuel Zedel <manuel.zedel@northern.tech>
2021-01-21 00:49:29 +0100

    updated docs & help references in onboarding tooltips & aligned their implementation
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/common/dialogs/virtualdeviceonboarding.js
+++ src/js/components/common/dialogs/virtualdeviceonboarding.js
@@ -1,62 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { setOnboardingApproach } from '../../../actions/onboardingActions';
 import { getDemoDeviceCreationCommand } from '../../../helpers';
+import { getDocsVersion } from '../../../selectors';
 import CopyCode from '../copy-code';
 
-export class VirtualDeviceOnboarding extends React.PureComponent {
-  componentDidMount() {
-    this.props.setOnboardingApproach('virtual');
-  }
+export const VirtualDeviceOnboarding = ({ docsVersion, isHosted, setOnboardingApproach, token }) => {
+  useEffect(() => {
+    setOnboardingApproach('virtual');
+  }, []);
 
-  render() {
-    const self = this;
-    const { isHosted, token } = self.props;
+  const codeToCopy = getDemoDeviceCreationCommand(token);
 
-    const codeToCopy = getDemoDeviceCreationCommand(token);
-
-    return (
-      <div>
-        {isHosted ? (
-          <div>
-            <b>1. Get Docker Engine</b>
-            <p>If you do not have it already, please install Docker on your local machine.</p>
-            <p>
-              For example if you are using Ubuntu follow this tutorial:{' '}
-              <a href="https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/" target="_blank" rel="noopener noreferrer">
-                https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
-              </a>
-            </p>
-          </div>
-        ) : (
-          <div>
-            <b>1. Prerequisites</b>
-            <p>
-              As you are running Mender on-premise, for these instructions we assume that you already have Docker installed and the Mender integration
-              environment up and running on your machine.
-            </p>
-            <p>To start a virtual device, change directory into the folder where you cloned Mender integration.</p>
-          </div>
-        )}
-        <p>
-          <b>2. Copy & paste and run the following command to start the virtual device:</b>
-        </p>
-        <CopyCode code={codeToCopy} withDescription={true} />
-        <p>The device should appear in the Pending devices view in a couple of minutes.</p>
-        <p>
-          Visit the <Link to="/help/application-updates/demo-virtual-device">Virtual Devices Help page</Link> for more info on managing the virtual device.
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {isHosted ? (
+        <div>
+          <b>1. Get Docker Engine</b>
+          <p>If you do not have it already, please install Docker on your local machine.</p>
+          <p>
+            For example if you are using Ubuntu follow this tutorial:{' '}
+            <a href="https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/" target="_blank" rel="noopener noreferrer">
+              https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
+            </a>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <b>1. Prerequisites</b>
+          <p>
+            As you are running Mender on-premise, for these instructions we assume that you already have Docker installed and the Mender integration environment
+            up and running on your machine.
+          </p>
+          <p>To start a virtual device, change directory into the folder where you cloned Mender integration.</p>
+        </div>
+      )}
+      <p>
+        <b>2. Copy & paste and run the following command to start the virtual device:</b>
+      </p>
+      <CopyCode code={codeToCopy} withDescription={true} />
+      <p>The device should appear in the Pending devices view in a couple of minutes.</p>
+      <p>
+        Visit the <a href={`https://docs.mender.io/${docsVersion}get-started/preparation/prepare-a-virtual-device`}>our documentation</a> for more information
+        on managing the virtual device.
+      </p>
+    </div>
+  );
+};
 
 const actionCreators = { setOnboardingApproach };
 
 const mapStateToProps = state => {
   return {
+    docsVersion: getDocsVersion(state),
     isHosted: state.app.features.isHosted,
     token: state.organization.organization.tenant_token
   };
