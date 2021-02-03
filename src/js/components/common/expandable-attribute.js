bb74dd0851a6a04b6a208d222526f479f5f971cc
Manuel Zedel <manuel.zedel@northern.tech>
2021-02-03 16:06:32 +0100

    refactored expandable attribute using hooks
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/common/expandable-attribute.js
+++ src/js/components/common/expandable-attribute.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // material ui
 import { Divider, ListItem, ListItemText, Tooltip } from '@material-ui/core';
@@ -7,81 +7,75 @@ import { FileCopyOutlined as CopyToClipboardIcon } from '@material-ui/icons';
 
 import copy from 'copy-to-clipboard';
 
-export default class ExpandableAttribute extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      expanded: false,
-      overflowActive: false,
-      tooltipVisible: false
-    };
-  }
+const defaultClasses = { root: 'attributes' };
 
-  componentDidUpdate() {
-    const self = this;
-    if (self.textContent) {
-      const overflowActive = self.textContent.scrollWidth > self.textContent.clientWidth || self.textContent.scrollHeight > self.textContent.clientHeight;
-      if (self.state.overflowActive !== overflowActive && !self.state.expanded) {
-        self.setState({ overflowActive });
+export const ExpandableAttribute = ({
+  copyToClipboard,
+  dividerDisabled,
+  primary,
+  secondary,
+  secondaryTypographyProps = {},
+  setSnackbar,
+  style,
+  textClasses
+}) => {
+  const textContent = useRef(null);
+  const [expanded, setExpanded] = useState(false);
+  const [overflowActive, setOverflowActive] = useState(false);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  useEffect(() => {
+    if (textContent.current) {
+      const overflowActiveCurrently =
+        textContent.current.scrollWidth > textContent.current.clientWidth || textContent.current.scrollHeight > textContent.current.clientHeight;
+      if (overflowActive !== overflowActiveCurrently && !expanded) {
+        setOverflowActive(overflowActiveCurrently);
       }
     }
-  }
+  }, [textContent]);
 
-  render() {
-    const self = this;
-    const { classes, copyToClipboard, dividerDisabled, primary, secondary, secondaryTypographyProps = {}, setSnackbar, style, textClasses } = self.props;
-    const defaultClasses = { root: 'attributes' };
-    const currentTextClasses = `${textClasses ? textClasses.secondary : 'inventory-text'}${
-      self.state.expanded && self.state.overflowActive ? ' expanded-attribute' : ''
-    }`;
-    const secondaryText = (
-      <div>
-        <span className={currentTextClasses} ref={r => (self.textContent = r)} style={self.state.overflowActive ? { marginBottom: '-0.5em' } : {}}>
-          {secondary}
-        </span>{' '}
-        {self.state.overflowActive ? <a>show {self.state.expanded ? 'less' : 'more'}</a> : null}
-      </div>
-    );
-
-    const onClick = () => {
-      if (copyToClipboard) {
-        // Date/Time components
-        if (secondary.props && secondary.props.value) {
-          copy(secondary.props.value);
-        } else {
-          copy(secondary);
-        }
-        setSnackbar('Value copied to clipboard');
-      }
-      self.setState({ expanded: !self.state.expanded });
-    };
-
-    var cssClasses = classes || defaultClasses;
+  const onClick = () => {
     if (copyToClipboard) {
-      cssClasses.root = (cssClasses.root + ' copy-to-clipboard').trim();
+      // Date/Time components
+      if (secondary.props && secondary.props.value) {
+        copy(secondary.props.value);
+      } else {
+        copy(secondary);
+      }
+      setSnackbar('Value copied to clipboard');
     }
+    setExpanded(!expanded);
+  };
 
-    return (
-      <div
-        onClick={onClick}
-        onMouseEnter={() => this.setState({ 'tooltipVisible': true })}
-        onMouseLeave={() => this.setState({ 'tooltipVisible': false })}
-        style={style}
-      >
-        <ListItem classes={cssClasses} disabled={true}>
-          <ListItemText
-            primary={primary}
-            secondary={secondaryText}
-            secondaryTypographyProps={{ title: secondary, component: 'div', ...secondaryTypographyProps }}
-          />
-          {copyToClipboard ? (
-            <Tooltip title={'Copy to clipboard'} placement="top" open={this.state.tooltipVisible}>
-              <CopyToClipboardIcon fontSize="small"></CopyToClipboardIcon>
-            </Tooltip>
-          ) : null}
-        </ListItem>
-        {dividerDisabled ? null : <Divider />}
-      </div>
-    );
-  }
-}
+  const currentTextClasses = `${textClasses ? textClasses.secondary : 'inventory-text'}${expanded && overflowActive ? ' expanded-attribute' : ''}`;
+  const secondaryText = (
+    <div>
+      <span className={currentTextClasses} ref={textContent} style={overflowActive ? { marginBottom: '-0.5em' } : {}}>
+        {secondary}
+      </span>{' '}
+      {overflowActive ? <a>show {expanded ? 'less' : 'more'}</a> : null}
+    </div>
+  );
+
+  const cssClasses = { ...defaultClasses, root: `${defaultClasses.root} ${copyToClipboard ? 'copy-to-clipboard' : ''}`.trim() };
+
+  return (
+    <div onClick={onClick} onMouseEnter={() => setTooltipVisible(true)} onMouseLeave={() => setTooltipVisible(false)} style={style}>
+      <ListItem classes={cssClasses} disabled={true}>
+        <ListItemText
+          primary={primary}
+          secondary={secondaryText}
+          secondaryTypographyProps={{ title: secondary, component: 'div', ...secondaryTypographyProps }}
+        />
+        {copyToClipboard ? (
+          <Tooltip title={'Copy to clipboard'} placement="top" open={tooltipVisible}>
+            <CopyToClipboardIcon fontSize="small"></CopyToClipboardIcon>
+          </Tooltip>
+        ) : null}
+      </ListItem>
+      {dividerDisabled ? null : <Divider />}
+    </div>
+  );
+};
+
+export default ExpandableAttribute;
