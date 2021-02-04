a68f584b0f3024282b12ee9b17660092ef92e9eb
Manuel Zedel <manuel.zedel@northern.tech>
2021-02-04 16:12:11 +0100

    fixed an issue that could lead to stale device type selection data from being shown refactored device connection info dialog to do so
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/common/dialogs/physicaldeviceonboarding.test.js
+++ src/js/components/common/dialogs/physicaldeviceonboarding.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
-import PhysicalDeviceOnboarding from './physicaldeviceonboarding';
+import PhysicalDeviceOnboarding, { DeviceTypeSelectionStep, InstallationStep } from './physicaldeviceonboarding';
 import { defaultState, undefineds } from '../../../../../tests/mockData';
 
 const mockStore = configureStore([thunk]);
@@ -14,15 +14,37 @@ describe('PhysicalDeviceOnboarding Component', () => {
     store = mockStore({ ...defaultState });
   });
 
+  describe('tiny onboarding tips', () => {
+    [DeviceTypeSelectionStep, InstallationStep].forEach(async Component => {
+      it(`renders ${Component.displayName || Component.name} correctly`, () => {
+        const { baseElement } = render(
+          <Component
+            advanceOnboarding={jest.fn}
+            docsVersion={''}
+            hasConvertedImage={true}
+            ipAddress="test.address"
+            isEnterprise={false}
+            isHosted={true}
+            onSelect={jest.fn}
+            selection="raspberrypi7"
+            token="testtoken"
+          />
+        );
+        const view = baseElement.parentElement;
+        expect(view).toMatchSnapshot();
+        expect(view).toEqual(expect.not.stringMatching(undefineds));
+      });
+    });
+  });
+
   it('renders correctly', async () => {
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <PhysicalDeviceOnboarding progress={1} />
-        </Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
+    const { baseElement } = render(
+      <Provider store={store}>
+        <PhysicalDeviceOnboarding progress={1} />
+      </Provider>
+    );
+    const view = baseElement.parentElement.parentElement;
+    expect(view).toMatchSnapshot();
+    expect(view).toEqual(expect.not.stringMatching(undefineds));
   });
 });
