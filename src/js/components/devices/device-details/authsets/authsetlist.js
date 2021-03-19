7a3acc44e8d6204c7d633be171b0761ff1f922b3
Manuel Zedel <manuel.zedel@northern.tech>
2021-03-19 15:06:30 +0100

    adjusted authset layout to align with design
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/device-details/authsets/authsetlist.js
+++ src/js/components/devices/device-details/authsets/authsetlist.js
@@ -1,26 +1,45 @@
 import React, { useState } from 'react';
 
+import { DEVICE_STATES } from '../../../../constants/deviceConstants';
+import { AuthExplainButton } from '../../../helptips/helptooltips';
 import AuthsetListItem from './authsetlistitem';
 
-export const AuthsetList = ({ active, authsets, hideHeader, ...remainingProps }) => {
+export const AuthsetList = ({ device, showHelptips, ...remainingProps }) => {
   const [expandRow, setExpandRow] = useState();
+  const { auth_sets: authsets = [], status = DEVICE_STATES.accepted } = device;
+
+  let groupedAuthsets = authsets.reduce(
+    // for each authset compare the device status and if it matches authset status, put it in correct list
+    (accu, authset) => {
+      if (authset.status === status) {
+        accu.active.push(authset);
+      } else if (authset.status === DEVICE_STATES.pending) {
+        accu.active.unshift(authset);
+      } else {
+        accu.inactive.push(authset);
+      }
+      return accu;
+    },
+    { active: [], inactive: [] }
+  );
+
+  const orderedAuthsets = [...groupedAuthsets.active, ...groupedAuthsets.inactive];
 
   return (
     <div className="authsets">
-      {!hideHeader && (
-        <div className="flexbox header">
-          {['Public key', 'Request time', 'Status', 'Actions'].map((headerName, index) => (
-            <div className="columnHeader" key={`columnHeader-${index}`}>
-              {headerName}
-            </div>
-          ))}
-        </div>
-      )}
-      <div className="body">
-        {authsets.map(authset => (
+      <div className="header">
+        {['', 'Status', 'Public key', 'Time of request', 'Actions'].map((headerName, index) => (
+          <div className="columnHeader" key={`columnHeader-${index}`}>
+            {headerName}
+          </div>
+        ))}
+      </div>
+      <div className="body" style={{ position: 'relative' }}>
+        {showHelptips && <AuthExplainButton />}
+        {orderedAuthsets.map(authset => (
           <AuthsetListItem
             authset={authset}
-            isActive={active}
+            device={device}
             isExpanded={expandRow === authset.id}
             key={`authset-${authset.id}`}
             onExpand={setExpandRow}
