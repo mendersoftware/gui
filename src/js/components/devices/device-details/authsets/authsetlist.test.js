10da165a10c070eeda60e8c937a03a100a15e866
Manuel Zedel <manuel.zedel@northern.tech>
2021-03-19 18:24:08 +0100

    adjusted tests
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/device-details/authsets/authsetlist.test.js
+++ src/js/components/devices/device-details/authsets/authsetlist.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import AuthsetList from './authsetlist';
-import { undefineds } from '../../../../tests/mockData';
+import { undefineds } from '../../../../../../tests/mockData';
 
 describe('AuthsetList Component', () => {
   it('renders correctly', async () => {
-    const tree = renderer.create(<AuthsetList authsets={[]} />).toJSON();
+    const tree = renderer.create(<AuthsetList device={{ auth_sets: [] }} />).toJSON();
     expect(tree).toMatchSnapshot();
     expect(JSON.stringify(tree)).toEqual(expect.not.stringMatching(undefineds));
   });
