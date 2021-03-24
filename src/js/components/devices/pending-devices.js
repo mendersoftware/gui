77afd5ba31882ef987f90ac9461db10cbb8ff51e
Manuel Zedel <manuel.zedel@northern.tech>
2021-03-24 19:18:25 +0100

    Revert "Revert "extracted device limit check to selector to ease reusability""
    This reverts commit 8db1a235739702139c91d6ab4c0ce97f71a4c162.
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/pending-devices.js
+++ src/js/components/devices/pending-devices.js
@@ -17,7 +17,7 @@ import { getDevicesByStatus, selectGroup, setDeviceFilters, updateDevicesAuth } 
 import { advanceOnboarding } from '../../actions/onboardingActions';
 import { DEVICE_LIST_MAXIMUM_LENGTH, DEVICE_STATES } from '../../constants/deviceConstants';
 import { onboardingSteps } from '../../constants/onboardingConstants';
-import { getIdAttribute, getOnboardingState } from '../../selectors';
+import { getIdAttribute, getLimitMaxed, getOnboardingState } from '../../selectors';
 import { getOnboardingComponentFor } from '../../utils/onboardingmanager';
 import Loader from '../common/loader';
 import DeviceList from './devicelist';
@@ -139,12 +139,12 @@ export class Pending extends BaseDevices {
       filters,
       highlightHelp,
       idAttribute,
+      limitMaxed,
       onboardingState,
       openSettingsDialog,
       showHelptips
     } = self.props;
     const { authLoading, pageLoading, selectedRows, showActions, showFilters } = self.state;
-    const limitMaxed = deviceLimit ? deviceLimit <= acceptedDevices : false;
     const limitNear = deviceLimit ? deviceLimit < acceptedDevices + devices.length : false;
     const selectedOverLimit = deviceLimit ? deviceLimit < acceptedDevices + selectedRows.length : false;
 
@@ -305,6 +305,7 @@ const mapStateToProps = state => {
     count: state.devices.byStatus.pending.total,
     devices: state.devices.selectedDeviceList.slice(0, DEVICE_LIST_MAXIMUM_LENGTH),
     deviceLimit: state.devices.limit,
+    limitMaxed: getLimitMaxed(state),
     filters: state.devices.filters || [],
     highlightHelp: !state.devices.byStatus.accepted.total,
     idAttribute: getIdAttribute(state),
