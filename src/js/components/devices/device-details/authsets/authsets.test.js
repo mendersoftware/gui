c1875b0163e19f4c6364fcb19b9617c4c6408518
Manuel Zedel <manuel.zedel@northern.tech>
2021-03-24 19:19:11 +0100

    Revert "Revert "adjusted tests""
    This reverts commit 070af27fa5726b0dccd161cc8cb83ec7ddc9b723.
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/device-details/authsets/authsets.test.js
+++ src/js/components/devices/device-details/authsets/authsets.test.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
-import Authsets from './index';
+import Authsets from './authsets';
 import { defaultState, undefineds } from '../../../../../../tests/mockData';
 
 const mockStore = configureStore([thunk]);
