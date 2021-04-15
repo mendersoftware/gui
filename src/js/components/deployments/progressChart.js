575a6fc6ec5f9366a33158b82aa8a02965a3c383
Manuel Zedel <manuel.zedel@northern.tech>
2021-04-15 17:10:11 +0200

    fixed state manipulation error raised by immer
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/progressChart.js
+++ src/js/components/deployments/progressChart.js
@@ -68,28 +68,29 @@ export default class ProgressChart extends React.Component {
     // 3 successful deployments -> the 3rd phase should end up with 1 failure so far
     const displayablePhases = phases.reduce(
       (accu, phase) => {
+        let displayablePhase = { ...phase };
         // ongoing phases might not have a device_count yet - so we calculate it
-        let expectedDeviceCountInPhase = Math.floor((totalDeviceCount / 100) * phase.batch_size) || phase.batch_size;
+        let expectedDeviceCountInPhase = Math.floor((totalDeviceCount / 100) * displayablePhase.batch_size) || displayablePhase.batch_size;
         // for phases with more successes than phase.device_count or more failures than phase.device_count we have to guess what phase to put them in =>
         // because of that we have to limit per phase success/ failure counts to the phase.device_count and split the progress between those with a bias for success,
         // therefore we have to track the remaining width and work with it - until we get per phase success & failure information
         let leftoverDevices = expectedDeviceCountInPhase;
-        const possiblePhaseSuccesses = Math.max(Math.min(phase.device_count, totalSuccessCount - accu.countedSuccesses), 0);
+        const possiblePhaseSuccesses = Math.max(Math.min(displayablePhase.device_count, totalSuccessCount - accu.countedSuccesses), 0);
         leftoverDevices -= possiblePhaseSuccesses;
         const possiblePhaseFailures = Math.max(Math.min(leftoverDevices, totalFailureCount - accu.countedFailures), 0);
         leftoverDevices -= possiblePhaseFailures;
         const possiblePhaseProgress = Math.max(Math.min(leftoverDevices, currentProgressCount - accu.countedProgress), 0);
         // if there are too few devices in a phase to register, fallback to occured deployments, as those have definitely happened
         expectedDeviceCountInPhase = Math.max(expectedDeviceCountInPhase, possiblePhaseSuccesses + possiblePhaseProgress + possiblePhaseFailures, 0);
-        phase.successWidth = (possiblePhaseSuccesses / expectedDeviceCountInPhase) * 100 || 0;
-        phase.failureWidth = (possiblePhaseFailures / expectedDeviceCountInPhase) * 100 || 0;
-        if (phase.id === currentPhase.id || leftoverDevices > 0) {
-          phase.progressWidth = (possiblePhaseProgress / expectedDeviceCountInPhase) * 100;
+        displayablePhase.successWidth = (possiblePhaseSuccesses / expectedDeviceCountInPhase) * 100 || 0;
+        displayablePhase.failureWidth = (possiblePhaseFailures / expectedDeviceCountInPhase) * 100 || 0;
+        if (displayablePhase.id === currentPhase.id || leftoverDevices > 0) {
+          displayablePhase.progressWidth = (possiblePhaseProgress / expectedDeviceCountInPhase) * 100;
           accu.countedProgress += possiblePhaseProgress;
         }
         accu.countedFailures += possiblePhaseFailures;
         accu.countedSuccesses += possiblePhaseSuccesses;
-        accu.displayablePhases.push(phase);
+        accu.displayablePhases.push(displayablePhase);
         return accu;
       },
       { countedFailures: 0, countedSuccesses: 0, countedProgress: 0, displayablePhases: [] }
