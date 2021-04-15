4df13a8d906288039b38f6bf99b6c11a8d6e502d
Manuel Zedel <manuel.zedel@northern.tech>
2021-04-15 17:18:59 +0200

    prevented failed single device auth requests from disturbing follow up processing
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/actions/deviceActions.js
+++ src/js/actions/deviceActions.js
@@ -616,7 +616,13 @@ export const deviceFileUpload = (deviceId, path, file) => dispatch => {
     .finally(() => Promise.resolve(dispatch({ type: AppConstants.UPLOAD_PROGRESS, inprogress: false, uploadProgress: 0 })));
 };
 
-export const getDeviceAuth = id => dispatch => Promise.resolve(dispatch(getDevicesWithAuth([{ id }]))).then(results => Promise.resolve(results[1][0]));
+export const getDeviceAuth = id => dispatch =>
+  Promise.resolve(dispatch(getDevicesWithAuth([{ id }]))).then(results => {
+    if (results[results.length - 1]) {
+      return Promise.resolve(results[results.length - 1][0]);
+    }
+    return Promise.resolve();
+  });
 
 export const getDevicesWithAuth = devices => dispatch =>
   devices.length
