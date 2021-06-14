cc4d617532506da2314e5cf48bfc02e2315fbf10
Manuel Zedel <manuel.zedel@northern.tech>
2021-06-14 15:08:45 +0200

    added role dependent (admin only) notification muting to global settings - this also includes a refactor of the global (id_attribute) settings component to ease including it in a dialog or alongside other components closing: men-4757 Changelog: Title
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/settings/global.js
+++ src/js/components/settings/global.js
@@ -1,101 +1,199 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { Button, FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@material-ui/core';
+import { Button, Checkbox, FormControl, FormControlLabel, FormHelperText, InputLabel, MenuItem, Select } from '@material-ui/core';
 import { InfoOutlined as InfoOutlinedIcon } from '@material-ui/icons';
 
 import { getDeviceAttributes } from '../../actions/deviceActions';
+import { changeNotificationSetting } from '../../actions/monitorActions';
 import { getGlobalSettings, saveGlobalSettings } from '../../actions/userActions';
-import { deepCompare } from '../../helpers';
-import { getDocsVersion } from '../../selectors';
+import { alertChannels } from '../../constants/monitorConstants';
+import { getDocsVersion, getUserRoles, getTenantCapabilities } from '../../selectors';
 
-export class Global extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = { updatedSettings: { ...props.settings } };
-    if (!props.settings) {
-      props.getGlobalSettings();
+const maxWidth = 750;
+
+export const IdAttributeSelection = ({ attributes, dialog, docsVersion, onCloseClick, onSaveClick, selectedAttribute = '' }) => {
+  const [attributeSelection, setAttributeSelection] = useState(selectedAttribute);
+
+  useEffect(() => {
+    setAttributeSelection(selectedAttribute);
+  }, [selectedAttribute]);
+
+  const changed = selectedAttribute !== attributeSelection;
+
+  const onChangeIdAttribute = ({ target: { value } }) => {
+    setAttributeSelection(value);
+  };
+
+  const undoChanges = e => {
+    setAttributeSelection(selectedAttribute);
+    if (dialog) {
+      onCloseClick(e);
     }
-    props.getDeviceAttributes();
-  }
+  };
 
-  componentDidUpdate(prevProps) {
-    if (!deepCompare(prevProps.settings, this.props.settings)) {
-      this.setState({ updatedSettings: { ...this.state.updatedSettings, ...this.props.settings } });
+  const saveSettings = e => onSaveClick(e, attributeSelection);
+
+  return (
+    <div className="flexbox space-between" style={{ alignItems: 'flex-start', maxWidth }}>
+      <FormControl>
+        <InputLabel shrink id="device-id">
+          Device identity attribute
+        </InputLabel>
+        <Select value={attributeSelection} onChange={onChangeIdAttribute}>
+          {attributes.map(item => (
+            <MenuItem key={item.value} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText className="info" component="div">
+          <div className="margin-top-small margin-bottom-small">Choose a device identity attribute to use to identify your devices throughout the UI.</div>
+          <div className="margin-top-small margin-bottom-small">
+            <a href={`https://docs.mender.io/${docsVersion}client-installation/identity`} target="_blank" rel="noopener noreferrer">
+              Learn how to add custom identity attributes
+            </a>{' '}
+            to your devices.
+          </div>
+        </FormHelperText>
+      </FormControl>
+      <div className="margin-left margin-top flexbox">
+        <Button disabled={!changed && !dialog} onClick={undoChanges} style={{ marginRight: 10 }}>
+          Cancel
+        </Button>
+        <Button variant="contained" onClick={saveSettings} disabled={!changed} color="primary">
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const GlobalSettingsDialog = ({
+  attributes,
+  docsVersion,
+  hasMonitor,
+  isAdmin,
+  notificationChannelSettings,
+  onChangeNotificationSetting,
+  onCloseClick,
+  onSaveClick,
+  selectedAttribute
+}) => {
+  const [channelSettings, setChannelSettings] = useState(notificationChannelSettings);
+
+  useEffect(() => {
+    setChannelSettings(notificationChannelSettings);
+  }, [notificationChannelSettings]);
+
+  const onNotificationSettingsClick = ({ target: { checked } }, channel) => {
+    setChannelSettings({ ...channelSettings, channel: { enabled: !checked } });
+    onChangeNotificationSetting(!checked, channel);
+  };
+
+  return (
+    <div style={{ maxWidth }} className="margin-top-small">
+      <h2 className="margin-top-small">Global settings</h2>
+      <p className="info" style={{ marginBottom: 30 }}>
+        <InfoOutlinedIcon fontSize="small" style={{ verticalAlign: 'middle', margin: '0 6px 4px 0' }} />
+        {`These settings apply to all users, so changes made here may affect other users' experience.`}
+      </p>
+      <IdAttributeSelection
+        attributes={attributes}
+        docsVersion={docsVersion}
+        onCloseClick={onCloseClick}
+        onSaveClick={onSaveClick}
+        selectedAttribute={selectedAttribute}
+      />
+      {isAdmin &&
+        hasMonitor &&
+        Object.keys(alertChannels).map(channel => (
+          <FormControl key={channel}>
+            <InputLabel className="capitalized-start" shrink id={`${channel}-notifications`}>
+              {channel} notifications
+            </InputLabel>
+            <FormControlLabel
+              control={<Checkbox checked={!channelSettings[channel].enabled} onChange={e => onNotificationSettingsClick(e, channel)} />}
+              label={`Mute ${channel} notifications`}
+            />
+            <FormHelperText className="info" component="div">
+              Mute {channel} notifications for deployment and monitoring issues for all users
+            </FormHelperText>
+          </FormControl>
+        ))}
+    </div>
+  );
+};
+
+export const GlobalSettingsContainer = ({
+  attributes,
+  changeNotificationSetting,
+  closeDialog,
+  dialog,
+  docsVersion,
+  getDeviceAttributes,
+  getGlobalSettings,
+  hasMonitor,
+  isAdmin,
+  notificationChannelSettings,
+  saveGlobalSettings,
+  settings
+}) => {
+  const [updatedSettings, setUpdatedSettings] = useState({ ...settings });
+
+  useEffect(() => {
+    if (!settings) {
+      getGlobalSettings();
     }
-  }
+    getDeviceAttributes();
+  }, []);
 
-  changeIdAttribute(value) {
-    const updatedSettings = { ...this.state.updatedSettings, id_attribute: value };
-    this.setState({ updatedSettings });
-  }
+  useEffect(() => {
+    setUpdatedSettings({ ...updatedSettings, ...settings });
+  }, [settings]);
 
-  undoChanges(e) {
-    const self = this;
-    self.setState({ updatedSettings: self.props.settings });
-    if (self.props.dialog) {
-      self.props.closeDialog(e);
+  const onCloseClick = e => {
+    if (dialog) {
+      return closeDialog(e);
     }
-  }
+  };
 
-  saveSettings(e) {
-    const self = this;
-    return self.props.saveGlobalSettings(self.state.updatedSettings, false, true).then(() => {
-      if (self.props.dialog) {
-        self.props.closeDialog(e);
+  const saveAttributeSetting = (e, id_attribute) => {
+    return saveGlobalSettings({ ...updatedSettings, id_attribute }, false, true).then(() => {
+      if (dialog) {
+        closeDialog(e);
       }
     });
-  }
+  };
 
-  render() {
-    const { attributes, dialog, docsVersion, settings } = this.props;
-    const changed = !deepCompare(this.props.settings, this.state.updatedSettings);
-    const value = this.state.updatedSettings.id_attribute || settings.id_attribute || '';
+  const selectedAttribute = settings.id_attribute;
+  if (dialog) {
     return (
-      <div style={{ maxWidth: '750px' }} className="margin-top-small">
-        {!dialog && (
-          <>
-            <h2 style={{ marginTop: '15px' }}>Global settings</h2>
-            <p className="info" style={{ marginBottom: '30px' }}>
-              <InfoOutlinedIcon fontSize="small" style={{ verticalAlign: 'middle', margin: '0 6px 4px 0' }} />
-              {`These settings apply to all users, so changes made here may affect other users' experience.`}
-            </p>
-          </>
-        )}
-        <FormControl>
-          <InputLabel shrink id="device-id">
-            Device identity attribute
-          </InputLabel>
-          <Select value={value} onChange={e => this.changeIdAttribute(e.target.value)}>
-            {attributes.map((item, index) => (
-              <MenuItem key={index} value={item.value}>
-                {item.label}
-              </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText className="info" component="div">
-            <div className="margin-top-small margin-bottom-small">Choose a device identity attribute to use to identify your devices throughout the UI.</div>
-            <div className="margin-top-small margin-bottom-small">
-              <a href={`https://docs.mender.io/${docsVersion}client-installation/identity`} target="_blank" rel="noopener noreferrer">
-                Learn how to add custom identity attributes
-              </a>{' '}
-              to your devices.
-            </div>
-          </FormHelperText>
-        </FormControl>
-        <div className="margin-top-large float-right">
-          <Button disabled={!changed && !dialog} onClick={e => this.undoChanges(e)} style={{ marginRight: '10px' }}>
-            Cancel
-          </Button>
-          <Button variant="contained" onClick={e => this.saveSettings(e)} disabled={!changed} color="primary">
-            Save
-          </Button>
-        </div>
-      </div>
+      <IdAttributeSelection
+        attributes={attributes}
+        dialog
+        docsVersion={docsVersion}
+        onCloseClick={onCloseClick}
+        onSaveClick={saveAttributeSetting}
+        selectedAttribute={selectedAttribute}
+      />
     );
   }
-}
+  return (
+    <GlobalSettingsDialog
+      attributes={attributes}
+      docsVersion={docsVersion}
+      hasMonitor={hasMonitor}
+      isAdmin={isAdmin}
+      notificationChannelSettings={notificationChannelSettings}
+      onChangeNotificationSetting={changeNotificationSetting}
+      onCloseClick={onCloseClick}
+      onSaveClick={saveAttributeSetting}
+      selectedAttribute={selectedAttribute}
+    />
+  );
+};
 
-const actionCreators = { getDeviceAttributes, getGlobalSettings, saveGlobalSettings };
+const actionCreators = { changeNotificationSetting, getDeviceAttributes, getGlobalSettings, saveGlobalSettings };
 
 const mapStateToProps = state => {
   const attributes = state.devices.filteringAttributes.identityAttributes.slice(0, state.devices.filteringAttributesLimit);
@@ -109,10 +207,13 @@ const mapStateToProps = state => {
   return {
     // limit the selection of the available attribute to AVAILABLE_ATTRIBUTE_LIMIT
     attributes: id_attributes,
+    hasMonitor: getTenantCapabilities(state).hasMonitor,
+    isAdmin: getUserRoles(state).isAdmin,
     devicesCount: Object.keys(state.devices.byId).length,
     docsVersion: getDocsVersion(state),
+    notificationChannelSettings: state.monitor.settings.global.channels,
     settings: state.users.globalSettings
   };
 };
 
-export default connect(mapStateToProps, actionCreators)(Global);
+export default connect(mapStateToProps, actionCreators)(GlobalSettingsContainer);
