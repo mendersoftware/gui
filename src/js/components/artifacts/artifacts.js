7af4bf2826798574eaecb692eede13db05c57445
Manuel Zedel <manuel.zedel@northern.tech>
2021-09-14 17:11:59 +0200

    refactored artifacts component as functional component - this is to unify components & ease state tracking & restore Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/artifacts/artifacts.js
+++ src/js/components/artifacts/artifacts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
@@ -8,15 +8,7 @@ import { CloudUpload, InfoOutlined as InfoIcon } from '@material-ui/icons';
 import { cancelFileUpload, setSnackbar } from '../../actions/appActions';
 import { setDeviceListState } from '../../actions/deviceActions';
 import { advanceOnboarding, setShowCreateArtifactDialog } from '../../actions/onboardingActions';
-import {
-  createArtifact,
-  getReleases,
-  removeArtifact,
-  selectArtifact,
-  selectRelease,
-  showRemoveArtifactDialog,
-  uploadArtifact
-} from '../../actions/releaseActions';
+import { createArtifact, getReleases, removeArtifact, selectRelease, showRemoveArtifactDialog, uploadArtifact } from '../../actions/releaseActions';
 import { onboardingSteps } from '../../constants/onboardingConstants';
 import { getOnboardingState } from '../../selectors';
 import { getOnboardingComponentFor } from '../../utils/onboardingmanager';
@@ -27,163 +19,152 @@ import ReleasesList from './releaseslist';
 
 const refreshArtifactsLength = 30000; //60000
 
-export class Artifacts extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      doneLoading: false,
-      showCreateArtifactDialog: false
-    };
-  }
-  componentDidUpdate(prevProps) {
-    if (prevProps.releases.length !== this.props.releases.length && this.props.releases.length) {
-      const selectedRelease = this.props.releases.find(release => prevProps.releases.every(item => item.Name !== release.Name)) || this.props.releases[0];
-      this.props.selectRelease(selectedRelease);
-    }
-    if (this.props.params && this.props.params.artifactVersion && prevProps.params && prevProps.params.artifactVersion !== this.props.params.artifactVersion) {
-      // selected artifacts
-      self.props.selectArtifact(this.props.params.artifactVersion);
-    }
-  }
-  componentDidMount() {
-    const self = this;
-    const { artifactVersion } = self.props.match.params;
-    if (!self.props.onboardingState.complete) {
-      self.props.setDeviceListState({ deviceIds: [] });
-    }
-    if (!self.props.releases.length) {
-      self._getReleases(artifactVersion);
-    } else {
-      self.setState({ doneLoading: true }, () => {
-        if (artifactVersion) {
-          self.props.selectRelease(artifactVersion);
-        } else {
-          self.props.selectRelease(self.props.releases[0]);
-        }
-      });
-    }
-    self.artifactTimer = setInterval(() => self._getReleases(), refreshArtifactsLength);
-  }
-  componentWillUnmount() {
-    clearInterval(this.artifactTimer);
-  }
+let artifactTimer;
 
-  onFilterReleases(releases) {
-    let selectedRelease = this.props.selectedRelease;
+export const Artifacts = props => {
+  const {
+    getReleases,
+    match,
+    onboardingState,
+    releases,
+    removeArtifact,
+    selectedArtifact,
+    selectedRelease,
+    selectRelease,
+    setShowCreateArtifactDialog,
+    showCreateArtifactDialog,
+    showRemoveArtifactDialog,
+    showRemoveDialog
+  } = props;
+
+  const [doneLoading, setDoneLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const uploadButtonRef = useRef();
+
+  useEffect(() => {
+    if (!releases.length) {
+      return;
+    }
+    if (selectedRelease) {
+      window.location.replace(`#/releases/${selectedRelease.Name}`);
+      return;
+    }
+    selectRelease(releases[0]);
+  }, [releases.length, selectedRelease]);
+
+  useEffect(() => {
+    if (!match.params.artifactVersion) {
+      return;
+    }
+    // selected artifacts
+    selectRelease(match.params.artifactVersion);
+  }, [match.params.artifactVersion]);
+
+  useEffect(() => {
+    const { artifactVersion } = match.params;
+    if (!releases.length) {
+      onGetReleases(artifactVersion);
+    } else {
+      setDoneLoading(true);
+    }
+    artifactTimer = setInterval(onGetReleases, refreshArtifactsLength);
+    return () => {
+      clearInterval(artifactTimer);
+    };
+  }, []);
+
+  const onFilterReleases = releases => {
+    let selectedRelease = { ...props.selectedRelease };
     if (releases && !releases.find(item => selectedRelease && selectedRelease.Name === item.Name)) {
       selectedRelease = releases.length ? releases[0] : null;
     }
-    if (this.props.selectedRelease != selectedRelease) {
-      this.props.selectRelease(selectedRelease);
+    if (selectedRelease != props.selectedRelease) {
+      selectRelease(selectedRelease);
     }
-  }
+  };
 
-  _getReleases(artifactVersion) {
-    var self = this;
-    return self.props.getReleases().finally(() => {
+  const onGetReleases = artifactVersion =>
+    getReleases().finally(() => {
       if (artifactVersion) {
-        self.props.selectRelease(artifactVersion);
+        selectRelease(artifactVersion);
       }
-      self.setState({ doneLoading: true });
+      setDoneLoading(true);
     });
-  }
 
-  onUploadClick() {
-    if (this.props.releases.length) {
-      this.props.advanceOnboarding(onboardingSteps.UPLOAD_NEW_ARTIFACT_TIP);
+  const onUploadClick = () => {
+    if (releases.length) {
+      advanceOnboarding(onboardingSteps.UPLOAD_NEW_ARTIFACT_TIP);
     }
-    this.setState({ showCreateArtifactDialog: true });
-  }
+    setShowCreateArtifactDialog(true);
+  };
 
-  _removeArtifact(artifact) {
-    const self = this;
-    return self.props.removeArtifact(artifact.id).finally(() => self.props.showRemoveArtifactDialog(false));
-  }
+  const onFileUploadClick = selectedFile => {
+    setSelectedFile(selectedFile);
+    setShowCreateArtifactDialog(true);
+  };
 
-  render() {
-    const self = this;
-    const { doneLoading, selectedFile, showCreateArtifactDialog } = self.state;
-    const {
-      onboardingState,
-      releases,
-      showRemoveDialog,
-      selectedArtifact,
-      selectedRelease,
-      setShowCreateArtifactDialog,
-      showOnboardingDialog,
-      showRemoveArtifactDialog
-    } = self.props;
+  const onRemoveArtifact = artifact => removeArtifact(artifact.id).finally(() => showRemoveArtifactDialog(false));
 
-    let uploadArtifactOnboardingComponent = null;
-    if (!onboardingState.complete && self.uploadButtonRef) {
-      uploadArtifactOnboardingComponent = getOnboardingComponentFor(
-        onboardingSteps.UPLOAD_NEW_ARTIFACT_TIP,
-        { ...onboardingState, setShowCreateArtifactDialog },
-        {
-          place: 'right',
-          anchor: {
-            left: self.uploadButtonRef.offsetLeft + self.uploadButtonRef.offsetWidth,
-            top: self.uploadButtonRef.offsetTop + self.uploadButtonRef.offsetHeight / 2
-          }
+  let uploadArtifactOnboardingComponent = null;
+  if (!onboardingState.complete && uploadButtonRef.current) {
+    uploadArtifactOnboardingComponent = getOnboardingComponentFor(
+      onboardingSteps.UPLOAD_NEW_ARTIFACT_TIP,
+      { ...onboardingState, setShowCreateArtifactDialog },
+      {
+        place: 'right',
+        anchor: {
+          left: uploadButtonRef.current.offsetLeft + uploadButtonRef.current.offsetWidth,
+          top: uploadButtonRef.current.offsetTop + uploadButtonRef.current.offsetHeight / 2
         }
-      );
-    }
-
-    return (
-      <>
-        <div className="repository">
-          <div className="flexbox column leftFixed" style={{ alignItems: 'flex-start' }}>
-            <ReleasesList
-              releases={releases}
-              selectedRelease={selectedRelease}
-              onSelect={release => self.props.selectRelease(release)}
-              onFilter={rels => self.onFilterReleases(rels)}
-              loading={!doneLoading}
-            />
-            <Button
-              buttonRef={ref => (this.uploadButtonRef = ref)}
-              color="secondary"
-              onClick={() => self.onUploadClick()}
-              startIcon={<CloudUpload fontSize="small" />}
-              style={{ marginTop: 30, minHeight: 'min-content', minWidth: 164 }}
-              variant="contained"
-            >
-              Upload
-            </Button>
-            <p className="info flexbox center-aligned">
-              <InfoIcon fontSize="small" />
-              Upload an Artifact to an existing or new Release
-            </p>
-            {!!uploadArtifactOnboardingComponent && !showOnboardingDialog && !showCreateArtifactDialog && uploadArtifactOnboardingComponent}
-          </div>
-          <ReleaseRepository
-            refreshArtifacts={() => self._getReleases()}
-            loading={!doneLoading}
-            onUpload={selectedFile => self.setState({ selectedFile, showCreateArtifactDialog: true })}
-            release={selectedRelease}
-          />
-        </div>
-        {showRemoveDialog && (
-          <RemoveArtifactDialog
-            artifact={selectedArtifact.name}
-            open={showRemoveDialog}
-            onCancel={() => showRemoveArtifactDialog(false)}
-            onRemove={() => self._removeArtifact(selectedArtifact || selectedRelease.Artifacts[0])}
-          />
-        )}
-        {showCreateArtifactDialog && (
-          <AddArtifactDialog
-            {...self.props}
-            onCancel={() => self.setState({ showCreateArtifactDialog: false })}
-            onUploadStarted={() => self.setState({ showCreateArtifactDialog: false })}
-            onUploadFinished={() => self._getReleases()}
-            selectedFile={selectedFile}
-          />
-        )}
-      </>
+      }
     );
   }
-}
+
+  const onUploadFinished = releaseName => getReleases().then(() => selectRelease(releaseName));
+
+  return (
+    <>
+      <div className="repository">
+        <div className="flexbox column leftFixed" style={{ alignItems: 'flex-start' }}>
+          <ReleasesList releases={releases} selectedRelease={selectedRelease} onSelect={selectRelease} onFilter={onFilterReleases} loading={!doneLoading} />
+          <Button
+            buttonRef={uploadButtonRef}
+            color="secondary"
+            onClick={onUploadClick}
+            startIcon={<CloudUpload fontSize="small" />}
+            style={{ marginTop: 30, minHeight: 'min-content', minWidth: 164 }}
+            variant="contained"
+          >
+            Upload
+          </Button>
+          <p className="info flexbox center-aligned">
+            <InfoIcon fontSize="small" />
+            Upload an Artifact to an existing or new Release
+          </p>
+          {!!uploadArtifactOnboardingComponent && !showCreateArtifactDialog && uploadArtifactOnboardingComponent}
+        </div>
+        <ReleaseRepository refreshArtifacts={onGetReleases} loading={!doneLoading} onUpload={onFileUploadClick} release={selectedRelease} />
+      </div>
+      {showRemoveDialog && (
+        <RemoveArtifactDialog
+          artifact={selectedArtifact.name}
+          open={showRemoveDialog}
+          onCancel={() => showRemoveArtifactDialog(false)}
+          onRemove={() => onRemoveArtifact(selectedArtifact || selectedRelease.Artifacts[0])}
+        />
+      )}
+      {showCreateArtifactDialog && (
+        <AddArtifactDialog
+          {...props}
+          onCancel={() => setShowCreateArtifactDialog(false)}
+          onUploadStarted={() => setShowCreateArtifactDialog(false)}
+          onUploadFinished={onUploadFinished}
+          selectedFile={selectedFile}
+        />
+      )}
+    </>
+  );
+};
 
 const actionCreators = {
   advanceOnboarding,
@@ -191,7 +172,6 @@ const actionCreators = {
   createArtifact,
   getReleases,
   removeArtifact,
-  selectArtifact,
   setDeviceListState,
   selectRelease,
   setShowCreateArtifactDialog,
@@ -214,11 +194,11 @@ const mapStateToProps = state => {
   return {
     deviceTypes: Object.keys(deviceTypes),
     onboardingState: getOnboardingState(state),
-    showOnboardingDialog: state.onboarding.showCreateArtifactDialog,
     pastCount: state.deployments.byStatus.finished.total,
     releases: Object.values(state.releases.byId),
     selectedArtifact: state.releases.selectedArtifact,
     selectedRelease: state.releases.selectedRelease ? state.releases.byId[state.releases.selectedRelease] : null,
+    showCreateArtifactDialog: state.onboarding.showCreateArtifactDialog,
     showRemoveDialog: state.releases.showRemoveDialog
   };
 };
