2a2ed6123110c3aff8667aa441367f0abc6385cc
Manuel Zedel <manuel.zedel@northern.tech>
2021-09-29 13:24:36 +0200

    refactored software distribution as functional component
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/dashboard/widgets/distribution.js
+++ src/js/components/dashboard/widgets/distribution.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { IconButton } from '@material-ui/core';
 import { Clear as ClearIcon } from '@material-ui/icons';
@@ -10,32 +10,19 @@ import { chartColorPalette } from '../../../themes/mender-theme';
 
 const seriesOther = '__OTHER__';
 
-export default class DistributionReport extends React.Component {
-  constructor(props, state) {
-    super(props, state);
-    this.state = {
-      distribution: [],
-      removing: false
-    };
-  }
+export const DistributionReport = ({ attribute, devices, group, groups, onClick, selectGroup, style }) => {
+  const [distribution, setDistribution] = useState([]);
+  const [removing, setRemoving] = useState(false);
 
-  componentDidMount() {
-    this.initializeDistributionData();
-  }
+  useEffect(() => {
+    initializeDistributionData();
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    const { group, groups } = this.props;
-    if (
-      group &&
-      groups &&
-      (prevProps.groups !== groups || (prevProps.groups[group] && prevProps.groups[group].deviceIds.length !== groups[group].deviceIds.length))
-    ) {
-      this.initializeDistributionData();
-    }
-  }
+  useEffect(() => {
+    initializeDistributionData();
+  }, [groups, groups[group]?.deviceIds.length]);
 
-  initializeDistributionData() {
-    const { attribute, devices, group, groups } = this.props;
+  const initializeDistributionData = () => {
     const relevantDevices = group && groups[group] ? groups[group].deviceIds.map(id => devices[id]) : Object.values(devices);
     const distributionByAttribute = relevantDevices.reduce((accu, item) => {
       if (!item.attributes || item.status !== 'accepted') return accu;
@@ -56,84 +43,76 @@ export default class DistributionReport extends React.Component {
       const others = distributionByAttributeSorted.slice(colors.length).reduce((accu, [, value]) => accu + value, 0);
       distribution.splice(0, 0, { x: seriesOther, name: 'other', y: others, fill: chartColorPalette[chartColorPalette.length - 1] });
     }
-    distribution = distribution.reverse();
-    this.setState({ distribution });
-  }
+    setDistribution(distribution.reverse());
+  };
 
-  onSliceClick(thing) {
+  const onSliceClick = (e, { datum }) => {
+    const thing = datum.x;
     if (thing != seriesOther) {
-      const { attribute, group, groups, selectGroup } = this.props;
       const groupFilters = groups[group]?.filters?.length ? groups[group].filters : [];
       const filters = [...groupFilters, { key: attribute, value: thing, operator: '$eq', scope: 'inventory' }];
       selectGroup(group, filters);
       window.location.replace(`#/devices?${group ? `group=${group}&` : ''}${attribute}=${thing}`);
     }
-  }
+  };
 
-  render() {
-    const self = this;
-    const { group, groups, onClick, style } = self.props;
-    const { distribution: data, removing } = self.state;
-    const total = data.reduce((prev, item) => prev + item.y, 0);
-    return (
-      <div className="margin-right margin-bottom widget chart-widget" style={style}>
-        {removing ? (
-          <Confirm
-            classes="flexbox centered confirmation-overlay"
-            cancel={() => self.setState({ removing: !removing })}
-            action={onClick}
-            style={{ height: '100%' }}
-            type="chartRemoval"
-          />
-        ) : (
-          <>
-            <div className="flexbox space-between margin-left-small">
-              <h4>{group || 'All devices'}</h4>
-              <IconButton className="widgetRemover" onClick={() => self.setState({ removing: !removing })} style={{ alignSelf: 'flex-end' }}>
-                <ClearIcon fontSize="small" />
-              </IconButton>
-            </div>
-            {data.length ? (
-              <VictoryGroup
-                style={{
-                  data: { fill: ({ datum }) => datum.fill },
-                  parent: { display: 'flex', alignSelf: 'center', height: 'initial', width: 'initial' }
-                }}
-                data={data}
-                width={300}
-                height={228}
-              >
-                <VictoryLegend x={30} y={150} width={320} height={65} orientation="horizontal" itemsPerRow={2} gutter={15} rowGutter={-10} />
-                <VictoryPie
-                  endAngle={90}
-                  events={[
-                    {
-                      target: 'data',
-                      eventHandlers: {
-                        onClick: (e, clickedProps) => self.onSliceClick(clickedProps.datum.x)
-                      }
+  const toggleRemoving = () => setRemoving(!removing);
+
+  const total = distribution.reduce((prev, item) => prev + item.y, 0);
+  return (
+    <div className="margin-right margin-bottom widget chart-widget" style={style}>
+      {removing ? (
+        <Confirm classes="flexbox centered confirmation-overlay" cancel={toggleRemoving} action={onClick} style={{ height: '100%' }} type="chartRemoval" />
+      ) : (
+        <>
+          <div className="flexbox space-between margin-left-small">
+            <h4>{group || 'All devices'}</h4>
+            <IconButton className="widgetRemover" onClick={toggleRemoving} style={{ alignSelf: 'flex-end' }}>
+              <ClearIcon fontSize="small" />
+            </IconButton>
+          </div>
+          {distribution.length ? (
+            <VictoryGroup
+              style={{
+                data: { fill: ({ datum }) => datum.fill },
+                parent: { display: 'flex', alignSelf: 'center', height: 'initial', width: 'initial' }
+              }}
+              data={distribution}
+              width={300}
+              height={228}
+            >
+              <VictoryLegend x={30} y={150} width={320} height={65} orientation="horizontal" itemsPerRow={2} gutter={15} rowGutter={-10} />
+              <VictoryPie
+                endAngle={90}
+                events={[
+                  {
+                    target: 'data',
+                    eventHandlers: {
+                      onClick: onSliceClick
                     }
-                  ]}
-                  labelComponent={
-                    <VictoryLabel
-                      text={({ datum }) => datum.y.toString() + ' (' + (Math.round((datum.y * 1000) / (total || 1)) / 10.0).toString() + '%)'}
-                      textAnchor={({ datum }) => (datum.startAngle < 0 ? 'end' : 'start')}
-                    />
                   }
-                  radius={75}
-                  startAngle={-90}
-                />
-              </VictoryGroup>
-            ) : groups[group]?.filters.length && !groups[group]?.deviceIds.length ? (
-              <p className="muted flexbox centered" style={{ height: '100%' }}>
-                No devices are part of this group.
-              </p>
-            ) : (
-              <Loader show={true} />
-            )}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+                ]}
+                labelComponent={
+                  <VictoryLabel
+                    text={({ datum }) => datum.y.toString() + ' (' + (Math.round((datum.y * 1000) / (total || 1)) / 10.0).toString() + '%)'}
+                    textAnchor={({ datum }) => (datum.startAngle < 0 ? 'end' : 'start')}
+                  />
+                }
+                radius={75}
+                startAngle={-90}
+              />
+            </VictoryGroup>
+          ) : groups[group]?.filters.length && !groups[group]?.deviceIds.length ? (
+            <p className="muted flexbox centered" style={{ height: '100%' }}>
+              No devices are part of this group.
+            </p>
+          ) : (
+            <Loader show={true} />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DistributionReport;
