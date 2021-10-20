165316b523017549ffefef5b3dd48f3d00a42f6e
Manuel Zedel <manuel.zedel@northern.tech>
2021-10-20 16:23:10 +0200

    Revert "Revert "added microsoft oauth option""
    This reverts commit ad95706e997b840d1f7080b65ce373cc25f1aee2.
    
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/user-management/oauth2providers.js
+++ src/js/components/user-management/oauth2providers.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { SvgIcon } from '@material-ui/core';
-import { mdiGithub, mdiGoogle } from '@mdi/js';
+import { mdiGithub, mdiGoogle, mdiMicrosoft } from '@mdi/js';
 
 export const OAuth2Providers = [
   {
@@ -19,6 +19,15 @@ export const OAuth2Providers = [
     icon: (
       <SvgIcon fontSize="inherit">
         <path d={mdiGoogle} />
+      </SvgIcon>
+    )
+  },
+  {
+    id: 'microsoft',
+    name: 'Microsoft',
+    icon: (
+      <SvgIcon fontSize="inherit">
+        <path d={mdiMicrosoft} />
       </SvgIcon>
     )
   }
