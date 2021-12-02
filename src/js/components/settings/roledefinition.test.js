487b58e6cbdee536bf6ca418408ef10530f66237
Manuel Zedel <manuel.zedel@northern.tech>
2021-12-02 15:12:12 +0100

    chore: aligned role definition with updated design
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/settings/roledefinition.test.js
+++ src/js/components/settings/roledefinition.test.js
@@ -3,11 +3,22 @@ import React from 'react';
 import { defaultState, undefineds } from '../../../../tests/mockData';
 import { render } from '../../../../tests/setupTests';
 import RoleDefinition from './roledefinition';
+import { emptyRole } from '../../constants/userConstants';
 
 describe('Roles Component', () => {
   it('renders correctly', async () => {
-    const { baseElement } = render(<RoleDefinition adding={true} stateGroups={defaultState.devices.groups.byId} />);
-    const view = baseElement.firstChild.firstChild;
+    const { baseElement } = render(
+      <RoleDefinition
+        adding
+        editing
+        onCancel={jest.fn}
+        onSubmit={jest.fn}
+        removeRole={jest.fn}
+        stateGroups={defaultState.devices.groups.byId}
+        selectedRole={{ ...emptyRole }}
+      />
+    );
+    const view = baseElement.lastElementChild;
     expect(view).toMatchSnapshot();
     expect(view).toEqual(expect.not.stringMatching(undefineds));
   });
