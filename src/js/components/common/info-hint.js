4d97d2335f05eaec0b85c675a9ba8ab94cd8867c
Manuel Zedel <manuel.zedel@northern.tech>
2021-12-16 10:27:34 +0100

    added reusable informational hint component
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ src/js/components/common/info-hint.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { InfoOutlined as InfoOutlinedIcon } from '@material-ui/icons';
+
+const iconStyle = { verticalAlign: 'middle', margin: '0 6px 4px 0' };
+
+export const InfoHint = ({ className = '', content, style = {} }) => (
+  <p className={`info icon ${className}`} style={style}>
+    <InfoOutlinedIcon fontSize="small" style={iconStyle} />
+    {content}
+  </p>
+);
+
+export default InfoHint;
