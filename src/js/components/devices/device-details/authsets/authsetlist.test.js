44db95cc61e63503543deea08d4895b9541c8e29
Manuel Zedel <manuel.zedel@northern.tech>
2021-12-16 15:57:11 +0100

    added sorting to list of device authsets, so that the most relevant should be at the top - most relevant: most recent + pending or active (in the same state as the device is), followed by the remaining authsets
    Changelog: title
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/device-details/authsets/authsetlist.test.js
+++ src/js/components/devices/device-details/authsets/authsetlist.test.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AuthsetList from './authsetlist';
+import { DEVICE_STATES } from '../../../../constants/deviceConstants';
 import { undefineds } from '../../../../../../tests/mockData';
 
 describe('AuthsetList Component', () => {
   it('renders correctly', async () => {
-    const { baseElement } = render(<AuthsetList device={{ auth_sets: [] }} />);
+    const authset = {
+      id: '123',
+      identity_data: { mac: '24:7d:30:90:21:a8' },
+      pubkey: `-----BEGIN PUBLIC KEY-----
+MIIBoj
+-----END PUBLIC KEY-----
+`,
+      ts: '2020-09-21T12:42:34.571Z',
+      status: 'accepted'
+    };
+    const authSets = Object.keys(DEVICE_STATES).reduce((accu, status, index) => {
+      accu.push({ ...authset, id: `${status}-${index}`, status, ts: `2020-09-21T12:${42 + index - 5}:34.571Z` });
+      accu.push({ ...authset, id: `${status}-${index}`, status, ts: `2020-09-21T12:${42 + index}:34.571Z` });
+      accu.push({ ...authset, id: `${status}-${index}`, status, ts: `2020-09-21T12:${42 + index + 5}:34.571Z` });
+      return accu;
+    }, []);
+    const device = {
+      id: 'a7503dcc-afb6-4926-897e-3b1346d1600f',
+      attributes: { device_type: [], artifact_name: '' },
+      updated_ts: '2020-09-21T12:42:38.751Z',
+      identity_data: { mac: '24:7d:30:90:21:a8', status: 'accepted' },
+      status: 'accepted',
+      created_ts: '2020-09-21T12:42:34.567Z',
+      auth_sets: authSets
+    };
+
+    const { baseElement } = render(<AuthsetList device={device} />);
     const view = baseElement.firstChild.firstChild;
     expect(view).toMatchSnapshot();
     expect(view).toEqual(expect.not.stringMatching(undefineds));
