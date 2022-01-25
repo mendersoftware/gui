d207933cc191f56a58a20fe2597fffe2b96efb43
Manuel Zedel <manuel.zedel@northern.tech>
2022-01-25 11:45:53 +0100

    removed reliance on ungrouped group name positioning - was used to filter out static ungrouped group, but was brittle Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/createdeployment.js
+++ src/js/components/deployments/createdeployment.js
@@ -220,8 +220,7 @@ export const mapStateToProps = state => {
   const { [UNGROUPED_GROUP.id]: ungrouped, ...groups } = state.devices.groups.byId;
   return {
     acceptedDeviceCount: state.devices.byStatus.accepted.total,
-    // we need to use group 1 here, as group 0 will be the ungrouped group placeholder
-    createdGroup: Object.keys(state.devices.groups.byId)[1],
+    createdGroup: Object.keys(groups),
     docsVersion: getDocsVersion(state),
     globalSettings: state.users.globalSettings,
     groups,
