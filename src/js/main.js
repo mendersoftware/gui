9a1fad1beacb686631656085fd957da69965d481
Jackbennett <code@jackben.net>
2022-01-26 10:14:56 +0000

    ESLint Fixes
    Changelog: none
    
    Signed-off-by: Jackbennett <code@jackben.net>

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { withStyles} from '@material-ui/core/styles';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './../less/main.less';
@@ -13,16 +12,16 @@ import App from './components/app';
 import store from './reducers';
 import ErrorBoundary from './errorboundary';
 
-const cssVariables = ({palette: p}) => ({
+const cssVariables = ({ palette: p }) => ({
   '@global': {
-    ':root':{
+    ':root': {
       '--mui-primary-main': p.primary.main,
       '--mui-secondary-main': p.secondary.main
     }
   }
-})
+});
 
-const WrappedBaseline = withStyles(cssVariables)(CssBaseline)
+const WrappedBaseline = withStyles(cssVariables)(CssBaseline);
 
 function AppProviders() {
   const theme = createTheme();
