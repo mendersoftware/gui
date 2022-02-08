e3d9cb0b3a1fed95eaac81c4f67dee2c9d78b77b
Manuel Zedel <manuel.zedel@northern.tech>
2022-02-08 22:10:45 +0100

    chore(e2e-tests): aligned tests w/ updated mui paths
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/e2e_tests/integration/04-deployments.spec.ts
+++ tests/e2e_tests/integration/04-deployments.spec.ts
@@ -17,14 +17,13 @@ test.describe('Deployments', () => {
   });
 
   test('allows shortcut deployments', async ({ environment, loggedInPage: page }) => {
-    console.log(`allows shortcut deployments`);
     // create an artifact to download first
     await page.click(`.repository-list-item:has-text('mender-demo-artifact')`);
     await page.click(`a:has-text('Create deployment')`);
     await page.waitForSelector('#deployment-device-group-selection', { timeout: 5000 });
     await page.focus('#deployment-device-group-selection');
     await page.type('#deployment-device-group-selection', 'All');
-    await page.click(`#deployment-device-group-selection-popup li:has-text('All devices')`);
+    await page.click(`#deployment-device-group-selection-listbox li:has-text('All devices')`);
     await page.click(`button:has-text('Next')`);
     if (['enterprise', 'staging'].includes(environment)) {
       await page.click(`css=.MuiDialog-container button >> text=Next`);
@@ -56,12 +55,12 @@ test.describe('Deployments', () => {
     await page.waitForSelector('#deployment-release-selection', { timeout: 5000 });
     await page.focus('#deployment-release-selection');
     await page.type('#deployment-release-selection', 'mender');
-    await page.click(`#deployment-release-selection-popup li:has-text('mender-demo-artifact')`);
+    await page.click(`#deployment-release-selection-listbox li:has-text('mender-demo-artifact')`);
 
     await page.waitForSelector('#deployment-device-group-selection', { timeout: 5000 });
     await page.focus('#deployment-device-group-selection');
     await page.type('#deployment-device-group-selection', 'test');
-    await page.click(`#deployment-device-group-selection-popup li:has-text('testgroup')`);
+    await page.click(`#deployment-device-group-selection-listbox li:has-text('testgroup')`);
     await page.click(`button:has-text('Next')`);
     if (['enterprise', 'staging'].includes(environment)) {
       await page.click(`.MuiDialog-container button >> text=Next`);
