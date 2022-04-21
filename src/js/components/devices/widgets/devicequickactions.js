b3088cb686b76620245dd0af8eec1b8116f0b292
Manuel Zedel <manuel.zedel@northern.tech>
2022-04-21 23:59:27 +0200

    chore: made device actions easier to reuse for more scenarios
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/devices/widgets/devicequickactions.js
+++ src/js/components/devices/widgets/devicequickactions.js
@@ -7,103 +7,94 @@ import {
   CheckCircle as CheckCircleIcon,
   HeightOutlined as HeightOutlinedIcon,
   HighlightOffOutlined as HighlightOffOutlinedIcon,
-  RemoveCircleOutline as RemoveCircleOutlineIcon
+  RemoveCircleOutline as RemoveCircleOutlineIcon,
+  Replay as ReplayIcon
 } from '@mui/icons-material';
 import { mdiTrashCanOutline as TrashCan } from '@mdi/js';
 
+import GatewayIcon from '../../../../assets/img/gateway.svg';
 import MaterialDesignIcon from '../../common/materialdesignicon';
 import { DEVICE_STATES, UNGROUPED_GROUP } from '../../../constants/deviceConstants';
-import { deepCompare } from '../../../helpers';
+import { deepCompare, stringToBoolean } from '../../../helpers';
 
 const defaultActions = {
   accept: {
     icon: <CheckCircleIcon className="green" />,
     key: 'accept',
     title: pluralized => `Accept ${pluralized}`,
-    action: ({ onAuthorizationChange, selection }) => onAuthorizationChange(selection, DEVICE_STATES.accepted)
+    action: ({ onAuthorizationChange, selection }) => onAuthorizationChange(selection, DEVICE_STATES.accepted),
+    checkRelevance: ({ device }) => [DEVICE_STATES.pending, DEVICE_STATES.rejected].includes(device.status)
   },
   dismiss: {
     icon: <RemoveCircleOutlineIcon className="red" />,
     key: 'dismiss',
     title: pluralized => `Dismiss ${pluralized}`,
-    action: ({ onDeviceDismiss, selection }) => onDeviceDismiss(selection)
+    action: ({ onDeviceDismiss, selection }) => onDeviceDismiss(selection),
+    checkRelevance: ({ device }) =>
+      [DEVICE_STATES.accepted, DEVICE_STATES.pending, DEVICE_STATES.preauth, DEVICE_STATES.rejected, 'noauth'].includes(device.status)
   },
   reject: {
     icon: <HighlightOffOutlinedIcon className="red" />,
     key: 'reject',
     title: pluralized => `Reject ${pluralized}`,
-    action: ({ onAuthorizationChange, selection }) => onAuthorizationChange(selection, DEVICE_STATES.rejected)
+    action: ({ onAuthorizationChange, selection }) => onAuthorizationChange(selection, DEVICE_STATES.rejected),
+    checkRelevance: ({ device }) => [DEVICE_STATES.accepted, DEVICE_STATES.pending].includes(device.status)
   },
   addToGroup: {
     icon: <AddCircleIcon className="green" />,
     key: 'group-add',
     title: pluralized => `Add selected ${pluralized} to a group`,
-    action: ({ onAddDevicesToGroup, selection }) => onAddDevicesToGroup(selection)
+    action: ({ onAddDevicesToGroup, selection }) => onAddDevicesToGroup(selection),
+    checkRelevance: ({ selectedGroup }) => !selectedGroup
   },
   moveToGroup: {
     icon: <HeightOutlinedIcon className="rotated ninety" />,
     key: 'group-change',
     title: pluralized => `Move selected ${pluralized} to another group`,
-    action: ({ onAddDevicesToGroup, selection }) => onAddDevicesToGroup(selection)
+    action: ({ onAddDevicesToGroup, selection }) => onAddDevicesToGroup(selection),
+    checkRelevance: ({ selectedGroup }) => !!selectedGroup
   },
   removeFromGroup: {
     icon: <MaterialDesignIcon path={TrashCan} />,
     key: 'group-remove',
     title: pluralized => `Remove selected ${pluralized} from this group`,
-    action: ({ onRemoveDevicesFromGroup, selection }) => onRemoveDevicesFromGroup(selection)
+    action: ({ onRemoveDevicesFromGroup, selection }) => onRemoveDevicesFromGroup(selection),
+    checkRelevance: ({ selectedGroup }) => selectedGroup && selectedGroup !== UNGROUPED_GROUP.id
+  },
+  promoteToGateway: {
+    icon: <GatewayIcon style={{ width: 20 }} />,
+    key: 'promote-to-gateway',
+    title: () => 'Promote to gateway',
+    action: ({ onPromoteGateway, selection }) => onPromoteGateway(selection),
+    checkRelevance: ({ device }) => !stringToBoolean(device.attributes.mender_is_gateway) && device.status === DEVICE_STATES.accepted
+  },
+  createDeployment: {
+    icon: <ReplayIcon />,
+    key: 'create-deployment',
+    title: pluralized => `Create deployment for this ${pluralized}`,
+    action: ({ onCreateDeployment, selection }) => onCreateDeployment(selection),
+    checkRelevance: ({ device, isSingleDevice }) => isSingleDevice && device && device.status === DEVICE_STATES.accepted
   }
 };
 
-const defaultActionsList = [
-  {
-    ...defaultActions.dismiss,
-    allowedStates: [DEVICE_STATES.accepted, DEVICE_STATES.pending, DEVICE_STATES.preauth, DEVICE_STATES.rejected, 'noauth']
-  },
-  {
-    ...defaultActions.reject,
-    allowedStates: [DEVICE_STATES.accepted, DEVICE_STATES.pending]
-  },
-  {
-    ...defaultActions.accept,
-    allowedStates: [DEVICE_STATES.pending, DEVICE_STATES.rejected]
-  }
-];
-
-export const DeviceQuickActions = ({ devices, actionCallbacks, selectedGroup, selectedRows }, ref) => {
+export const DeviceQuickActions = ({ actionCallbacks, devices, isSingleDevice = false, selectedGroup, selectedRows }, ref) => {
   const [showActions, setShowActions] = useState(false);
 
-  const actions = useMemo(() => {
-    const deviceStates = Object.keys(
-      selectedRows.reduce((accu, row) => {
-        const device = devices[row];
-        accu[device.status] = (accu[device.status] || 0) + 1;
-        return accu;
-      }, {})
-    );
-    let relevantActions = defaultActionsList.reduce((accu, action) => {
-      if (deviceStates.every(state => action.allowedStates.includes(state))) {
+  const { actions, selectedDevices } = useMemo(() => {
+    const selectedDevices = selectedRows.map(row => devices[row]);
+    const actions = Object.values(defaultActions).reduce((accu, action) => {
+      if (selectedDevices.every(device => action.checkRelevance({ device, isSingleDevice, selectedGroup }))) {
         accu.push(action);
       }
       return accu;
     }, []);
+    return { actions, selectedDevices };
+  }, [devices, isSingleDevice, selectedRows, selectedGroup]);
 
-    if (selectedGroup) {
-      relevantActions.push(defaultActions.moveToGroup);
-      if (selectedGroup !== UNGROUPED_GROUP.id) {
-        relevantActions.push(defaultActions.removeFromGroup);
-      }
-    } else {
-      relevantActions.push(defaultActions.addToGroup);
-    }
-    return relevantActions;
-  }, [selectedRows]);
-
-  const pluralized = pluralize('devices', selectedRows.length);
+  const pluralized = pluralize('devices', selectedDevices.length);
   return (
     <div className="flexbox fixedButtons" ref={ref}>
-      <div className="margin-right">
-        {selectedRows.length} {pluralize('devices', selectedRows.length)} selected
-      </div>
+      <div className="margin-right">{isSingleDevice ? 'Device actions' : `${selectedDevices.length} ${pluralized} selected`}</div>
       <SpeedDial
         ariaLabel="device-actions"
         className="margin-small"
@@ -119,7 +110,7 @@ export const DeviceQuickActions = ({ devices, actionCallbacks, selectedGroup, se
             icon={action.icon}
             tooltipTitle={action.title(pluralized)}
             tooltipOpen
-            onClick={() => action.action({ ...actionCallbacks, selection: selectedRows })}
+            onClick={() => action.action({ ...actionCallbacks, selection: selectedDevices })}
           />
         ))}
       </SpeedDial>
