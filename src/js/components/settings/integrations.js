0f7a32503515981a530d51671eb9656eaf86c042
Manuel Zedel <manuel.zedel@northern.tech>
2022-07-20 22:11:10 +0200

    feat: added a way to configure an AWS IoT core connection
    Ticket: MEN-5453
    Changelog: Title
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/settings/integrations.js
+++ src/js/components/settings/integrations.js
@@ -1,32 +1,135 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import validator from 'validator';
 
-import { Button, MenuItem, Select, TextField } from '@mui/material';
+import { Button, Divider, MenuItem, Select, TextField } from '@mui/material';
 
 import { changeIntegration, createIntegration, deleteIntegration, getIntegrations } from '../../actions/organizationActions';
 import { EXTERNAL_PROVIDER } from '../../constants/deviceConstants';
 import Confirm from '../common/confirm';
 import InfoHint from '../common/info-hint';
+import { useDebounce } from '../../utils/debouncehook';
+import { makeStyles } from 'tss-react/mui';
+import { customSort } from '../../helpers';
 
 const maxWidth = 750;
 
-export const IntegrationConfiguration = ({ integration, onCancel, onDelete, onSave }) => {
-  const { credentials = {}, provider } = integration;
-  const connectionString = credentials[EXTERNAL_PROVIDER[provider].credentialsAttribute] || '';
-  const [connectionConfig, setConnectionConfig] = useState(connectionString);
-  const [isEditing, setIsEditing] = useState(!connectionString);
-  const [isDeleting, setIsDeleting] = useState(false);
+const useStyles = makeStyles()(theme => ({
+  leftButton: { marginRight: theme.spacing() },
+  inputWrapper: { alignItems: 'flex-end' },
+  select: { marginTop: theme.spacing(2), minWidth: 300 },
+  textInput: { margin: theme.spacing(), minWidth: 500, wordBreak: 'break-all' },
+  widthLimit: { maxWidth }
+}));
+
+const ConnectionDetailsInput = ({ connectionConfig, isEditing, setConnectionConfig }) => {
+  const { access_key_id = '', secret_access_key = '', endpoint_url = '', device_policy_arn = '' } = connectionConfig;
+  const [keyId, setKeyId] = useState(access_key_id);
+  const [keySecret, setKeySecret] = useState(secret_access_key);
+  const [endpoint, setEndpoint] = useState(endpoint_url);
+  const [endpointError, setEndpointError] = useState('');
+  const [policyARN, setPolicyARN] = useState(device_policy_arn);
+
+  const debouncedId = useDebounce(keyId, 700);
+  const debouncedSecret = useDebounce(keySecret, 700);
+  const debouncedEndpoint = useDebounce(endpoint, 700);
+  const debouncedARN = useDebounce(policyARN, 700);
+
+  const { classes } = useStyles();
 
   useEffect(() => {
-    const { credentials = {}, provider } = integration;
-    const connectionString = credentials[EXTERNAL_PROVIDER[provider].credentialsAttribute] || '';
-    setConnectionConfig(connectionString);
-    setIsEditing(!connectionString);
+    setConnectionConfig({
+      access_key_id: debouncedId,
+      secret_access_key: debouncedSecret,
+      endpoint_url: debouncedEndpoint,
+      device_policy_arn: debouncedARN
+    });
+  }, [debouncedARN, debouncedEndpoint, debouncedId, debouncedSecret]);
+
+  useEffect(() => {
+    setKeyId(access_key_id);
+    setKeySecret(secret_access_key);
+    setEndpoint(endpoint_url);
+    setPolicyARN(device_policy_arn);
+  }, [access_key_id, secret_access_key, endpoint_url, device_policy_arn]);
+
+  const onKeyChange = ({ target: { value = '' } }) => setKeyId(value);
+  const onSecretChange = ({ target: { value = '' } }) => setKeySecret(value);
+  const onEndpointChange = ({ target: { value = '' } }) => {
+    if (value?.length > 4 && !validator.isURL(value)) {
+      setEndpointError('Incorrect endpoint URL format, please enter a valid URL.');
+    } else {
+      setEndpointError('');
+    }
+    setEndpoint(value);
+  };
+  const onARNChange = ({ target: { value = '' } }) => setPolicyARN(value);
+
+  const commonProps = { className: classes.textInput, disabled: !isEditing, multiline: true };
+  return (
+    <div className="flexbox column">
+      <TextField {...commonProps} label="Key ID" onChange={onKeyChange} value={keyId} />
+      <TextField {...commonProps} label="Key Secret" onChange={onSecretChange} value={keySecret} />
+      <TextField {...commonProps} label="Endpoint" onChange={onEndpointChange} value={endpoint} error={!!endpointError} helperText={endpointError} />
+      <TextField {...commonProps} label="Policy ARN" onChange={onARNChange} value={policyARN} />
+    </div>
+  );
+};
+
+const ConnectionStringInput = ({ connectionConfig, isEditing, setConnectionConfig, title }) => {
+  const [value, setValue] = useState(connectionConfig.connection_string);
+  const debouncedValue = useDebounce(value, 700);
+
+  const { classes } = useStyles();
+
+  useEffect(() => {
+    setConnectionConfig({ connection_string: debouncedValue });
+  }, [debouncedValue]);
+
+  useEffect(() => {
+    setValue(connectionConfig.connection_string);
+  }, [connectionConfig.connection_string]);
+
+  const updateConnectionConfig = ({ target: { value = '' } }) => setValue(value);
+
+  return (
+    <TextField
+      className={classes.textInput}
+      disabled={!isEditing}
+      label={`${title} connection string`}
+      multiline
+      onChange={updateConnectionConfig}
+      value={value}
+    />
+  );
+};
+
+const providerConfigMap = {
+  'iot-core': ConnectionDetailsInput,
+  'iot-hub': ConnectionStringInput
+};
+
+export const IntegrationConfiguration = ({ integration, isLast, onCancel, onDelete, onSave }) => {
+  const { credentials = {}, provider } = integration;
+  const [connectionConfig, setConnectionConfig] = useState(credentials);
+  // eslint-disable-next-line no-unused-vars
+  const { type, ...otherProps } = credentials;
+  const [isEditing, setIsEditing] = useState(!Object.values(otherProps).some(i => i));
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const { classes } = useStyles();
+
+  useEffect(() => {
+    const { credentials = {} } = integration;
+    // eslint-disable-next-line no-unused-vars
+    const { type, ...otherProps } = credentials;
+    setConnectionConfig(credentials);
+    setIsEditing(!Object.values(otherProps).some(i => i));
   }, [integration]);
 
   const onCancelClick = () => {
     setIsEditing(false);
-    setConnectionConfig(connectionString);
+    setConnectionConfig(credentials);
     onCancel(integration);
   };
   const onDeleteClick = () => setIsDeleting(true);
@@ -37,50 +140,40 @@ export const IntegrationConfiguration = ({ integration, onCancel, onDelete, onSa
       ...integration,
       credentials: {
         type: EXTERNAL_PROVIDER[provider].credentialsType,
-        [EXTERNAL_PROVIDER[provider].credentialsAttribute]: connectionConfig
+        ...connectionConfig
       }
     });
 
-  const updateConnectionConfig = ({ target: { value = '' } }) => setConnectionConfig(value);
-
+  const ConfigInput = providerConfigMap[provider];
   const { configHint, title } = EXTERNAL_PROVIDER[provider];
   return (
     <>
       <h3 className="margin-bottom-none">{title}</h3>
-      <div className="flexbox space-between relative" style={{ alignItems: 'flex-end', maxWidth }}>
-        <TextField
-          disabled={!isEditing}
-          InputLabelProps={{ shrink: !!connectionConfig }}
-          label={`${title} connection string`}
-          multiline
-          onChange={updateConnectionConfig}
-          style={{ minWidth: 500, wordBreak: 'break-all' }}
-          value={connectionConfig}
-        />
+      <div className={`flexbox space-between relative ${classes.widthLimit} ${classes.inputWrapper}`}>
+        <ConfigInput connectionConfig={connectionConfig} isEditing={isEditing} setConnectionConfig={setConnectionConfig} title={title} />
         <div className="flexbox">
           {isEditing ? (
             <>
-              <Button onClick={onCancelClick} style={{ marginRight: 10 }}>
+              <Button className={classes.leftButton} onClick={onCancelClick}>
                 Cancel
               </Button>
-              <Button variant="contained" onClick={onSaveClick} disabled={connectionString === connectionConfig}>
+              <Button variant="contained" onClick={onSaveClick} disabled={credentials === connectionConfig}>
                 Save
               </Button>
             </>
           ) : (
             <>
-              <Button onClick={onEditClick} style={{ marginRight: 10 }}>
+              <Button className={classes.leftButton} onClick={onEditClick}>
                 Edit
               </Button>
               <Button onClick={onDeleteClick}>Delete</Button>
             </>
           )}
         </div>
-        {isDeleting && (
-          <Confirm type="integrationRemoval" classes="confirmation-overlay" action={onDeleteConfirm} cancel={() => setIsDeleting(false)} style={{}} />
-        )}
+        {isDeleting && <Confirm type="integrationRemoval" classes="confirmation-overlay" action={onDeleteConfirm} cancel={() => setIsDeleting(false)} />}
       </div>
-      <InfoHint content={configHint} style={{ maxWidth }} />
+      <InfoHint className={`margin-bottom ${classes.widthLimit}`} content={configHint} />
+      {!isLast && <Divider className={`margin-bottom ${classes.widthLimit}`} />}
     </>
   );
 };
@@ -88,28 +181,22 @@ export const IntegrationConfiguration = ({ integration, onCancel, onDelete, onSa
 export const Integrations = ({ integrations = [], changeIntegration, createIntegration, deleteIntegration, getIntegrations }) => {
   const [availableIntegrations, setAvailableIntegrations] = useState([]);
   const [configuredIntegrations, setConfiguredIntegrations] = useState([]);
-  const [isCreating, setIsCreating] = useState(false);
-  const [currentValue, setCurrentValue] = useState('');
+
+  const { classes } = useStyles();
+
+  const determineAvailableIntegrations = integrations =>
+    Object.values(EXTERNAL_PROVIDER).reduce((accu, provider) => {
+      const hasIntegrationConfigured = integrations.some(integration => integration.provider == provider.provider);
+      if (provider.enabled && !hasIntegrationConfigured) {
+        accu.push(provider);
+      }
+      return accu;
+    }, []);
 
   useEffect(() => {
-    const { available, needsUpdate } = Object.values(EXTERNAL_PROVIDER).reduce(
-      (accu, provider) => {
-        const hasIntegrationConfigured = integrations.some(integration => integration.provider == provider.provider);
-        if (provider.enabled && !hasIntegrationConfigured) {
-          accu.available.push(provider);
-        }
-        if (hasIntegrationConfigured && availableIntegrations.some(availableIntegration => availableIntegration.provider === provider.provider)) {
-          accu.needsUpdate = true;
-        }
-        return accu;
-      },
-      { available: [], needsUpdate: !(availableIntegrations.length || integrations.length) }
-    );
-    if (needsUpdate) {
-      setAvailableIntegrations(available);
-    }
+    const available = determineAvailableIntegrations(integrations);
+    setAvailableIntegrations(available);
     setConfiguredIntegrations(integrations);
-    setIsCreating(!!integrations.length);
   }, [integrations]);
 
   useEffect(() => {
@@ -117,32 +204,39 @@ export const Integrations = ({ integrations = [], changeIntegration, createInteg
   }, []);
 
   const onConfigureIntegration = ({ target: { value: provider = '' } }) => {
-    setCurrentValue(provider);
-    setConfiguredIntegrations([{ id: 'new', provider }, ...configuredIntegrations]);
-    setIsCreating(true);
+    setConfiguredIntegrations([...configuredIntegrations, { id: 'new', provider }]);
+    setAvailableIntegrations(integrations => integrations.filter(integration => integration.provider !== provider));
   };
 
-  const onCancelClick = ({ connectionString }) => {
-    setIsCreating(!!connectionString);
-    setCurrentValue('');
-    setConfiguredIntegrations(integrations);
+  const onCancelClick = ({ id, provider }) => {
+    if (id === 'new') {
+      setAvailableIntegrations(current => [...current, EXTERNAL_PROVIDER[provider]].sort(customSort(true, 'provider')));
+      setConfiguredIntegrations(current => current.filter(integration => !(integration.id === id && integration.provider === provider)));
+    }
   };
 
   const onSaveClick = integration => {
     if (integration.id === 'new') {
-      createIntegration(integration);
-    } else {
-      changeIntegration(integration);
+      return createIntegration(integration);
     }
-    setCurrentValue('');
-    return setIsCreating(isCreating && !integration.connectionString ? false : isCreating);
+    changeIntegration(integration);
   };
 
   return (
     <div>
       <h2 className="margin-top-small">Integrations</h2>
-      {!isCreating && !!availableIntegrations.length && (
-        <Select displayEmpty onChange={onConfigureIntegration} value={currentValue} style={{ minWidth: 300 }}>
+      {configuredIntegrations.map((integration, index) => (
+        <IntegrationConfiguration
+          key={integration.provider}
+          integration={integration}
+          isLast={configuredIntegrations.length === index + 1}
+          onCancel={onCancelClick}
+          onDelete={deleteIntegration}
+          onSave={onSaveClick}
+        />
+      ))}
+      {!!availableIntegrations.length && (
+        <Select className={classes.select} displayEmpty onChange={onConfigureIntegration} value="">
           <MenuItem value="">Add new integration</MenuItem>
           {availableIntegrations.map(item => (
             <MenuItem key={item.provider} value={item.provider}>
@@ -151,9 +245,6 @@ export const Integrations = ({ integrations = [], changeIntegration, createInteg
           ))}
         </Select>
       )}
-      {configuredIntegrations.map(integration => (
-        <IntegrationConfiguration key={integration.id} integration={integration} onCancel={onCancelClick} onDelete={deleteIntegration} onSave={onSaveClick} />
-      ))}
     </div>
   );
 };
