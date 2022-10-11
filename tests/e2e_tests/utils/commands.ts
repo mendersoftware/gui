9d8eca421a75fb89db57c37fa4b4d1db782b98bb
Manuel Zedel <manuel.zedel@northern.tech>
2022-10-11 15:59:18 +0200

    fix(e2e-tests): unify client image tags to avoid inconsistencies
    Ticket: None
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/e2e_tests/utils/commands.ts
+++ tests/e2e_tests/utils/commands.ts
@@ -98,7 +98,7 @@ export const startDockerClient = async (baseUrl, token) => {
     `${projectRoot}/dockerClient/mender-test.json:/etc/mender/mender.conf`,
     '-v',
     `${projectRoot}/dockerClient/mender-connect-test.json:/etc/mender/mender-connect.conf`,
-    'mendersoftware/mender-client-docker-addons:master'
+    'mendersoftware/mender-client-docker-addons:mender-master'
   ];
   console.log(`starting with: ${token}`);
   console.log(`starting using: docker ${args.join(' ')}`);
