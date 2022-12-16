ba99a984a5b33a1ff9dde8538564d0df223f89c6
Manuel Zedel <manuel.zedel@northern.tech>
2022-12-16 15:30:37 +0100

    fix: worked around missing esm support in linkify
    Ticket: None
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/header/announcement.test.js
+++ src/js/components/header/announcement.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Linkify from 'react-linkify';
 
 import { undefineds } from '../../../../tests/mockData';
 import { render } from '../../../../tests/setupTests';
 import Announcement from './announcement';
 
+jest.mock('react-linkify');
+
 describe('Announcement Component', () => {
   it('renders correctly', async () => {
+    Linkify.default = jest.fn();
+    Linkify.default.mockReturnValue(null);
     const { baseElement } = render(<Announcement errorIconClassName="" iconClassName="" sectionClassName="" />);
     const view = baseElement.firstChild.firstChild;
     expect(view).toMatchSnapshot();
