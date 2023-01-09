f568f695b106d8b59d730de2379993ff94a4d17d
Manuel Zedel <manuel.zedel@northern.tech>
2023-01-09 16:31:45 +0100

    feat: expanded artifact upload form to allow more options to be sent
    + refactored upload dialog to ease further extensions per artifact upload type
    
    Ticket: MEN-6085
    Changelog: Title
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/artifacts/dialogs/artifactinformationform.js
+++ src/js/components/artifacts/dialogs/artifactinformationform.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { InfoOutlined as InfoIcon } from '@mui/icons-material';
@@ -8,6 +8,7 @@ import { onboardingSteps } from '../../../constants/onboardingConstants';
 import { duplicateFilter, unionizeStrings } from '../../../helpers';
 import { getOnboardingComponentFor } from '../../../utils/onboardingmanager';
 import useWindowSize from '../../../utils/resizehook';
+import { FileInformation } from './addartifact';
 
 export const ReleaseTooltip = () => (
   <div style={{ fontSize: 12 }}>
@@ -21,18 +22,62 @@ export const ReleaseTooltip = () => (
   </div>
 );
 
-export const ArtifactInformation = ({ customDeviceTypes, deviceTypes = [], name, onboardingState, selectedDeviceTypes = [], updateCreation }) => {
+export const VersionInformation = ({ creation = {}, onRemove, updateCreation }) => {
+  const { file, fileSystem: propFs, name, softwareName: propName, softwareVersion: version = '', type } = creation;
+  const [fileSystem, setFileSystem] = useState(propFs);
+  const [softwareName, setSoftwareName] = useState(propName);
+  const [softwareVersion, setSoftwareVersion] = useState(version || name);
+
+  useEffect(() => {
+    updateCreation({ finalStep: true });
+  }, []);
+
+  useEffect(() => {
+    updateCreation({ fileSystem, softwareName, softwareVersion, isValid: fileSystem && softwareName && softwareVersion });
+  }, [fileSystem, softwareName, softwareVersion]);
+
+  return (
+    <>
+      <FileInformation file={file} type={type} onRemove={onRemove} />
+      <h4>Version information</h4>
+      <div className="flexbox column">
+        {[
+          { key: 'fileSystem', title: 'Software filesystem', setter: setFileSystem, value: fileSystem },
+          { key: 'softwareName', title: 'Software name', setter: setSoftwareName, value: softwareName },
+          { key: 'softwareVersion', title: 'Software version', setter: setSoftwareVersion, value: softwareVersion }
+        ].map(({ key, title, setter, value: currentValue }, index) => (
+          <TextField autoFocus={!index} fullWidth key={key} label={title} onChange={({ target: { value } }) => setter(value)} value={currentValue} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const checkDestinationValidity = destination => (destination.length ? /^(?:\/|[a-z]+:\/\/)/.test(destination) : true);
+
+export const ArtifactInformation = ({ advanceOnboarding, creation = {}, deviceTypes = [], onboardingState, onRemove, updateCreation }) => {
+  const { customDeviceTypes = [], destination = '', file, name = '', selectedDeviceTypes = [], type } = creation;
   const deviceTypeRef = useRef();
   const releaseNameRef = useRef();
+  const destinationRef = useRef();
   // eslint-disable-next-line no-unused-vars
   const size = useWindowSize();
+
+  useEffect(() => {
+    const nextDestination = onboardingState.complete ? destination : '/data/www/localhost/htdocs';
+    updateCreation({
+      destination: nextDestination,
+      isValid: checkDestinationValidity(nextDestination) && selectedDeviceTypes.length && name,
+      finalStep: false
+    });
+  }, []);
 
   // to allow device types to automatically be selected on entered ',' we have to filter the input and transform any completed device types (followed by a ',')
   // while also checking for duplicates and allowing complete resets of the input
   const onTextInputChange = (inputValue, reason) => {
     const value = inputValue || '';
     if (reason === 'clear') {
-      return updateCreation({ customDeviceTypes: '', selectedDeviceTypes: [] });
+      return updateCreation({ customDeviceTypes: '', selectedDeviceTypes: [], isValid: false });
     } else if (reason === 'reset') {
       return;
     }
@@ -40,13 +85,25 @@ export const ArtifactInformation = ({ customDeviceTypes, deviceTypes = [], name,
     const possibleCustomDeviceTypes = value.substring(0, lastIndex).split(',').filter(duplicateFilter);
     const customDeviceTypes = value.substring(lastIndex + 1);
     const possibleDeviceTypeSelection = unionizeStrings(selectedDeviceTypes, possibleCustomDeviceTypes);
-    updateCreation({ customDeviceTypes, selectedDeviceTypes: possibleDeviceTypeSelection });
+    if (customDeviceTypes.length > 3) {
+      advanceOnboarding(onboardingSteps.UPLOAD_NEW_ARTIFACT_DIALOG_DEVICE_TYPE);
+    }
+    updateCreation({ customDeviceTypes, isValid: possibleDeviceTypeSelection.length && name && destination, selectedDeviceTypes: possibleDeviceTypeSelection });
   };
+
+  const onDeviceTypeSelectionChange = value => updateCreation({ isValid: value.length && name && destination, selectedDeviceTypes: value });
 
   const onTextInputLeave = value => {
     const possibleDeviceTypeSelection = unionizeStrings(selectedDeviceTypes, [value]);
-    updateCreation({ customDeviceTypes: '', selectedDeviceTypes: possibleDeviceTypeSelection });
+    updateCreation({
+      customDeviceTypes: '',
+      isValid: possibleDeviceTypeSelection.length && name && destination,
+      selectedDeviceTypes: possibleDeviceTypeSelection
+    });
   };
+
+  const onDestinationChange = ({ target: { value } }) =>
+    updateCreation({ destination: value, isValid: checkDestinationValidity(value) && selectedDeviceTypes.length && name });
 
   let onboardingComponent = null;
   if (!onboardingState.complete && deviceTypeRef.current && releaseNameRef.current) {
@@ -58,10 +115,23 @@ export const ArtifactInformation = ({ customDeviceTypes, deviceTypes = [], name,
       left: releaseNameRef.current.parentElement.parentElement.offsetLeft + releaseNameRef.current.clientWidth,
       top: releaseNameRef.current.parentElement.parentElement.offsetTop + releaseNameRef.current.clientHeight / 2
     };
-    onboardingComponent = getOnboardingComponentFor(onboardingSteps.UPLOAD_NEW_ARTIFACT_DIALOG_DEVICE_TYPE, onboardingState, {
-      anchor: deviceTypeAnchor,
+    const destinationAnchor = {
+      left: destinationRef.current.parentElement.parentElement.offsetLeft + destinationRef.current.clientWidth,
+      top: destinationRef.current.parentElement.parentElement.offsetTop + destinationRef.current.clientHeight / 2
+    };
+    onboardingComponent = getOnboardingComponentFor(onboardingSteps.UPLOAD_NEW_ARTIFACT_DIALOG_DESTINATION, onboardingState, {
+      anchor: destinationAnchor,
       place: 'right'
     });
+    onboardingComponent = getOnboardingComponentFor(
+      onboardingSteps.UPLOAD_NEW_ARTIFACT_DIALOG_DEVICE_TYPE,
+      onboardingState,
+      {
+        anchor: deviceTypeAnchor,
+        place: 'right'
+      },
+      onboardingComponent
+    );
     onboardingComponent = getOnboardingComponentFor(
       onboardingSteps.UPLOAD_NEW_ARTIFACT_DIALOG_RELEASE_NAME,
       onboardingState,
@@ -70,18 +140,50 @@ export const ArtifactInformation = ({ customDeviceTypes, deviceTypes = [], name,
     );
   }
 
+  const isValidDestination = checkDestinationValidity(destination);
   return (
-    <div className="flexbox column" style={{ maxWidth: 400 }}>
+    <div className="flexbox column">
+      <FileInformation file={file} type={type} onRemove={onRemove} />
+      <TextField
+        autoFocus={true}
+        error={!isValidDestination}
+        fullWidth
+        helperText={!isValidDestination && <div className="warning">Destination has to be an absolute path</div>}
+        inputProps={{ style: { marginTop: 16 } }}
+        InputLabelProps={{ shrink: true }}
+        label="Destination directory where the file will be installed on your devices"
+        onChange={onDestinationChange}
+        placeholder="Example: /opt/installed-by-single-file"
+        ref={destinationRef}
+        value={destination}
+      />
       <h4>Artifact information</h4>
+      <FormControl>
+        <InputLabel htmlFor="release-name" style={{ alignItems: 'center', display: 'flex' }}>
+          Release name
+          <Tooltip key="release-name-tip" title={<ReleaseTooltip />} placement="bottom" arrow leaveDelay={300}>
+            <InfoIcon fontSize="small" classes={{ root: 'margin-left-small' }} />
+          </Tooltip>
+        </InputLabel>
+        <Input
+          defaultValue={name}
+          className="release-name-input"
+          id="release-name"
+          placeholder="A descriptive name for the software"
+          onChange={e => updateCreation({ name: e.target.value })}
+          inputRef={releaseNameRef}
+        />
+      </FormControl>
       <Autocomplete
         id="compatible-device-type-selection"
+        // blurOnSelect
         value={selectedDeviceTypes}
         filterSelectedOptions
         freeSolo={true}
         includeInputInList={true}
         multiple
         // allow edits to the textinput without deleting existing device types by ignoring backspace
-        onChange={(e, value) => (e.key !== 'Backspace' ? updateCreation({ selectedDeviceTypes: value }) : null)}
+        onChange={(e, value) => (e.key !== 'Backspace' ? onDeviceTypeSelectionChange(value) : null)}
         onInputChange={(e, v, reason) => onTextInputChange(null, reason)}
         options={deviceTypes}
         renderInput={params => (
@@ -102,25 +204,16 @@ export const ArtifactInformation = ({ customDeviceTypes, deviceTypes = [], name,
           />
         )}
       />
-      <FormControl>
-        <InputLabel htmlFor="release-name" style={{ alignItems: 'center', display: 'flex' }}>
-          Release name
-          <Tooltip key="release-name-tip" title={<ReleaseTooltip />} placement="bottom" arrow leaveDelay={300}>
-            <InfoIcon fontSize="small" classes={{ root: 'margin-left-small' }} />
-          </Tooltip>
-        </InputLabel>
-        <Input
-          defaultValue={name}
-          className="release-name-input"
-          id="release-name"
-          placeholder="A descriptive name for the software"
-          onChange={e => updateCreation({ name: e.target.value })}
-          inputRef={releaseNameRef}
-        />
-      </FormControl>
       {!!onboardingComponent && onboardingComponent}
     </div>
   );
 };
 
-export default ArtifactInformation;
+const steps = [ArtifactInformation, VersionInformation];
+
+export const ArtifactInformationForm = ({ activeStep, ...remainder }) => {
+  const Component = steps[activeStep];
+  return <Component {...remainder} />;
+};
+
+export default ArtifactInformationForm;
