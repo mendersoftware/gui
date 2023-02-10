e0c5f43975760c7a8578bf21e0c0aa91e486ab00
Manuel Zedel <manuel.zedel@northern.tech>
2023-02-10 18:21:28 +0100

    fix: switched to custom pie chart scaling to account for flexible widget layout
    Ticket: MEN-6304
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/dashboard/widgets/distribution.js
+++ src/js/components/dashboard/widgets/distribution.js
@@ -6,7 +6,7 @@ import { IconButton, LinearProgress, linearProgressClasses, svgIconClasses } fro
 import { useTheme } from '@mui/styles';
 import { makeStyles } from 'tss-react/mui';
 
-import { VictoryBar, VictoryPie, VictoryStack } from 'victory';
+import { VictoryBar, VictoryContainer, VictoryPie, VictoryStack } from 'victory';
 
 import { ensureVersionString } from '../../../actions/deviceActions';
 import { chartTypes } from '../../../constants/appConstants';
@@ -113,7 +113,11 @@ const BarChart = ({ data, events }) => {
   );
 };
 
-const ChartContainer = ({ className, children }) => <div className={className}>{children}</div>;
+const ChartContainer = ({ className, children, innerRef, style = {} }) => (
+  <div className={className} ref={innerRef} style={style}>
+    {children}
+  </div>
+);
 
 const BarChartContainer = ({ classes = {}, data, events, ...remainder }) => (
   <ChartContainer className={classes.wrapper}>
@@ -124,12 +128,21 @@ const BarChartContainer = ({ classes = {}, data, events, ...remainder }) => (
 
 const PieChart = props => <VictoryPie {...props} padding={{ left: 0, right: 0, top: 0, bottom: 15 }} />;
 
-const PieChartContainer = ({ classes = {}, ...chartProps }) => (
-  <ChartContainer className={classes.wrapper}>
-    <ChartLegend {...chartProps} classes={classes} />
-    <PieChart {...chartProps} />
-  </ChartContainer>
-);
+const padding = 10;
+const PieChartContainer = ({ classes = {}, ...chartProps }) => {
+  const ref = useRef();
+  let height;
+  if (ref.current) {
+    // use the widget height, remove the space the header takes up and account for widget padding (top + padding) + own padding for the chart
+    height = ref.current.parentElement.offsetHeight - ref.current.parentElement.children[0].offsetHeight - 3 * padding;
+  }
+  return (
+    <ChartContainer className={classes.wrapper} innerRef={ref} style={{ height }}>
+      <ChartLegend {...chartProps} classes={classes} />
+      {height && <PieChart {...chartProps} containerComponent={<VictoryContainer style={{ height }} />} />}
+    </ChartContainer>
+  );
+};
 
 const VictoryBarChartContainer = ({ classes = {}, ...chartProps }) => (
   <ChartContainer className={classes.wrapper}>
