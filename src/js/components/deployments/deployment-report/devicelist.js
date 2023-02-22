7a374700ded624dff0b5f9c234316e0e55725e89
Manuel Zedel <manuel.zedel@northern.tech>
2023-02-22 13:34:42 +0100

    refac: made deployment devices list reuse detailstable - also added size + delta information
    Ticket: MEN-3917
    Changelog: added size + delta information in deployment device table
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/deployment-report/devicelist.js
+++ src/js/components/deployments/deployment-report/devicelist.js
@@ -1,17 +1,166 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // material ui
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Button, LinearProgress } from '@mui/material';
+import { makeStyles } from 'tss-react/mui';
 
+import DeltaIcon from '../../../../assets/img/deltaicon.svg';
 import { TIMEOUTS } from '../../../constants/appConstants';
+import { deploymentSubstates } from '../../../constants/deploymentConstants';
 import { DEVICE_LIST_DEFAULTS } from '../../../constants/deviceConstants';
+import { rootfsImageVersion as rootfsImageVersionAttribute } from '../../../constants/releaseConstants';
+import { FileSize, formatTime } from '../../../helpers';
+import { TwoColumns } from '../../common/configurationobject';
+import DetailsTable from '../../common/detailstable';
+import DeviceIdentityDisplay from '../../common/deviceidentity';
 import Loader from '../../common/loader';
+import MenderTooltip from '../../common/mendertooltip';
 import Pagination from '../../common/pagination';
-import DeploymentDeviceListItem from './deploymentdevicelistitem';
+import { MaybeTime } from '../../common/time';
+
+const useStyles = makeStyles()(() => ({
+  table: { minHeight: '10vh', maxHeight: '40vh', overflowX: 'auto' }
+}));
 
 const { page: defaultPage } = DEVICE_LIST_DEFAULTS;
 
-const deviceListColumns = ['Device type', 'Current software', 'Started', 'Finished', 'Attempts', 'Deployment status', ''];
+const stateTitleMap = {
+  noartifact: 'No compatible artifact found',
+  'already-installed': 'Already installed',
+  'pause-before-installing': 'Paused before installing',
+  'pause-before-rebooting': 'Paused before rebooting',
+  'pause-before-committing': 'Paused before committing'
+};
+
+const determinedStateMap = {
+  'noartifact': 0,
+  'aborted': 100,
+  'already-installed': 100,
+  'failure': 100,
+  'success': 100
+};
+
+const statusColorMap = {
+  failure: 'secondary',
+  aborted: 'secondary',
+  default: 'primary'
+};
+
+const undefinedStates = [deploymentSubstates.pending, deploymentSubstates.decommissioned, deploymentSubstates.alreadyInstalled];
+
+const canShow = () => true;
+
+const deviceListColumns = [
+  {
+    key: 'idAttribute',
+    title: 'id',
+    renderTitle: ({ idAttribute }) => idAttribute,
+    render: ({ device, idAttribute }) => (
+      <Link style={{ fontWeight: 'initial', opacity: idAttribute === 'name' ? 0.6 : 1 }} to={`/devices?id=${device.id}`}>
+        <DeviceIdentityDisplay device={device} idAttribute={idAttribute} isEditable={false} />
+      </Link>
+    ),
+    canShow
+  },
+  {
+    key: 'device-type',
+    title: 'Device Type',
+    render: ({ device }) => {
+      const { attributes = {} } = device;
+      const { device_type: deviceTypes = [] } = attributes;
+      return deviceTypes.length ? deviceTypes.join(',') : '-';
+    },
+    canShow
+  },
+  {
+    key: 'current-software',
+    title: 'Current software',
+    render: ({ device: { attributes = {} }, userCapabilities: { canReadReleases } }) => {
+      const { artifact_name, [rootfsImageVersionAttribute]: rootfsImageVersion } = attributes;
+      const softwareName = rootfsImageVersion || artifact_name;
+      const encodedArtifactName = encodeURIComponent(softwareName);
+      return softwareName ? (
+        canReadReleases ? (
+          <Link style={{ fontWeight: 'initial' }} to={`/releases/${encodedArtifactName}`}>
+            {softwareName}
+          </Link>
+        ) : (
+          softwareName
+        )
+      ) : (
+        '-'
+      );
+    },
+    canShow
+  },
+  { key: 'started', title: 'Started', render: ({ device: { created } }) => <MaybeTime value={formatTime(created)} />, sortable: false, canShow },
+  { key: 'finished', title: 'Finished', render: ({ device: { finished } }) => <MaybeTime value={formatTime(finished)} />, sortable: false, canShow },
+  {
+    key: 'artifact_size',
+    title: 'Artifact size',
+    render: ({ device: { image = {} } }) => {
+      const { size } = image;
+      return <FileSize fileSize={size} />;
+    },
+    sortable: false,
+    canShow
+  },
+  {
+    key: 'delta',
+    title: '',
+    render: ({ device: { isDelta } }) =>
+      isDelta ? (
+        <MenderTooltip placement="bottom" title="Device is enabled for delta updates">
+          <DeltaIcon />
+        </MenderTooltip>
+      ) : (
+        ''
+      ),
+    canShow
+  },
+  {
+    key: 'attempts',
+    title: 'Attempts',
+    render: ({ device: { attempts, retries } }) => (attempts || 1) / (retries + 1),
+    canShow: ({ deployment: { retries } }) => !!retries
+  },
+  {
+    key: 'status',
+    title: 'Deployment status',
+    render: ({ device: { substate, status = '' } }) => {
+      const statusTitle = stateTitleMap[status] || status;
+      const progressColor = statusColorMap[statusTitle.toLowerCase()] ?? statusColorMap.default;
+      const devicePercentage = determinedStateMap[status];
+      return (
+        <>
+          {substate ? (
+            <div className="flexbox">
+              <div className="capitalized-start" style={{ verticalAlign: 'top' }}>{`${statusTitle}: `}</div>
+              <div className="substate">{substate}</div>
+            </div>
+          ) : (
+            statusTitle
+          )}
+          {!undefinedStates.includes(status.toLowerCase()) && (
+            <div style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+              <LinearProgress color={progressColor} value={devicePercentage} variant={devicePercentage !== undefined ? 'determinate' : 'indeterminate'} />
+            </div>
+          )}
+        </>
+      );
+    },
+    canShow
+  },
+  {
+    key: 'log',
+    title: '',
+    render: ({ device: { id, log }, viewLog }) => (log ? <Button onClick={() => viewLog(id)}>View log</Button> : null),
+    canShow
+  }
+];
+
+const ValueFileSize = ({ value, ...props }) => <FileSize fileSize={value} {...props} />;
 
 export const DeploymentDeviceList = ({
   deployment,
@@ -27,8 +176,11 @@ export const DeploymentDeviceList = ({
   const [currentPage, setCurrentPage] = useState(defaultPage);
   const [loadingDone, setLoadingDone] = useState(false);
   const [perPage, setPerPage] = useState(10);
-  const { device_count = 0, retries, totalDeviceCount: totalDevices } = deployment;
+  const { device_count = 0, totalDeviceCount: totalDevices, statistics = {} } = deployment;
+  const totalSize = statistics.total_size ?? 0;
   const totalDeviceCount = totalDevices ?? device_count;
+  const timer = useRef();
+  const { classes } = useStyles();
 
   useEffect(() => {
     setCurrentPage(defaultPage);
@@ -54,30 +206,35 @@ export const DeploymentDeviceList = ({
   }, [currentPage, deployment.status, deployment.stats, loadingDone]);
 
   useEffect(() => {
-    setTimeout(() => setLoadingDone(true), TIMEOUTS.oneSecond);
+    timer.current = setTimeout(() => setLoadingDone(true), TIMEOUTS.oneSecond);
+    return () => {
+      clearTimeout(timer.current);
+    };
   }, []);
 
+  const onChangeSorting = sortingColumn => {
+    console.log(sortingColumn);
+  };
+
+  const columns = deviceListColumns.reduce((accu, column) => (column.canShow({ deployment }) ? [...accu, { ...column, extras: { idAttribute } }] : accu), []);
+  const items = selectedDevices.map(device => ({ device, id: device.id, idAttribute, userCapabilities, viewLog }));
   return (
     <>
       <Loader show={!loadingDone} />
       {!!totalDeviceCount && (
         <div>
-          <Table style={{ minHeight: '10vh', maxHeight: '40vh', overflowX: 'auto' }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>{idAttribute}</TableCell>
-                {deviceListColumns.map((content, index) =>
-                  content != 'Attempts' || retries ? <TableCell key={`device-list-header-${index + 1}`}>{content}</TableCell> : null
-                )}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {selectedDevices.map(device => (
-                <DeploymentDeviceListItem key={device.id} device={device} idAttribute={idAttribute} userCapabilities={userCapabilities} viewLog={viewLog} />
-              ))}
-            </TableBody>
-          </Table>
-          <Pagination count={totalDeviceCount} rowsPerPage={perPage} onChangePage={setCurrentPage} onChangeRowsPerPage={setPerPage} page={currentPage} />
+          <DetailsTable className={classes.table} columns={columns} items={items} onChangeSorting={onChangeSorting} />
+          <div className="flexbox space-between center-aligned margin-top">
+            <Pagination
+              className="margin-top-none"
+              count={totalDeviceCount}
+              rowsPerPage={perPage}
+              onChangePage={setCurrentPage}
+              onChangeRowsPerPage={setPerPage}
+              page={currentPage}
+            />
+            <TwoColumns chipLikeKey={false} compact items={{ 'Total download size': totalSize }} ValueComponent={ValueFileSize} />
+          </div>
         </div>
       )}
     </>
