f0e5858bc0de4fd21f09fe866fd8ac6b64edbee7
Manuel Zedel <manuel.zedel@northern.tech>
2023-03-22 12:36:19 +0100

    chore(e2e-tests): increased resiliency against rbac limitations & ui rendering slowdowns on login
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/e2e_tests/integration/01-login.spec.ts
+++ tests/e2e_tests/integration/01-login.spec.ts
@@ -27,8 +27,10 @@ test.describe('Login', () => {
       // now we can log out
       await page.click('.header-dropdown', { force: true });
       await page.click(`text=Log out`, { force: true });
-      await page.waitForSelector(`text=Log in`, { timeout: 5000 });
-      expect(await page.isVisible(`text=Log in`)).toBeTruthy();
+      const loginButton = page.getByRole('button', { name: /log in/i });
+      await loginButton.waitFor({ timeout: 7000 });
+      const loginVisible = await loginButton.isVisible();
+      expect(loginVisible).toBeTruthy();
     });
 
     test('does not stay logged in across sessions, after browser restart', async ({ baseUrl, page }) => {
