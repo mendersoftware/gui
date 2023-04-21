bba37e5345c2ad376888813811324b4fd321ffc6
Manuel Zedel <manuel.zedel@northern.tech>
2023-04-21 13:35:25 +0200

    chore: added api types generation based on enterprise management specs
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ scripts/typeGeneration/convert.js
@@ -0,0 +1,135 @@
+import { basename, dirname, join, resolve } from 'https://deno.land/std/path/posix.ts';
+import { parse, stringify } from 'https://deno.land/std/yaml/mod.ts';
+import { camelCase } from 'https://deno.land/x/case/mod.ts';
+import { generate } from 'npm:openapi-typescript-codegen';
+import converter from 'npm:swagger2openapi';
+
+const apiTypes = ['management'];
+
+const getFiles = async folder => {
+  const files = [];
+  for await (const singleDirEntry of Deno.readDir(folder)) {
+    if (singleDirEntry.isDirectory) {
+      files.push(...(await getFiles(join(folder, singleDirEntry.name))));
+    } else if (singleDirEntry.isFile) {
+      const res = resolve(folder, singleDirEntry.name);
+      const filename = res.substring(res.lastIndexOf('/') + 1, res.indexOf('.yml'));
+      const apiType = filename.substring(0, filename.indexOf('_'));
+      let versionSuffix = filename.substring(filename.lastIndexOf('_'));
+      versionSuffix = versionSuffix != '_api' ? versionSuffix : '';
+      const service = basename(dirname(res));
+      if (apiTypes.includes(apiType)) {
+        files.push({ filename, apiType, path: res, service, versionSuffix });
+      }
+    }
+  }
+  return files;
+};
+
+const getFileContents = async file => {
+  const fileContent = await Deno.readTextFile(file.path);
+  const fileData = parse(fileContent);
+  if (!fileData.swagger) {
+    return { ...file, fileData };
+  }
+  const converted = await converter.convertObj(fileData, {});
+  return { ...file, fileData: converted.openapi };
+};
+
+const sanitizeSchemaRefs = (pathSpec, renamedSchemas) => {
+  const text = JSON.stringify(pathSpec);
+  const sanitizedText = renamedSchemas.reduce((accu, { key, sanitizedKey }) => accu.replaceAll(`/schemas/${key}"`, `/schemas/${sanitizedKey}"`), text);
+  return JSON.parse(sanitizedText);
+};
+
+const repairPathSpec = (pathSpec, service, versionSuffix) => {
+  const text = JSON.stringify(pathSpec);
+  const sanitizedText = text.replaceAll(`"summary":`, `"description":`);
+  const content = JSON.parse(sanitizedText);
+  return Object.entries(content).reduce(
+    (accu, [key, value]) => ({
+      ...accu,
+      [key]: {
+        ...value,
+        summary: value.summary ?? value.operationId,
+        operationId: camelCase(`${service}${value.operationId || value.summary}${versionSuffix}`)
+      }
+    }),
+    {}
+  );
+};
+
+const sanitizeSchemas = (schemas, existingSchemas, service, counter = {}) =>
+  Object.entries(schemas).reduce(
+    (schemaAccu, [key, schema]) => {
+      let sanitizedKey = key;
+      if (existingSchemas[key]) {
+        schemaAccu.counter[key] = (schemaAccu.counter[key] ?? 1) + 1;
+        console.log(`duplicate schema, candidate for merge: ${key} (${service}) - total: ${schemaAccu.counter[key]}`);
+        sanitizedKey = `${key}${service[0].toUpperCase()}${service.substring(1)}`;
+        schemaAccu.renamedSchemas.push({ key, sanitizedKey });
+      }
+      schemaAccu.serviceSchemas[sanitizedKey] = schema;
+      return schemaAccu;
+    },
+    { renamedSchemas: [], serviceSchemas: {}, counter }
+  );
+
+const defaultManagementUrl = 'https://hosted.mender.io/api/management';
+const baseSpec = {
+  openapi: '3.0.3',
+  info: {
+    contact: { name: 'contact@mender.io' },
+    description: 'Combined API specification for the management features of the different Mender backend services, suitable for code generation applications',
+    title: 'Mender Management API',
+    version: '1.0'
+  },
+  tags: [{ name: 'Management API', description: 'used for management APIs' }],
+  servers: [{ url: defaultManagementUrl }],
+  paths: {},
+  components: { securitySchemes: {}, schemas: {} }
+};
+
+const processFiles = async root => {
+  const files = await getFiles(root);
+  const fileContents = await Promise.all(files.map(getFileContents));
+  return fileContents.reduce((accu, { fileData, service, versionSuffix }) => {
+    const { renamedSchemas, serviceSchemas, counter } = sanitizeSchemas(fileData.components.schemas, accu.components.schemas, service, accu.counter);
+    const basePath = fileData.servers[0].url.substring(defaultManagementUrl.length);
+    const scopedPaths = Object.entries(fileData.paths).reduce((pathAccu, [path, content]) => {
+      const sanitizedContent = sanitizeSchemaRefs(content, renamedSchemas);
+      pathAccu[`${basePath}${path}`] = repairPathSpec(sanitizedContent, service, versionSuffix);
+      return pathAccu;
+    }, {});
+    return {
+      ...accu,
+      components: {
+        ...accu.components,
+        ...fileData.components,
+        responses: { ...accu.components.responses, ...fileData.components.responses },
+        schemas: { ...accu.components.schemas, ...serviceSchemas },
+        securitySchemes: { ...accu.components.securitySchemes, ...fileData.components.securitySchemes }
+      },
+      counter,
+      paths: { ...accu.paths, ...scopedPaths }
+    };
+  }, baseSpec);
+};
+
+const generateTypeIndex = async () => {
+  const fileContent = await Deno.readTextFile('./generated/index.ts');
+  const lines = fileContent.split('\n').reduce((accu, line) => {
+    if (line.startsWith('/* ')) {
+      accu.push(line);
+    } else if (line.includes(`from './models/`)) {
+      accu.push(line.replace('/models', ''));
+    }
+    return accu;
+  }, []);
+  await Deno.writeTextFile('./generated/models/MenderTypes.ts', lines.join('\n'));
+};
+
+const mergedContent = await processFiles('./specs');
+await generate({ input: mergedContent, output: './generated' });
+await Deno.writeTextFile('combined.yml', stringify(mergedContent));
+await generateTypeIndex();
