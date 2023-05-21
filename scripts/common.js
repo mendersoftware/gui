e7821fb7f0a03bafa17b2b8d724086b4ac1177a3
Manuel Zedel <manuel.zedel@northern.tech>
2023-05-21 17:15:07 +0200

    chore: extracted shared scripting util & used it for type conversion
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ scripts/common.js
@@ -0,0 +1,17 @@
+import { join, resolve } from 'https://deno.land/std/path/posix.ts';
+
+export const getFiles = async (folder, { fileProcessor }) => {
+  const files = [];
+  for await (const singleDirEntry of Deno.readDir(folder)) {
+    if (singleDirEntry.isDirectory) {
+      files.push(...(await getFiles(join(folder, singleDirEntry.name), { fileProcessor })));
+    } else if (singleDirEntry.isFile) {
+      const res = resolve(folder, singleDirEntry.name);
+      const fileInfo = await fileProcessor(res);
+      if (fileInfo) {
+        files.push(fileInfo);
+      }
+    }
+  }
+  return files;
+};
