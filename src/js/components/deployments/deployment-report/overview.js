fab398fc48ef9a5d8f4d2f0f74f52c22270cd94e
Manuel Zedel <manuel.zedel@northern.tech>
2023-05-30 09:55:56 +0200

    fix: fixed an issue that could prevent showing the device identity for a fresh deployment
    Ticket: MEN-6485
    Changelog: Title
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/components/deployments/deployment-report/overview.js
+++ src/js/components/deployments/deployment-report/overview.js
@@ -24,7 +24,7 @@ import isUUID from 'validator/lib/isUUID';
 import failImage from '../../../../assets/img/largeFail.png';
 import successImage from '../../../../assets/img/largeSuccess.png';
 import { DEPLOYMENT_STATES, DEPLOYMENT_TYPES } from '../../../constants/deploymentConstants';
-import { groupDeploymentStats } from '../../../helpers';
+import { groupDeploymentStats, isEmpty } from '../../../helpers';
 import { TwoColumnData } from '../../common/configurationobject';
 import DeviceIdentityDisplay from '../../common/deviceidentity';
 import Time from '../../common/time';
@@ -61,21 +61,30 @@ const defaultLinkProps = {
 
 export const DeploymentOverview = ({ creator, deployment, devicesById, idAttribute, onScheduleClick, tenantCapabilities }) => {
   const { classes } = useStyles();
-  const { artifact_name, devices = {}, filter, group, status, totalDeviceCount } = deployment;
+  const {
+    artifact_name,
+    created: creationTime = new Date().toISOString(),
+    devices = {},
+    filter,
+    group,
+    name = '',
+    status,
+    totalDeviceCount,
+    type = DEPLOYMENT_TYPES.software
+  } = deployment;
   const { failures, successes } = groupDeploymentStats(deployment);
+  const finished = deployment.finished || status === DEPLOYMENT_STATES.finished;
+  const isDeviceDeployment = isUUID(name) && (isEmpty(devices) || Object.keys(devices).length === 1);
+  const isSoftwareDeployment = type === DEPLOYMENT_TYPES.software;
   const { hasFullFiltering } = tenantCapabilities;
 
-  const finished = deployment.finished || status === DEPLOYMENT_STATES.finished;
-
-  const { created: creationTime = new Date().toISOString(), name = '', type = DEPLOYMENT_TYPES.software } = deployment;
-  const isSoftwareDeployment = type === DEPLOYMENT_TYPES.software;
   let deploymentRelease = (
     <Link {...defaultLinkProps} to={`/releases/${encodeURIComponent(artifact_name)}`}>
       {artifact_name}
       <LaunchIcon className="margin-left-small" fontSize="small" />
     </Link>
   );
-  const isDeviceDeployment = isUUID(name) && Object.keys(devices).length === 1;
+
   const devicesLink = getDevicesLink({ devices: Object.values(devices), group, hasFullFiltering, name });
   let targetDevices = (
     <Link {...defaultLinkProps} to={devicesLink}>
